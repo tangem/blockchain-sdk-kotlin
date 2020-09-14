9e2ab1fe478cf60fad27f604460152d4ecae8017
Tangem <git@tangem.com>
2020-09-14 23:44:40 +0300

    Updated on 2025-10-09

--- blockchain/src/main/java/com/tangem/blockchain/common/WalletManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/WalletManager.kt
@@ -12,6 +12,23 @@ abstract class WalletManager(val cardId: String, var wallet: Wallet) {
 
     abstract suspend fun update()
 
+    protected open fun updateRecentTransactions(transactions: List<BasicTransactionData>) {
+        val (confirmedTransactions, unconfirmedTransactions) =
+                transactions.partition { it.isConfirmed }
+
+        wallet.recentTransactions.forEach {
+            if (confirmedTransactions.find {confirmed -> confirmed.hash == it.hash } != null) {
+                it.status = TransactionStatus.Confirmed
+            }
+        }
+        unconfirmedTransactions.forEach {
+            if (wallet.recentTransactions.find { unconfirmed -> unconfirmed.hash == it.hash } == null) {
+                wallet.recentTransactions.add(it.toTransactionData())
+            }
+        }
+        wallet.sentTransactionsCount = transactions.filter { it.balanceDif < 0.toBigDecimal() }.size
+    }
+
     fun createTransaction(amount: Amount, fee: Amount, destination: String): TransactionData {
         return TransactionData(amount, fee,
                     wallet.address, destination, wallet.token?.contractAddress,
@@ -34,6 +51,23 @@ abstract class WalletManager(val cardId: String, var wallet: Wallet) {
     private fun validateAmount(amount: Amount): Boolean {
         return amount.isAboveZero() &&
                 wallet.fundsAvailable(amount.type) >= amount.value
+    }
+
+    protected fun BasicTransactionData.toTransactionData(): TransactionData {
+        val isIncoming = this.balanceDif.signum() > 0
+        return TransactionData(
+                amount = Amount(wallet.amounts[AmountType.Coin]!!, this.balanceDif.abs()),
+                fee = null,
+                sourceAddress = if (isIncoming) "unknown" else wallet.address,
+                destinationAddress = if (isIncoming) wallet.address else "unknown",
+                hash = this.hash,
+                date = this.date,
+                status = if (this.isConfirmed) {
+                    TransactionStatus.Confirmed
+                } else {
+                    TransactionStatus.Unconfirmed
+                }
+        )
     }
 }
 
