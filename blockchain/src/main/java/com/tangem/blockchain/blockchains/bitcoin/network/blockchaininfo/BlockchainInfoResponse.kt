442ed4c0ed254a0354d191c4e6156582d7a56a43
Tangem <git@tangem.com>
2020-09-14 18:36:51 +0300

    Updated on 2026-02-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoin/network/blockchaininfo/BlockchainInfoResponse.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoin/network/blockchaininfo/BlockchainInfoResponse.kt
@@ -8,23 +8,33 @@ data class BlockchainInfoAddress(
         @Json(name = "final_balance")
         val finalBalance: Long? = null,
 
+        @Json(name = "n_tx")
+        val transactionCount: Int? = null,
+
         @Json(name = "txs")
         val transactions: List<BlockchainInfoTransaction>? = null
 )
 
 @JsonClass(generateAdapter = true)
 data class BlockchainInfoTransaction(
-        @Json(name = "hash")
         val hash: String? = null,
 
         @Json(name = "block_height")
-        val blockHeight: Long? = null
+        val blockHeight: Long? = null,
+
+        @Json(name = "result")
+        val balanceDif: Long? = null,
+
+        @Json(name = "vin_sz")
+        val inputCount: Int? = null,
+
+        val time: Long? = null
 )
 
 @JsonClass(generateAdapter = true)
 data class BlockchainInfoUnspents(
         @Json(name = "unspent_outputs")
-        val unspentOutputs: List<BlockchainInfoUtxo>
+        val unspentOutputs: List<BlockchainInfoUtxo>? = null
 )
 
 @JsonClass(generateAdapter = true)
