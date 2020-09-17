8ba982cdfc194b1845582ed0d4ae7a12b0ec7222
Tangem <git@tangem.com>
2020-09-17 22:46:11 +0300

    Updated on 2025-10-09

--- blockchain/src/main/java/com/tangem/blockchain/extensions/String.kt
+++ blockchain/src/main/java/com/tangem/blockchain/extensions/String.kt
@@ -1,11 +1,22 @@
 package com.tangem.blockchain.extensions
 
+import com.tangem.blockchain.blockchains.binance.client.encoding.Bech32
+import com.tangem.blockchain.blockchains.binance.client.encoding.Crypto
 import org.bitcoinj.core.AddressFormatException
 import org.bitcoinj.core.Base58
 
 fun String.decodeBase58(): ByteArray? {
     return try {
         Base58.decode(this)
+    } catch (exception: AddressFormatException) {
+        null
+    }
+}
+
+fun String.decodeBech32(): ByteArray? {
+    return try {
+        val decoded: ByteArray = Bech32.decode(this).data
+        Crypto.convertBits(decoded, 0, decoded.size, 5, 8, false)
     } catch (exception: AddressFormatException) {
         null
     }
