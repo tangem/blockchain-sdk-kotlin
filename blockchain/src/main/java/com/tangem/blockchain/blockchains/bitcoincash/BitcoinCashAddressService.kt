48c879e34238158686c4cec8fbbcfe50ec11b1cc
Tangem <git@tangem.com>
2020-11-20 23:40:04 +0300

    Updated on 2026-06-27

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoincash/BitcoinCashAddressService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoincash/BitcoinCashAddressService.kt
@@ -2,12 +2,12 @@ package com.tangem.blockchain.blockchains.bitcoincash
 
 import com.tangem.blockchain.blockchains.bitcoincash.cashaddr.BitcoinCashAddressType
 import com.tangem.blockchain.blockchains.bitcoincash.cashaddr.CashAddr
-import com.tangem.blockchain.common.AddressService
+import com.tangem.blockchain.common.address.AddressService
 import com.tangem.common.extensions.calculateRipemd160
 import com.tangem.common.extensions.calculateSha256
 import com.tangem.common.extensions.toCompressedPublicKey
 
-class BitcoinCashAddressService() : AddressService {
+class BitcoinCashAddressService() : AddressService() {
     override fun makeAddress(walletPublicKey: ByteArray): String {
         val publicKeyHash = walletPublicKey.toCompressedPublicKey().calculateSha256().calculateRipemd160()
         return CashAddr.toCashAddress(BitcoinCashAddressType.P2PKH, publicKeyHash)
