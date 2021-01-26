50fbce70151c8c2f461554d67ad2d8c41f21e0a5
Tangem <git@tangem.com>
2021-01-26 20:30:22 +0300

    Updated on 2026-05-04

--- blockchain/src/test/java/com/tangem/blockchain/blockchains/xrp/XrpAddressTest.kt
+++ blockchain/src/test/java/com/tangem/blockchain/blockchains/xrp/XrpAddressTest.kt
@@ -4,7 +4,7 @@ import com.google.common.truth.Truth
 import com.tangem.common.extensions.hexToBytes
 import org.junit.Test
 
-internal class XrpAddressTest {
+class XrpAddressTest {
 
     private val addressService = XrpAddressService()
 
@@ -19,17 +19,36 @@ internal class XrpAddressTest {
 
     @Test
     fun makeAddressFromCorrectEdPublicKey() {
-        val walletPublicKey = "12CC4DE73BACF875D7423D152E46C1A665F1718CBE7CA0FEB2BA28C149E11909".hexToBytes()
+        val walletPublicKey = "12CC4DE73BACF875D7423D152E46C1A665F1718CBE7CA0FEB2BA28C149E11909"
+                .hexToBytes()
         val expected = "rwWMNBs2GtJwfX7YNVV1sUYaPy6DRmDHB4"
 
-        Truth.assertThat(addressService.makeAddress(walletPublicKey))
-                .isEqualTo(expected)
+        Truth.assertThat(addressService.makeAddress(walletPublicKey)).isEqualTo(expected)
     }
 
     @Test
-    fun validateCorrectAddress() {
+    fun validateCorrectRAddress() {
         val address = "rwWMNBs2GtJwfX7YNVV1sUYaPy6DRmDHB4"
-        Truth.assertThat(addressService.validate(address))
-                .isTrue()
+
+        Truth.assertThat(addressService.validate(address)).isTrue()
+    }
+
+    @Test
+    fun validateCorrectXAddress() {
+        val address = "X75YF1evckmkN9gWKkpN8MhDPeZGyou5x5c6DJT5qAdz1jJ"
+
+        Truth.assertThat(addressService.validate(address)).isTrue()
+    }
+
+    @Test
+    fun decodeCorrectXAddress() {
+        val address = "X75YF1evckmkN9gWKkpN8MhDPeZGyou5x5c6DJT5qAdz1jJ"
+        val expectedAddress = "rnbz2HXVqtsFouFEwbDuhL7doVxirBz2r6"
+        val expectedTag = 555L
+
+        val decoded = XrpAddressService.decodeXAddress(address)
+
+        Truth.assertThat(decoded!!.address).isEqualTo(expectedAddress)
+        Truth.assertThat(decoded.destinationTag).isEqualTo(expectedTag)
     }
 }
