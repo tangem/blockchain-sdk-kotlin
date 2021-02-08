2426602e93763fca22f6b2172e6cc6505244e237
Tangem <git@tangem.com>
2021-02-08 22:32:22 +0300

    develop-1150

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoin/network/BitcoinNetworkManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoin/network/BitcoinNetworkManager.kt
@@ -2,108 +2,34 @@ package com.tangem.blockchain.blockchains.bitcoin.network
 
 import com.tangem.blockchain.blockchains.bitcoin.BitcoinUnspentOutput
 import com.tangem.blockchain.common.BasicTransactionData
-import com.tangem.blockchain.network.blockcypher.BlockcypherApi
-import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.SimpleResult
-import com.tangem.blockchain.network.*
-import com.tangem.blockchain.network.blockchair.BlockchairApi
-import com.tangem.blockchain.network.blockchair.BlockchairProvider
-import com.tangem.blockchain.network.blockcypher.BlockcypherProvider
-import retrofit2.HttpException
-import java.io.IOException
+import com.tangem.blockchain.network.MultiProvider
 import java.math.BigDecimal
 
 
-class BitcoinNetworkManager(blockchain: Blockchain) : BitcoinNetworkService {
-
-//    private val blockchainInfoProvider by lazy {
-//        val api = createRetrofitInstance(API_BLOCKCHAIN_INFO)
-//                .create(BlockchainInfoApi::class.java)
-////        val bitcoinfeesEarnApi = createRetrofitInstance(API_BITCOINFEES_EARN)
-////                .create(BitcoinfeesEarnApi::class.java)
-//        BlockchainInfoProvider(api)
-//    }
-
-    private val blockchairProvider by lazy {
-        val api = createRetrofitInstance(API_BLOCKCHAIR)
-                .create(BlockchairApi::class.java)
-        BlockchairProvider(api, blockchain)
-    }
-
-    private val blockcypherProvider by lazy {
-        val api = createRetrofitInstance(API_BLOCKCYPHER)
-                .create(BlockcypherApi::class.java)
-        BlockcypherProvider(api, blockchain)
-    }
-
-    private var provider: BitcoinNetworkService = blockchairProvider
-
-    private fun changeProvider() {
-        provider = when (provider) {
-            blockchairProvider -> blockcypherProvider
-            else -> blockchairProvider
-        }
-    }
+class BitcoinNetworkManager(providers: List<BitcoinNetworkService>) :
+        MultiProvider<BitcoinNetworkService>(providers),
+        BitcoinNetworkService {
 
     override suspend fun getInfo(address: String): Result<BitcoinAddressInfo> {
         val result = provider.getInfo(address)
-        when (result) {
-            is Result.Success -> return result
-            is Result.Failure -> {
-                if (result.error is IOException || result.error is HttpException) {
-                    changeProvider()
-                    return provider.getInfo(address)
-                } else {
-                    return result
-                }
-            }
-        }
+        return if (result.needsRetry()) getInfo(address) else result
     }
 
     override suspend fun getFee(): Result<BitcoinFee> {
         val result = provider.getFee()
-        when (result) {
-            is Result.Success -> return result
-            is Result.Failure -> {
-                if (result.error is IOException || result.error is HttpException) {
-                    changeProvider()
-                    return provider.getFee()
-                } else {
-                    return result
-                }
-            }
-        }
+        return if (result.needsRetry()) getFee() else result
     }
 
     override suspend fun sendTransaction(transaction: String): SimpleResult {
         val result = provider.sendTransaction(transaction)
-        when (result) {
-            is SimpleResult.Success -> return result
-            is SimpleResult.Failure -> {
-                if (result.error is IOException || result.error is HttpException) {
-                    changeProvider()
-                    return provider.sendTransaction(transaction)
-                } else {
-                    return result
-                }
-            }
-        }
+        return if (result.needsRetry()) sendTransaction(transaction) else result
     }
 
     override suspend fun getSignatureCount(address: String): Result<Int> {
         val result = provider.getSignatureCount(address)
-        when (result) {
-            is Result.Success -> return result
-            is Result.Failure -> {
-                if (result.error is IOException || result.error is HttpException) {
-                    changeProvider()
-                    return provider.getSignatureCount(address)
-                } else {
-                    return result
-                }
-            }
-        }
+        return if (result.needsRetry()) getSignatureCount(address) else result
     }
 }
 
