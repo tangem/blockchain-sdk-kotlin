c02992db147556344493a6daf565d9811a4a66bd
Tangem <git@tangem.com>
2021-02-08 22:32:22 +0300

    releases/5.27.0-1144

--- blockchain/src/main/java/com/tangem/blockchain/network/blockchair/BlockchairApi.kt
+++ blockchain/src/main/java/com/tangem/blockchain/network/blockchair/BlockchairApi.kt
@@ -4,46 +4,39 @@ import com.squareup.moshi.JsonClass
 import retrofit2.http.*
 
 interface BlockchairApi {
-    @GET("{blockchain}/dashboards/address/{address}")
+    @GET("dashboards/address/{address}")
     suspend fun getAddressData(
             @Path("address") address: String,
-            @Path("blockchain") blockchain: String,
             @Query("transaction_details") transactionDetails: Boolean = false,
             @Query("limit") limit: Int? = null,
-            @Query("key") key: String = API_KEY
+            @Query("key") key: String?
     ): BlockchairAddress
 
-    @GET("{blockchain}/dashboards/transaction/{transaction}")
+    @GET("dashboards/transaction/{transaction}")
     suspend fun getTransaction(
             @Path("transaction") transactionHash: String,
-            @Path("blockchain") blockchain: String,
-            @Query("key") key: String = API_KEY
+            @Query("key") key: String?
     ): BlockchairTransaction
 
-    @GET("{blockchain}/stats")
+    @GET("stats")
     suspend fun getBlockchainStats(
-            @Path("blockchain") blockchain: String,
-            @Query("key") key: String = API_KEY
+            @Query("key") key: String?
     ): BlockchairStats
 
-    @POST("{blockchain}/push/transaction")
+    @POST("push/transaction")
     suspend fun sendTransaction(
             @Body sendBody: BlockchairBody,
-            @Path("blockchain") blockchain: String,
-            @Query("key") key: String = API_KEY
+            @Query("key") key: String?
     )
 
-    @GET("{blockchain}/erc-20/{contract_address}/dashboards/address/{address}")
+    @GET("erc-20/{contract_address}/dashboards/address/{address}")
     suspend fun getTokenHolderData(
             @Path("address") address: String,
-            @Path("blockchain") blockchain: String,
             @Path("contract_address") contractAddress: String,
             @Query("limit") limit: Int? = null,
-            @Query("key") key: String = API_KEY
+            @Query("key") key: String?
     ): BlockchairTokenHolder
 }
 
 @JsonClass(generateAdapter = true)
 data class BlockchairBody(val data: String)
-
-private const val API_KEY = "A___0Shpsu4KagE7oSabrw20DfXAqWlT"
