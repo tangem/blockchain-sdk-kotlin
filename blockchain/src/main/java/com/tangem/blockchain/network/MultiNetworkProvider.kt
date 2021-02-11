5124e8d8a205998b81ea99e66faca57b09f28ef2
Tangem <git@tangem.com>
2021-02-11 22:47:54 +0300

    develop-1150

--- blockchain/src/main/java/com/tangem/blockchain/network/MultiNetworkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/network/MultiNetworkProvider.kt
@@ -5,7 +5,7 @@ import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.SimpleResult
 import com.tangem.blockchain.extensions.isNetworkError
 
-abstract class MultiProvider<E>(private val providers: List<E>) {
+abstract class MultiNetworkProvider<E>(private val providers: List<E>) {
 
     private val providerIterator = CycleListIterator(providers)
     protected var provider = providerIterator.next()
