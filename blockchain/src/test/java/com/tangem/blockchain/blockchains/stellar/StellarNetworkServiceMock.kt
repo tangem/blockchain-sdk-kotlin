ebe12acdef53f499176e22cf1fb626c5aab0f1b0
Tangem <git@tangem.com>
2021-02-12 23:26:48 +0300

    Updated on 2025-08-28

--- blockchain/src/test/java/com/tangem/blockchain/blockchains/stellar/StellarNetworkServiceMock.kt
+++ blockchain/src/test/java/com/tangem/blockchain/blockchains/stellar/StellarNetworkServiceMock.kt
@@ -1,10 +1,13 @@
 package com.tangem.blockchain.blockchains.stellar
 
+import com.tangem.blockchain.common.Token
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.SimpleResult
 
 class StellarNetworkServiceMock(
-        private val isAccountCreatedResponse: Boolean = true
+        private val checkTargetAccountResult: Result<StellarTargetAccountResponse> = Result.Success(
+                StellarTargetAccountResponse(accountCreated = true, trustlineCreated = true)
+        )
 ) : StellarNetworkProvider {
     override suspend fun getInfo(accountId: String): Result<StellarResponse> {
         TODO("Not yet implemented")
@@ -12,7 +15,10 @@ class StellarNetworkServiceMock(
     override suspend fun sendTransaction(transaction: String): SimpleResult {
         TODO("Not yet implemented")
     }
-    override suspend fun checkIsAccountCreated(address: String) = isAccountCreatedResponse
+    override suspend fun checkTargetAccount(
+            address: String,
+            token: Token?
+    ): Result<StellarTargetAccountResponse> = checkTargetAccountResult
     override suspend fun getSignatureCount(accountId: String): Result<Int> {
         TODO("Not yet implemented")
     }
