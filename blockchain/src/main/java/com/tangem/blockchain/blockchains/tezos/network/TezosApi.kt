55a7953628004708f78de603d33fc81e500d3ded
Tangem <git@tangem.com>
2021-03-29 23:20:45 +0300

    Updated on 2026-06-27

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/tezos/network/TezosApi.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/tezos/network/TezosApi.kt
@@ -1,9 +1,7 @@
 package com.tangem.blockchain.blockchains.tezos.network
 
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
+import com.squareup.moshi.JsonClass
+import retrofit2.http.*
 
 interface TezosApi {
     @GET("chains/main/blocks/head/context/contracts/{address}")
@@ -15,21 +13,26 @@ interface TezosApi {
     @GET("chains/main/blocks/head/context/contracts/{address}/manager_key")
     suspend fun getManagerKey(@Path("address") address: String): String
 
+    @Headers("Content-Type: application/json")
     @POST("chains/main/blocks/head/helpers/forge/operations")
     suspend fun forgeOperations(@Body tezosForgeBody: TezosForgeBody): String
 
+    @Headers("Content-Type: application/json")
     @POST("chains/main/blocks/head/helpers/preapply/operations")
     suspend fun preapplyOperations(@Body tezosPreapplyBodyList: List<TezosPreapplyBody>)
 
+    @Headers("Content-Type: application/json")
     @POST("injection/operation")
     suspend fun sendTransaction(@Body transaction: String)
 }
 
+@JsonClass(generateAdapter = true)
 data class TezosForgeBody(
         val branch: String,
         val contents: List<TezosOperationContent>
 )
 
+@JsonClass(generateAdapter = true)
 data class TezosOperationContent(
         val kind: String,
         val source: String,
@@ -42,6 +45,7 @@ data class TezosOperationContent(
         val amount: String? = null
 )
 
+@JsonClass(generateAdapter = true)
 data class TezosPreapplyBody(
         val protocol: String,
         val branch: String,
