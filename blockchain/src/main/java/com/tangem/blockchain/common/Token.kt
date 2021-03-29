121c0e0f69bc26db51f33e04d4dbf3d062487df2
Tangem <git@tangem.com>
2021-03-29 12:05:20 +0300

    Updated on 2025-11-03

--- blockchain/src/main/java/com/tangem/blockchain/common/Token.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/Token.kt
@@ -1,5 +1,7 @@
 package com.tangem.blockchain.common
 
+import java.util.*
+
 data class Token(
         val name: String,
         val symbol: String,
@@ -8,4 +10,24 @@ data class Token(
 ) {
     constructor(symbol: String, contractAddress: String, decimals: Int) :
             this(symbol, symbol, contractAddress, decimals)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Token
+
+        if (symbol != other.symbol) return false
+        if (contractAddress.toLowerCase(Locale.ROOT) != other.contractAddress.toLowerCase(Locale.ROOT)) {
+            return false
+        }
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = symbol.hashCode()
+        result = 31 * result + contractAddress.toLowerCase(Locale.ROOT).hashCode()
+        return result
+    }
 }
