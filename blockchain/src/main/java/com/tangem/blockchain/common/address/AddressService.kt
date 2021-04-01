9b1dae8046ed6a8819e0f28636957c186737c855
Tangem <git@tangem.com>
2021-04-01 20:44:56 +0300

    Updated on 2025-08-28

--- blockchain/src/main/java/com/tangem/blockchain/common/address/AddressService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/address/AddressService.kt
@@ -1,12 +1,18 @@
 package com.tangem.blockchain.common.address
 
+import com.tangem.commands.common.card.EllipticCurve
+
 abstract class AddressService {
-    abstract fun makeAddress(walletPublicKey: ByteArray): String
+    abstract fun makeAddress(walletPublicKey: ByteArray, curve: EllipticCurve? = null): String
     abstract fun validate(address: String): Boolean
-    open fun makeAddresses(walletPublicKey: ByteArray): Set<Address> =
-            setOf(Address(makeAddress(walletPublicKey)))
+    open fun makeAddresses(walletPublicKey: ByteArray, curve: EllipticCurve? = null): Set<Address> =
+            setOf(Address(makeAddress(walletPublicKey, curve)))
 }
 
 interface MultisigAddressProvider {
-    fun makeMultisigAddresses(walletPublicKey: ByteArray, pairPublicKey: ByteArray): Set<Address>
+    fun makeMultisigAddresses(
+            walletPublicKey: ByteArray,
+            pairPublicKey: ByteArray,
+            curve: EllipticCurve? = null
+    ): Set<Address>
 }
