3c3b6ff326463a87cea05259f822b6a0436875e2
Tangem <git@tangem.com>
2021-04-02 21:48:25 +0300

    develop-1150

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/tezos/TezosConstants.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/tezos/TezosConstants.kt
@@ -1,6 +1,6 @@
 package com.tangem.blockchain.blockchains.tezos
 
-import com.tangem.common.extensions.hexToBytes
+import com.tangem.commands.common.card.EllipticCurve
 
 class TezosConstants {
     companion object {
@@ -13,6 +13,10 @@ class TezosConstants {
         const val SPPK_PREFIX = "03FEE256"
         const val P2PK_PREFIX = "03B28B7F"
 
+        const val EDSIG_PREFIX = "09F5CD8612"
+        const val SPSIG_PREFIX = "0D7365133F"
+        const val P2SIG_PREFIX = "36F02C34"
+
         const val BRANCH_PREFIX = "0134"
         const val REVEAL_OPERATION_KIND = "6b"
         const val TRANSACTION_OPERATION_KIND = "6c"
@@ -21,5 +25,20 @@ class TezosConstants {
         const val TRANSACTION_FEE = 0.00142
         const val REVEAL_FEE = 0.0013
         const val ALLOCATION_FEE = 0.06425
+
+        fun getAddressPrefix(curve: EllipticCurve) = when (curve) {
+            EllipticCurve.Ed25519 -> TZ1_PREFIX
+            EllipticCurve.Secp256k1 -> TZ2_PREFIX
+        }
+
+        fun getPublicKeyPrefix(curve: EllipticCurve) = when (curve) {
+            EllipticCurve.Ed25519 -> EDPK_PREFIX
+            EllipticCurve.Secp256k1 -> SPPK_PREFIX
+        }
+
+        fun getSignaturePrefix(curve: EllipticCurve) = when (curve) {
+            EllipticCurve.Ed25519 -> EDSIG_PREFIX
+            EllipticCurve.Secp256k1 -> SPSIG_PREFIX
+        }
     }
 }
