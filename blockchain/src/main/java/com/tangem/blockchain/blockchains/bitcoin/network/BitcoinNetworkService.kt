4ea4ff1d814d65440d9f304467519d26f9d14583
Tangem <git@tangem.com>
2021-04-26 00:53:48 +0300

    Updated on 2025-12-22

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoin/network/BitcoinNetworkService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoin/network/BitcoinNetworkService.kt
@@ -1,34 +1,45 @@
 package com.tangem.blockchain.blockchains.bitcoin.network
 
-import com.tangem.blockchain.blockchains.bitcoin.BitcoinUnspentOutput
-import com.tangem.blockchain.common.BasicTransactionData
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.SimpleResult
 import com.tangem.blockchain.network.MultiNetworkProvider
-import java.math.BigDecimal
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 
 
-class BitcoinNetworkService(providers: List<BitcoinNetworkProvider>) :
+class BitcoinNetworkService(private val providers: List<BitcoinNetworkProvider>) :
         MultiNetworkProvider<BitcoinNetworkProvider>(providers),
         BitcoinNetworkProvider {
 
     override suspend fun getInfo(address: String): Result<BitcoinAddressInfo> {
-        val result = provider.getInfo(address)
+        val result = currentProvider.getInfo(address)
         return if (result.needsRetry()) getInfo(address) else result
     }
 
     override suspend fun getFee(): Result<BitcoinFee> {
-        val result = provider.getFee()
-        return if (result.needsRetry()) getFee() else result
+        return coroutineScope {
+            val resultsDeferred = providers.map { async { it.getFee() } }
+            val results = resultsDeferred.map { it.await() }
+            val fees = results.filterIsInstance<Result.Success<BitcoinFee>>().map { it.data }
+            if (fees.isEmpty()) return@coroutineScope results.first()
+
+            return@coroutineScope Result.Success(
+                    BitcoinFee(
+                            minimalPerKb =  fees.map { it.minimalPerKb }.maxOrNull()!!,
+                            normalPerKb = fees.map { it.normalPerKb }.maxOrNull()!!,
+                            priorityPerKb = fees.map { it.priorityPerKb }.maxOrNull()!!
+                    )
+            )
+        }
     }
 
     override suspend fun sendTransaction(transaction: String): SimpleResult {
-        val result = provider.sendTransaction(transaction)
+        val result = currentProvider.sendTransaction(transaction)
         return if (result.needsRetry()) sendTransaction(transaction) else result
     }
 
     override suspend fun getSignatureCount(address: String): Result<Int> {
-        val result = provider.getSignatureCount(address)
+        val result = currentProvider.getSignatureCount(address)
         return if (result.needsRetry()) getSignatureCount(address) else result
     }
 }
