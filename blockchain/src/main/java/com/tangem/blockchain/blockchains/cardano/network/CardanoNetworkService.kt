2a6483dc3dc149d5659e3ff999b703e34ac5208c
Tangem <git@tangem.com>
2021-04-26 00:53:48 +0300

    Updated on 2026-01-28

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/network/CardanoNetworkService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/network/CardanoNetworkService.kt
@@ -1,6 +1,5 @@
 package com.tangem.blockchain.blockchains.cardano.network
 
-import com.tangem.blockchain.blockchains.cardano.CardanoUnspentOutput
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.SimpleResult
 import com.tangem.blockchain.network.MultiNetworkProvider
@@ -10,12 +9,12 @@ class CardanoNetworkService(providers: List<CardanoNetworkProvider>) :
         CardanoNetworkProvider {
 
     override suspend fun getInfo(addresses: Set<String>): Result<CardanoAddressResponse> {
-        val result = provider.getInfo(addresses)
+        val result = currentProvider.getInfo(addresses)
         return if (result.needsRetry()) getInfo(addresses) else result
     }
 
     override suspend fun sendTransaction(transaction: ByteArray): SimpleResult {
-        val result = provider.sendTransaction(transaction)
+        val result = currentProvider.sendTransaction(transaction)
         return if (result.needsRetry()) sendTransaction(transaction) else result
     }
 }
