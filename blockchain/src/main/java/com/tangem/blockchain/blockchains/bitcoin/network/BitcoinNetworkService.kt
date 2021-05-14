74779dd6caeb0ab15eee05e4eb3a9c71d191cbea
Tangem <git@tangem.com>
2021-05-14 13:39:59 +0300

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoin/network/BitcoinNetworkService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoin/network/BitcoinNetworkService.kt
@@ -1,10 +1,13 @@
 package com.tangem.blockchain.blockchains.bitcoin.network
 
+import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.SimpleResult
 import com.tangem.blockchain.network.MultiNetworkProvider
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 
 open class BitcoinNetworkService(private val providers: List<BitcoinNetworkProvider>) :
@@ -23,13 +26,30 @@ open class BitcoinNetworkService(private val providers: List<BitcoinNetworkProvi
             val fees = results.filterIsInstance<Result.Success<BitcoinFee>>().map { it.data }
             if (fees.isEmpty()) return@coroutineScope results.first()
 
-            return@coroutineScope Result.Success(
-                    BitcoinFee(
-                            minimalPerKb =  fees.map { it.minimalPerKb }.maxOrNull()!!,
-                            normalPerKb = fees.map { it.normalPerKb }.maxOrNull()!!,
-                            priorityPerKb = fees.map { it.priorityPerKb }.maxOrNull()!!
-                    )
-            )
+            val bicoinFee =  if (fees.size > 2) {
+                val sortedMinimalFees = fees.map { it.minimalPerKb }.sorted().toMutableList()
+                val sortedNormalFees = fees.map { it.normalPerKb }.sorted().toMutableList()
+                val sortedPriorityFees = fees.map { it.priorityPerKb }.sorted().toMutableList()
+
+                sortedMinimalFees.removeFirst()
+                sortedNormalFees.removeFirst()
+                sortedPriorityFees.removeFirst()
+
+                BitcoinFee(
+                        minimalPerKb = sortedMinimalFees.average(),
+                        normalPerKb = sortedNormalFees.average(),
+                        priorityPerKb = sortedPriorityFees.average()
+
+                )
+            } else {
+                BitcoinFee(
+                        minimalPerKb = fees.map { it.minimalPerKb }.maxOrNull()!!,
+                        normalPerKb = fees.map { it.normalPerKb }.maxOrNull()!!,
+                        priorityPerKb = fees.map { it.priorityPerKb }.maxOrNull()!!
+                )
+            }
+
+            Result.Success(bicoinFee)
         }
     }
 
@@ -42,4 +62,8 @@ open class BitcoinNetworkService(private val providers: List<BitcoinNetworkProvi
         val result = currentProvider.getSignatureCount(address)
         return if (result.needsRetry()) getSignatureCount(address) else result
     }
+
+    private fun List<BigDecimal>.average(): BigDecimal =
+            this.reduce { acc, number -> acc + number }.divide(this.size.toBigDecimal())
+                    .setScale(Blockchain.Bitcoin.decimals(), RoundingMode.HALF_UP)
 }
