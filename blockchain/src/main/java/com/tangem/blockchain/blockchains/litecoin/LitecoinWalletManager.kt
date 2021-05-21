de48d73b1c461215a98b8f7160b8c6a9e3616190
Tangem <git@tangem.com>
2021-05-21 14:06:43 +0300

    Updated on 2026-03-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/litecoin/LitecoinWalletManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/litecoin/LitecoinWalletManager.kt
@@ -3,10 +3,10 @@ package com.tangem.blockchain.blockchains.litecoin
 import com.tangem.blockchain.blockchains.bitcoin.BitcoinTransactionBuilder
 import com.tangem.blockchain.blockchains.bitcoin.BitcoinWalletManager
 import com.tangem.blockchain.blockchains.bitcoin.network.BitcoinNetworkProvider
-import com.tangem.blockchain.common.Amount
-import com.tangem.blockchain.common.TransactionSender
-import com.tangem.blockchain.common.Wallet
+import com.tangem.blockchain.common.*
 import com.tangem.blockchain.extensions.Result
+import com.tangem.blockchain.extensions.SimpleResult
+import org.bitcoinj.core.TransactionInput.NO_SEQUENCE
 import java.math.BigDecimal
 
 class LitecoinWalletManager(
@@ -15,4 +15,9 @@ class LitecoinWalletManager(
         networkProvider: BitcoinNetworkProvider
 ) : BitcoinWalletManager(wallet, transactionBuilder, networkProvider), TransactionSender {
     override val minimalFeePerKb = DEFAULT_MINIMAL_FEE_PER_KB.toBigDecimal()
+
+    override suspend fun send(transactionData: TransactionData, signer: TransactionSigner) =
+            send(transactionData, signer, NO_SEQUENCE)
+
+    override suspend fun isPushAvailable(transactionHash: String) = Result.Success(false)
 }
