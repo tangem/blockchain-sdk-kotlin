d66ae55af295b8e25b1e71611dc8b4f26e6ba867
Tangem <git@tangem.com>
2021-06-07 15:07:19 +0300

    develop-1150

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/EthereumTransactionExtras.kt
@@ -0,0 +1,10 @@
+package com.tangem.blockchain.blockchains.ethereum
+
+import com.tangem.blockchain.common.TransactionExtras
+import java.math.BigInteger
+
+class EthereumTransactionExtras(
+    val data: ByteArray,
+    val gasLimit: BigInteger? = null,
+    val nonce: BigInteger? = null
+) : TransactionExtras
