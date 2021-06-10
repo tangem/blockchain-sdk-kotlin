d890302d3bc8bf0c03183486cf93e276aeab80c1
Tangem <git@tangem.com>
2021-06-10 22:44:09 +0300

    Updated on 2026-05-04

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/stellar/StellarNetworkService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/stellar/StellarNetworkService.kt
@@ -18,6 +18,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class StellarNetworkService : StellarNetworkProvider {
+    override val host: String = API_STELLAR
     private val blockchain = Blockchain.Stellar
     private val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ROOT)
     private val recordsLimitCap = 200
