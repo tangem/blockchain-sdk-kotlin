478ab4ed6aa8a3de9c3a372955e4de182db4ae7e
Tangem <git@tangem.com>
2021-06-23 00:09:58 +0300

    Updated on 2025-08-28

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/network/EthereumNetworkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/network/EthereumNetworkProvider.kt
@@ -12,18 +12,19 @@ interface EthereumNetworkProvider {
     suspend fun getInfo(address: String, tokens: Set<Token>): Result<EthereumInfoResponse>
     suspend fun sendTransaction(transaction: String): SimpleResult
     suspend fun getSignatureCount(address: String): Result<Int>
-    suspend fun getTokensBalance(address: String, tokens: Set<Token>): Result<Map<Token, BigDecimal>>
     suspend fun findErc20Tokens(address: String): Result<List<BlockchairToken>>
     suspend fun getGasPrice(): Result<Long>
-    suspend fun getGasLimit(
-        to: String, from: String, data: String?,fallbackGasLimit: Long?,
-    ): Result<Long>
+    suspend fun getGasLimit(to: String, from: String, data: String?): Result<Long>
+    suspend fun getTokensBalance(
+        address: String,
+        tokens: Set<Token>
+    ): Result<Map<Token, BigDecimal>>
 }
 
 data class EthereumInfoResponse(
-        val coinBalance: BigDecimal,
-        val tokenBalances: Map<Token, BigDecimal>,
-        val txCount: Long,
-        val pendingTxCount: Long,
-        val recentTransactions: List<TransactionData>?
+    val coinBalance: BigDecimal,
+    val tokenBalances: Map<Token, BigDecimal>,
+    val txCount: Long,
+    val pendingTxCount: Long,
+    val recentTransactions: List<TransactionData>?
 )
