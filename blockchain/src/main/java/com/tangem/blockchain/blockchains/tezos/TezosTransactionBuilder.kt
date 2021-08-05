d7da2b36fa26388386e3d188615b043f17809d9a
Tangem <git@tangem.com>
2021-08-05 16:18:07 +0300

    Updated on 2026-05-04

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/tezos/TezosTransactionBuilder.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/tezos/TezosTransactionBuilder.kt
@@ -6,7 +6,7 @@ import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.TransactionData
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.bigIntegerValue
-import com.tangem.commands.common.card.EllipticCurve
+import com.tangem.common.card.EllipticCurve
 import com.tangem.common.extensions.hexToBytes
 import com.tangem.common.extensions.toCompressedPublicKey
 import com.tangem.common.extensions.toHexString
