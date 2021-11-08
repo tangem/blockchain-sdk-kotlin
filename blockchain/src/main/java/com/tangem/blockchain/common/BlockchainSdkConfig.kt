85b1a226c9b9290ccca9e21d430faa4fba858901
Tangem <git@tangem.com>
2021-11-08 15:13:01 +0300

    Updated on 2025-08-28

--- blockchain/src/main/java/com/tangem/blockchain/common/BlockchainSdkConfig.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/BlockchainSdkConfig.kt
@@ -1,6 +1,7 @@
 package com.tangem.blockchain.common
 
 data class BlockchainSdkConfig (
+    val blockchairApiKey: String? = null,
     val blockchairAuthorizationToken: String? = null,
     val blockcypherTokens: Set<String>? = null,
     val infuraProjectId: String? = null
