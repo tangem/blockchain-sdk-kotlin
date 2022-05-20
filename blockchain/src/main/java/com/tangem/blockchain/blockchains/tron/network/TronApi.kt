208b6ddad7553469999203a9139d4c80ca8773e3
Tangem <git@tangem.com>
2022-05-20 17:12:42 +0400

    Updated on 2026-02-11

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/tron/network/TronApi.kt
@@ -0,0 +1,50 @@
+package com.tangem.blockchain.blockchains.tron.network
+
+import retrofit2.http.*
+
+interface TronApi {
+
+    @Headers("Content-Type: application/json", "Accept: application/json")
+    @POST("/wallet/getaccount")
+    suspend fun getAccount(
+        @Body requestBody: TronGetAccountRequest,
+    ): TronGetAccountResponse
+
+    @Headers("Content-Type: application/json", "Accept: application/json")
+    @POST("/wallet/getaccountresource")
+    suspend fun getAccountResource(
+        @Body requestBody: TronGetAccountRequest,
+    ): TronGetAccountResourceResponse
+
+    @Headers("Content-Type: application/json", "Accept: application/json")
+    @POST("/wallet/getnowblock")
+    suspend fun getNowBlock(): TronBlock
+
+    @Headers("Accept: application/json")
+    @POST("/wallet/broadcasthex")
+    suspend fun broadcastHex(
+        @Body requestBody: TronBroadcastRequest,
+    ): TronBroadcastResponse
+
+    @Headers("Content-Type: application/json", "Accept: application/json")
+    @POST("/wallet/triggersmartcontract")
+    suspend fun getTokenBalance(
+        @Body requestBody: TronTriggerSmartContractRequest,
+    ): TronTriggerSmartContractResponse
+
+    @Headers("Content-Type: application/json", "Accept: application/json")
+    @GET("/v1/contracts/{contractAddress}/transactions")
+    suspend fun getTokenTransactionHistory(
+        @Path("contractAddress") contractAddress: String,
+        @Query("only_confirmed") onlyConfirmed: Boolean = true,
+        @Query("limit") limit: Int = 50
+    ): TronTokenHistoryResponse
+
+    @Headers("Content-Type: application/json", "Accept: application/json")
+    @POST("/walletsolidity/gettransactioninfobyid")
+    suspend fun getTransactionInfoById(
+        @Body requestBody: TronTransactionInfoRequest,
+    ): TronTransactionInfoResponse
+
+
+}
