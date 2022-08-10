881f8aec93bc290446d2808de53a66e92b90ebf1
Tangem <git@tangem.com>
2022-08-10 22:17:34 +0300

    Updated on 2026-02-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/tezos/network/TezosJsonRpcNetworkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/tezos/network/TezosJsonRpcNetworkProvider.kt
@@ -1,7 +1,7 @@
 package com.tangem.blockchain.blockchains.tezos.network
 
 import com.tangem.blockchain.common.Blockchain
-import com.tangem.blockchain.common.toBlockchainCustomError
+import com.tangem.blockchain.common.toBlockchainSdkError
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.SimpleResult
 import com.tangem.blockchain.extensions.retryIO
@@ -26,7 +26,7 @@ class TezosJsonRpcNetworkProvider(baseUrl: String) : TezosNetworkProvider {
                 )
             )
         } catch (exception: Exception) {
-            Result.Failure(exception.toBlockchainCustomError())
+            Result.Failure(exception.toBlockchainSdkError())
         }
     }
 
@@ -49,7 +49,7 @@ class TezosJsonRpcNetworkProvider(baseUrl: String) : TezosNetworkProvider {
                 )
             )
         } catch (exception: Exception) {
-            Result.Failure(exception.toBlockchainCustomError())
+            Result.Failure(exception.toBlockchainSdkError())
         }
     }
 
@@ -60,7 +60,7 @@ class TezosJsonRpcNetworkProvider(baseUrl: String) : TezosNetworkProvider {
             }
             Result.Success(forgedContents)
         } catch (exception: Exception) {
-            Result.Failure(exception.toBlockchainCustomError())
+            Result.Failure(exception.toBlockchainSdkError())
         }
     }
 
@@ -75,7 +75,7 @@ class TezosJsonRpcNetworkProvider(baseUrl: String) : TezosNetworkProvider {
             retryIO { api.preapplyOperations(listOf(tezosPreapplyBody)) }
             SimpleResult.Success
         } catch (exception: Exception) {
-            SimpleResult.Failure(exception.toBlockchainCustomError())
+            SimpleResult.Failure(exception.toBlockchainSdkError())
         }
     }
 
@@ -84,7 +84,7 @@ class TezosJsonRpcNetworkProvider(baseUrl: String) : TezosNetworkProvider {
             retryIO { api.sendTransaction(transaction) }
             SimpleResult.Success
         } catch (exception: Exception) {
-            SimpleResult.Failure(exception.toBlockchainCustomError())
+            SimpleResult.Failure(exception.toBlockchainSdkError())
         }
     }
 }
