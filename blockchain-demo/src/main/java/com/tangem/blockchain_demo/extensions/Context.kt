e3aa4a59512de80771ef69179b0e0a2a763fc7e3
Tangem <git@tangem.com>
2022-08-12 13:31:16 +0300

    Updated on 2026-06-27

---
+++ blockchain-demo/src/main/java/com/tangem/blockchain_demo/extensions/Context.kt
@@ -0,0 +1,24 @@
+package com.tangem.blockchain_demo.extensions
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+
+/**
+[REDACTED_AUTHOR]
+ */
+fun Context.copyToClipboard(value: Any, label: String = "") {
+    val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager ?: return
+
+    val clip: ClipData = ClipData.newPlainText(label, value.toString())
+    clipboard.setPrimaryClip(clip)
+}
+
+fun Context.getFromClipboard(default: CharSequence? = null): CharSequence? {
+    val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager
+            ?: return default
+    val clipData = clipboard.primaryClip ?: return default
+    if (clipData.itemCount == 0) return default
+
+    return clipData.getItemAt(0).text
+}
