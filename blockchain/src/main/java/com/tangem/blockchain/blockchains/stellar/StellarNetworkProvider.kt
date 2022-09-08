d1848c39eaafcec044243d11c27a3919c14305a5
Tangem <git@tangem.com>
2022-09-08 18:35:29 +0300

    Updated on 2026-01-28

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/stellar/StellarNetworkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/stellar/StellarNetworkProvider.kt
@@ -4,36 +4,35 @@ import com.tangem.blockchain.common.Token
 import com.tangem.blockchain.common.TransactionData
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.SimpleResult
+import org.stellar.sdk.responses.FeeStatsResponse
 import java.math.BigDecimal
 
 interface StellarNetworkProvider {
     val host: String
     suspend fun getInfo(accountId: String): Result<StellarResponse>
+    suspend fun getFeeStats(): Result<FeeStatsResponse>
     suspend fun sendTransaction(transaction: String): SimpleResult
-    suspend fun checkTargetAccount(
-            address: String,
-            token: Token?
-    ): Result<StellarTargetAccountResponse>
+    suspend fun checkTargetAccount(address: String, token: Token?): Result<StellarTargetAccountResponse>
     suspend fun getSignatureCount(accountId: String): Result<Int>
 }
 
 data class StellarResponse(
-        val coinBalance: BigDecimal,
-        val tokenBalances: Set<StellarAssetBalance>,
-        val baseFee: BigDecimal,
-        val baseReserve: BigDecimal,
-        val sequence: Long,
-        val recentTransactions: List<TransactionData>,
-        val subEntryCount: Int
+    val coinBalance: BigDecimal,
+    val tokenBalances: Set<StellarAssetBalance>,
+    val baseFee: BigDecimal,
+    val baseReserve: BigDecimal,
+    val sequence: Long,
+    val recentTransactions: List<TransactionData>,
+    val subEntryCount: Int
 )
 
 data class StellarAssetBalance(
-        val balance: BigDecimal,
-        val symbol: String,
-        val issuer: String
+    val balance: BigDecimal,
+    val symbol: String,
+    val issuer: String
 )
 
 data class StellarTargetAccountResponse(
-        val accountCreated: Boolean,
-        val trustlineCreated: Boolean? = null
+    val accountCreated: Boolean,
+    val trustlineCreated: Boolean? = null
 )
