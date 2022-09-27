0a82bd16eba7f2e7ef3294a02db303f5359ac3fd
Tangem <git@tangem.com>
2022-09-27 11:24:41 +0300

    Updated on 2025-10-09

--- blockchain-demo/src/main/java/com/tangem/blockchain_demo/extensions/Context.kt
+++ blockchain-demo/src/main/java/com/tangem/blockchain_demo/extensions/Context.kt
@@ -3,6 +3,7 @@ package com.tangem.blockchain_demo.extensions
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.widget.TextView
 
 /**
 [REDACTED_AUTHOR]
@@ -12,6 +13,10 @@ fun Context.copyToClipboard(value: Any, label: String = "") {
 
     val clip: ClipData = ClipData.newPlainText(label, value.toString())
     clipboard.setPrimaryClip(clip)
+}
+
+fun TextView.copyTextToClipboard(label: String = "") {
+    this.context.copyToClipboard(this.text.toString(), label)
 }
 
 fun Context.getFromClipboard(default: CharSequence? = null): CharSequence? {
