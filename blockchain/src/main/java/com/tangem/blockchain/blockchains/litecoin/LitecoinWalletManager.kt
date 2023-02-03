b2fa2df0d5cede7e257d87f5dc98a19c7312b19b
Tangem <git@tangem.com>
2023-02-03 21:31:53 +0300

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/litecoin/LitecoinWalletManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/litecoin/LitecoinWalletManager.kt
@@ -3,16 +3,14 @@ package com.tangem.blockchain.blockchains.litecoin
 import com.tangem.blockchain.blockchains.bitcoin.BitcoinTransactionBuilder
 import com.tangem.blockchain.blockchains.bitcoin.BitcoinWalletManager
 import com.tangem.blockchain.blockchains.bitcoin.network.BitcoinNetworkProvider
-import com.tangem.blockchain.common.Amount
 import com.tangem.blockchain.common.TransactionSender
 import com.tangem.blockchain.common.Wallet
-import com.tangem.blockchain.extensions.Result
-import java.math.BigDecimal
 
 class LitecoinWalletManager(
-        wallet: Wallet,
-        transactionBuilder: BitcoinTransactionBuilder,
-        networkProvider: BitcoinNetworkProvider
+    wallet: Wallet,
+    transactionBuilder: BitcoinTransactionBuilder,
+    networkProvider: BitcoinNetworkProvider
 ) : BitcoinWalletManager(wallet, transactionBuilder, networkProvider), TransactionSender {
-    override val minimalFeePerKb = DEFAULT_MINIMAL_FEE_PER_KB.toBigDecimal()
+
+    override val minimalFee = 0.00001.toBigDecimal()
 }
