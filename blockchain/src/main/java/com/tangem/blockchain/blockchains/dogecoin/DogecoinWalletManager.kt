a83f148e81f7d5f2a8c4e69608b960ee5c9c404d
Tangem <git@tangem.com>
2023-02-03 21:31:53 +0300

    releases/5.27.0-1144

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/dogecoin/DogecoinWalletManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/dogecoin/DogecoinWalletManager.kt
@@ -3,17 +3,16 @@ package com.tangem.blockchain.blockchains.dogecoin
 import com.tangem.blockchain.blockchains.bitcoin.BitcoinTransactionBuilder
 import com.tangem.blockchain.blockchains.bitcoin.BitcoinWalletManager
 import com.tangem.blockchain.blockchains.bitcoin.network.BitcoinNetworkProvider
-import com.tangem.blockchain.common.Amount
 import com.tangem.blockchain.common.TransactionSender
 import com.tangem.blockchain.common.Wallet
-import com.tangem.blockchain.extensions.Result
 import java.math.BigDecimal
 
 class DogecoinWalletManager(
-        wallet: Wallet,
-        transactionBuilder: BitcoinTransactionBuilder,
-        networkProvider: BitcoinNetworkProvider
+    wallet: Wallet,
+    transactionBuilder: BitcoinTransactionBuilder,
+    networkProvider: BitcoinNetworkProvider
 ) : BitcoinWalletManager(wallet, transactionBuilder, networkProvider), TransactionSender {
+
     override val minimalFeePerKb: BigDecimal = BigDecimal.ONE
     override val minimalFee: BigDecimal = BigDecimal.ONE
     override val dustValue: BigDecimal = BigDecimal.ONE
