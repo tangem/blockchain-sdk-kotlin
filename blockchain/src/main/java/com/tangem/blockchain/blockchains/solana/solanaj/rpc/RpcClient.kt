2982a8443e2e5463cba0c5e8947c57a50e88e46f
Tangem <git@tangem.com>
2023-02-06 18:13:04 +0300

    Updated on 2026-03-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/solana/solanaj/rpc/RpcClient.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/solana/solanaj/rpc/RpcClient.kt
@@ -1,21 +1,17 @@
 package com.tangem.blockchain.blockchains.solana.solanaj.rpc
 
-import org.p2p.solanaj.rpc.Cluster
+import okhttp3.Interceptor
 import org.p2p.solanaj.rpc.RpcClient
 
 /**
 [REDACTED_AUTHOR]
  */
-class RpcClient(val host: String) : RpcClient(host, null) {
+class RpcClient(
+    val host: String,
+    httpInterceptors: List<Interceptor>? = null,
+) : RpcClient(host, httpInterceptors) {
 
     override fun createRpcApi(): RpcApi = RpcApi(this)
 
     override fun getApi(): RpcApi = super.getApi() as RpcApi
-
-    companion object {
-        fun devNet(): com.tangem.blockchain.blockchains.solana.solanaj.rpc.RpcClient =
-            com.tangem.blockchain.blockchains.solana.solanaj.rpc.RpcClient(Cluster.DEVNET.endpoint)
-        fun testNet(): com.tangem.blockchain.blockchains.solana.solanaj.rpc.RpcClient =
-            com.tangem.blockchain.blockchains.solana.solanaj.rpc.RpcClient(Cluster.TESTNET.endpoint)
-    }
 }
