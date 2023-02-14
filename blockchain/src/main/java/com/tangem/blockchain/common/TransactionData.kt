ac7089326b9d30afe0d9df6309b8be1592b814dd
Tangem <git@tangem.com>
2023-02-14 16:10:29 +0300

    Updated on 2026-06-27

--- blockchain/src/main/java/com/tangem/blockchain/common/TransactionData.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/TransactionData.kt
@@ -1,20 +1,20 @@
 package com.tangem.blockchain.common
 
 import java.math.BigDecimal
-import java.util.*
+import java.util.Calendar
 
 data class TransactionData(
-        val amount: Amount,
-        val fee: Amount?,
-        val sourceAddress: String,
-        val destinationAddress: String,
-        val contractAddress: String? = null, // TODO: remove this field, contract address is located in amount type
-        var status: TransactionStatus = TransactionStatus.Unconfirmed,
-        var date: Calendar? = null,
-        var hash: String? = null,
-        val extras: TransactionExtras? = null
-)
-
+    val amount: Amount,
+    val fee: Amount?,
+    val sourceAddress: String,
+    val destinationAddress: String,
+    var status: TransactionStatus = TransactionStatus.Unconfirmed,
+    var date: Calendar? = null,
+    var hash: String? = null,
+    val extras: TransactionExtras? = null,
+) {
+    val contractAddress: String? = (amount.type as? AmountType.Token)?.token?.contractAddress
+}
 
 enum class TransactionStatus { Confirmed, Unconfirmed }
 
@@ -33,10 +33,10 @@ enum class TransactionError { // TODO: add address validation?
 enum class TransactionDirection { Incoming, Outgoing }
 
 data class BasicTransactionData(
-        val balanceDif: BigDecimal, //change of balance
-        val hash: String,
-        val date: Calendar?,
-        val isConfirmed: Boolean
+    val balanceDif: BigDecimal, //change of balance
+    val hash: String,
+    val date: Calendar?,
+    val isConfirmed: Boolean,
 )
 
 interface TransactionExtras
