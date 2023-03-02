eae887526e6eb01da4d2d0cae5b8858a6e677dc7
Tangem <git@tangem.com>
2023-03-02 15:14:05 +0300

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/common/CommonSigner.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/CommonSigner.kt
@@ -9,7 +9,7 @@ import kotlin.coroutines.resume
 open class CommonSigner(
     private val tangemSdk: TangemSdk,
     var cardId: String? = null,
-    var initialMessage: Message? = null
+    var initialMessage: Message? = null,
 ) : TransactionSigner {
 
     override suspend fun sign(hashes: List<ByteArray>, publicKey: Wallet.PublicKey): CompletionResult<List<ByteArray>> =
@@ -22,8 +22,14 @@ open class CommonSigner(
                 initialMessage = initialMessage,
             ) { result ->
                 when (result) {
-                    is CompletionResult.Success -> continuation.resume(CompletionResult.Success(result.data.signatures))
-                    is CompletionResult.Failure -> continuation.resume(CompletionResult.Failure(result.error))
+                    is CompletionResult.Success ->
+                        if (continuation.isActive) {
+                            continuation.resume(CompletionResult.Success(result.data.signatures))
+                        }
+                    is CompletionResult.Failure ->
+                        if (continuation.isActive) {
+                            continuation.resume(CompletionResult.Failure(result.error))
+                        }
                 }
             }
         }
@@ -38,8 +44,14 @@ open class CommonSigner(
                 initialMessage = initialMessage,
             ) { result ->
                 when (result) {
-                    is CompletionResult.Success -> continuation.resume(CompletionResult.Success(result.data.signature))
-                    is CompletionResult.Failure -> continuation.resume(CompletionResult.Failure(result.error))
+                    is CompletionResult.Success ->
+                        if (continuation.isActive) {
+                            continuation.resume(CompletionResult.Success(result.data.signature))
+                        }
+                    is CompletionResult.Failure ->
+                        if (continuation.isActive) {
+                            continuation.resume(CompletionResult.Failure(result.error))
+                        }
                 }
             }
         }
