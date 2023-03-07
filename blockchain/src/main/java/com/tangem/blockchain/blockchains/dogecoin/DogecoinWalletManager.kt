efac67c5676a6bb36672a4511616932b65cecffa
Tangem <git@tangem.com>
2023-03-07 08:23:10 +0800

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/dogecoin/DogecoinWalletManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/dogecoin/DogecoinWalletManager.kt
@@ -2,18 +2,32 @@ package com.tangem.blockchain.blockchains.dogecoin
 
 import com.tangem.blockchain.blockchains.bitcoin.BitcoinTransactionBuilder
 import com.tangem.blockchain.blockchains.bitcoin.BitcoinWalletManager
+import com.tangem.blockchain.blockchains.bitcoin.network.BitcoinFee
 import com.tangem.blockchain.blockchains.bitcoin.network.BitcoinNetworkProvider
 import com.tangem.blockchain.common.TransactionSender
 import com.tangem.blockchain.common.Wallet
+import com.tangem.blockchain.extensions.Result
 import java.math.BigDecimal
 
 class DogecoinWalletManager(
     wallet: Wallet,
     transactionBuilder: BitcoinTransactionBuilder,
-    networkProvider: BitcoinNetworkProvider
+    networkProvider: BitcoinNetworkProvider,
 ) : BitcoinWalletManager(wallet, transactionBuilder, networkProvider), TransactionSender {
 
-    override val minimalFeePerKb: BigDecimal = BigDecimal.ONE
-    override val minimalFee: BigDecimal = BigDecimal.ONE
-    override val dustValue: BigDecimal = BigDecimal.ONE
+    override suspend fun getBitcoinFeePerKb(): Result<BitcoinFee> {
+        return Result.Success(
+            BitcoinFee(
+                minimalPerKb = BigDecimal(MINIMAL_FEE_PER_KB),
+                normalPerKb = BigDecimal(NORMAL_FEE_PER_KB),
+                priorityPerKb = BigDecimal(PRIORITY_FEE_PER_KB)
+            )
+        )
+    }
+
+    private companion object {
+        const val MINIMAL_FEE_PER_KB = 0.01
+        const val NORMAL_FEE_PER_KB = MINIMAL_FEE_PER_KB * 10
+        const val PRIORITY_FEE_PER_KB = MINIMAL_FEE_PER_KB * 100
+    }
 }
