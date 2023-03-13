3e82d659101f965147a2c0fc08bf76a5639c67dd
Tangem <git@tangem.com>
2023-03-13 18:04:28 +0400

    releases/5.27.0-1144

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/kaspa/network/KaspaResponse.kt
@@ -0,0 +1,38 @@
+package com.tangem.blockchain.blockchains.kaspa.network
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class KaspaBalanceResponse(
+        var balance: Long? = null,
+)
+
+@JsonClass(generateAdapter = true)
+data class KaspaUnspentOutputResponse(
+        var outpoint: KaspaOutpoint? = null,
+        var utxoEntry: KaspaUtxoEntry? = null
+)
+
+@JsonClass(generateAdapter = true)
+data class KaspaSendTransactionResponse(
+        var transactionId: String? = null,
+        var error: String? = null
+)
+
+@JsonClass(generateAdapter = true)
+data class KaspaOutpoint(
+        var transactionId: String? = null,
+        var index: Long? = null
+)
+
+@JsonClass(generateAdapter = true)
+data class KaspaUtxoEntry(
+        var amount: String? = null,
+        var scriptPublicKey: KaspaScriptPublicKeyResponse? = null
+)
+
+@JsonClass(generateAdapter = true)
+data class KaspaScriptPublicKeyResponse(
+        var scriptPublicKey: String? = null
+)
