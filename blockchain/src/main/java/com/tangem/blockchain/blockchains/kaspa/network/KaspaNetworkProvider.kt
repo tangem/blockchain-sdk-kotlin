ac0fe4e05ac136c64f74675d3bc9028a010e44e2
Tangem <git@tangem.com>
2023-03-13 18:04:28 +0400

    Updated on 2026-02-02

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/kaspa/network/KaspaNetworkProvider.kt
@@ -0,0 +1,23 @@
+package com.tangem.blockchain.blockchains.kaspa.network
+
+import com.tangem.blockchain.extensions.Result
+import com.tangem.blockchain.extensions.SimpleResult
+import java.math.BigDecimal
+
+interface KaspaNetworkProvider {
+    val host: String
+    suspend fun getInfo(address: String): Result<KaspaInfoResponse>
+    suspend fun sendTransaction(transaction: KaspaTransactionBody): SimpleResult
+}
+
+data class KaspaInfoResponse(
+    val balance: BigDecimal,
+    val unspentOutputs: List<KaspaUnspentOutput>
+)
+
+class KaspaUnspentOutput(
+    val amount: BigDecimal,
+    val outputIndex: Long,
+    val transactionHash: ByteArray,
+    val outputScript: ByteArray
+)
