12230aebd423353219b3794cbe169d66193cab41
Tangem <git@tangem.com>
2023-03-22 19:22:07 +0800

    Updated on 2025-11-28

--- blockchain/src/main/java/com/tangem/blockchain/network/blockbook/network/responses/GetAddressResponse.kt
+++ blockchain/src/main/java/com/tangem/blockchain/network/blockbook/network/responses/GetAddressResponse.kt
@@ -1,51 +1,20 @@
 package com.tangem.blockchain.network.blockbook.network.responses
 
 data class GetAddressResponse(
-    val address: String,
     val balance: String,
-    val transactions: List<Transaction>?,
+    val unconfirmedTxs: Int,
     val txs: Int,
-    val unconfirmedBalance: String,
-    val unconfirmedTxs: Int
+    val transactions: List<Transaction>?,
 ) {
 
     data class Transaction(
-        val blockHash: String?,
-        val blockHeight: Int,
-        val blockTime: Int,
-        val confirmations: Int,
-        val fees: String,
-        val hex: String,
-        val lockTime: Int,
-        val size: Int,
         val txid: String,
-        val value: String,
-        val valueIn: String,
-        val version: Int,
-        val vin: List<Vin>,
         val vout: List<Vout>,
-        val vsize: Int
+        val confirmations: Int,
+        val blockTime: Int,
+        val value: String,
     ) {
 
-        data class Vin(
-            val addresses: List<String>,
-            val isAddress: Boolean,
-            val isOwn: Boolean?,
-            val n: Int,
-            val sequence: Long?,
-            val txid: String,
-            val value: String?,
-            val vout: Int?
-        )
-
-        data class Vout(
-            val addresses: List<String>,
-            val hex: String,
-            val isAddress: Boolean,
-            val isOwn: Boolean?,
-            val n: Int,
-            val spent: Boolean?,
-            val value: String
-        )
+        data class Vout(val hex: String, val addresses: List<String>)
     }
 }
