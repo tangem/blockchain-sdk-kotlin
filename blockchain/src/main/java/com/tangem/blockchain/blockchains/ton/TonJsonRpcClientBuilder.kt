8da9c488d7b82bb736d0e9efd392e81af8d2b7a4
Tangem <git@tangem.com>
2023-03-30 16:51:28 +0500

    releases/5.27.0-1144

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ton/TonJsonRpcClientBuilder.kt
@@ -0,0 +1,48 @@
+package com.tangem.blockchain.blockchains.ton
+
+import com.tangem.blockchain.blockchains.ton.network.TonApi
+import com.tangem.blockchain.blockchains.ton.network.TonJsonRpcNetworkProvider
+import com.tangem.blockchain.common.BlockchainSdkConfig
+import com.tangem.blockchain.extensions.AddHeaderInterceptor
+import com.tangem.blockchain.extensions.letNotBlank
+import com.tangem.blockchain.network.createRetrofitInstance
+
+class TonJsonRpcClientBuilder {
+
+    fun build(isTestNet: Boolean, blockchainSdkConfig: BlockchainSdkConfig): List<TonJsonRpcNetworkProvider> {
+        return buildList {
+            blockchainSdkConfig.tonCenterCredentials?.getApiKey(isTestNet).letNotBlank {
+                add(createTonCenterJsonRpcProvider(isTestNet = isTestNet, apiKey = it))
+            }
+
+            if (!isTestNet) {
+                blockchainSdkConfig.nowNodeCredentials?.apiKey.letNotBlank { add(createNowNodeJsonRpcProvider(it)) }
+                blockchainSdkConfig.getBlockCredentials?.apiKey.letNotBlank { add(createGetBlockJsonRpcProvider(it)) }
+            }
+        }
+    }
+
+    private fun createTonCenterJsonRpcProvider(
+        isTestNet: Boolean,
+        apiKey: String,
+    ): TonJsonRpcNetworkProvider {
+        val url = if (isTestNet) "https://testnet.toncenter.com/api/v2/" else "https://toncenter.com/api/v2/"
+        val tonApi = createRetrofitInstance(
+            baseUrl = url,
+            headerInterceptors = listOf(AddHeaderInterceptor(mapOf(BlockchainSdkConfig.X_API_KEY_HEADER to apiKey)))
+        ).create(TonApi::class.java)
+        return TonJsonRpcNetworkProvider(host = url, api = tonApi)
+    }
+
+    private fun createGetBlockJsonRpcProvider(apiKey: String): TonJsonRpcNetworkProvider {
+        val url = "https://ton.getblock.io/"
+        val tonApi = createRetrofitInstance("$url${apiKey}/mainnet/").create(TonApi::class.java)
+        return TonJsonRpcNetworkProvider(host = url, api = tonApi)
+    }
+
+    private fun createNowNodeJsonRpcProvider(apiKey: String): TonJsonRpcNetworkProvider {
+        val url = "https://ton.nownodes.io/"
+        val tonApi = createRetrofitInstance("$url${apiKey}/").create(TonApi::class.java)
+        return TonJsonRpcNetworkProvider(host = url, api = tonApi)
+    }
+}
---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ton/TonTransactionBuilder.kt
@@ -0,0 +1,46 @@
+package com.tangem.blockchain.blockchains.ton
+
+import com.google.protobuf.ByteString
+import com.tangem.blockchain.common.Amount
+import com.tangem.common.KeyPair
+import com.tangem.common.card.EllipticCurve
+import com.tangem.crypto.CryptoUtils
+import wallet.core.jni.proto.TheOpenNetwork
+
+class TonTransactionBuilder {
+
+    private val keyPair: KeyPair by lazy { generateKeyPair() }
+    private val modeTransactionConstant: Int =
+        TheOpenNetwork.SendMode.PAY_FEES_SEPARATELY_VALUE.or(TheOpenNetwork.SendMode.IGNORE_ACTION_PHASE_ERRORS_VALUE)
+
+    fun buildForSign(sequenceNumber: Int, amount: Amount, destination: String): TheOpenNetwork.SigningInput {
+        return input(sequenceNumber, amount, destination)
+    }
+
+    fun buildForSend(output: TheOpenNetwork.SigningOutput): String {
+        return output.encoded
+    }
+
+    private fun input(sequenceNumber: Int, amount: Amount, destination: String): TheOpenNetwork.SigningInput {
+        val transfer = TheOpenNetwork.Transfer
+            .newBuilder()
+            .setWalletVersion(TheOpenNetwork.WalletVersion.WALLET_V4_R2)
+            .setDest(destination)
+            .setAmount(amount.longValue ?: 0L)
+            .setSequenceNumber(sequenceNumber)
+            .setMode(modeTransactionConstant)
+            .setBounceBehavior(TheOpenNetwork.BounceBehavior.NON_BOUNCEABLE)
+
+        return TheOpenNetwork.SigningInput
+            .newBuilder()
+            .setTransfer(transfer)
+            .setPrivateKey(ByteString.copyFrom(keyPair.privateKey))
+            .build()
+    }
+
+    private fun generateKeyPair(): KeyPair {
+        val privateKey = CryptoUtils.generateRandomBytes(32)
+        val publicKey = CryptoUtils.generatePublicKey(privateKey, EllipticCurve.Ed25519)
+        return KeyPair(publicKey, privateKey)
+    }
+}
