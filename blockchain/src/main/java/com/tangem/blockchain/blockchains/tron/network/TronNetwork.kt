66c445d3332eb9c4c1b13712c630fd1150500b59
Tangem <git@tangem.com>
2023-04-12 12:47:36 +0300

    Updated on 2026-06-27

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/tron/network/TronNetwork.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/tron/network/TronNetwork.kt
@@ -2,12 +2,10 @@ package com.tangem.blockchain.blockchains.tron.network
 
 sealed class TronNetwork(val url: String) {
 
-    data class NowNodes(val apiKey: String) :
-        TronNetwork(url = "https://trx.nownodes.io/${apiKey}")
+    data class NowNodes(val apiKey: String) : TronNetwork(url = "https://trx.nownodes.io/")
 
-    data class GetBlock(val apiKey: String) :
-        TronNetwork(url = "https://trx.getblock.io/mainnet/${apiKey}")
+    data class GetBlock(val apiKey: String) : TronNetwork(url = "https://trx.getblock.io/mainnet/${apiKey}/")
 
-    data class TronGrid(val apiKey: String?) : TronNetwork(url = "https://api.trongrid.io")
-    object Nile : TronNetwork(url = "https://nile.trongrid.io")
+    data class TronGrid(val apiKey: String?) : TronNetwork(url = "https://api.trongrid.io/")
+    object Nile : TronNetwork(url = "https://nile.trongrid.io/")
 }
