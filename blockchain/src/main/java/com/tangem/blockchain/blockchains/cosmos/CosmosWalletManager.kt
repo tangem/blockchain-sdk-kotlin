1df190f07d3b48cdf9c41d71184745e271994053
Tangem <git@tangem.com>
2023-05-26 16:40:35 +0300

    Updated on 2025-11-03

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/cosmos/CosmosWalletManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/cosmos/CosmosWalletManager.kt
@@ -105,7 +105,7 @@ class CosmosWalletManager(
                 if (amounts.size == 3) {
                     return Result.Success(TransactionFee.SetOfThree(amounts[0], amounts[1], amounts[2]))
                 } else {
-                    return Result.Success(TransactionFee.NormalFee(amounts[0]))
+                    return Result.Success(TransactionFee.Single(amounts[0]))
                 }
 
             }
