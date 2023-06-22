785230195aed8ab225a9c7a17f7ac5ff25caacb5
Tangem <git@tangem.com>
2023-06-22 18:37:00 +0300

    Updated on 2026-02-02

--- blockchain/src/main/java/com/tangem/blockchain/common/derivation/DerivationStyle.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/derivation/DerivationStyle.kt
@@ -1,42 +1,37 @@
 package com.tangem.blockchain.common.derivation
 
 
-
-//
-//  DerivationStyle.swift
-//  BlockchainSdk
-//
-[REDACTED_AUTHOR]
-
-//
-// import Foundation
-//
-
 enum class DerivationStyle {
 
     @Deprecated("Will be removed after refactoring")
-    Legacy,
+    LEGACY,
 
     @Deprecated("Will be removed after refactoring")
-    New,
+    NEW,
 
-    // All have derivation according to BIP44 `coinType`
-    // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
-    v1,
+    /**
+     * All have derivation according to BIP44 `coinType`
+     * https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+     */
+    V1,
 
-    // `EVM-like` have identical derivation with `ethereumCoinType == 60`
-    // Other blockchains - according to BIP44 `coinType`
-    v2,
+    /**
+     * `EVM-like` have identical derivation with `ethereumCoinType == 60`
+     *  Other blockchains - according to BIP44 `coinType`
+     */
+    V2,
 
-    // `EVM-like` blockchains have identical derivation with `ethereumCoinType == 60`
-    // `Bitcoin-like` blockchains have different derivation related to `BIP`. For example `Legacy` and `SegWit`
-    v3;
+    /**
+     * `EVM-like` blockchains have identical derivation with `ethereumCoinType == 60`
+     *  `Bitcoin-like` blockchains have different derivation related to `BIP`. For example `Legacy` and `SegWit`
+     */
+    V3;
 
     fun getConfig() : DerivationConfig {
         return when(this) {
-            Legacy, v1 -> DerivationConfigV2
-            New, v2 -> DerivationConfigV1
-            v3 -> DerivationConfigV3
+            LEGACY, V1 -> DerivationConfigV2
+            NEW, V2 -> DerivationConfigV1
+            V3 -> DerivationConfigV3
         }
     }
 
