b7726115894fbeb1557f3a78b02d0015b8127f06
Tangem <git@tangem.com>
2023-06-22 19:06:16 +0300

    releases/5.27.0-1144

---
+++ blockchain/src/main/java/com/tangem/blockchain/common/assembly/KaspaWalletManagerAssembly.kt
@@ -0,0 +1,30 @@
+package com.tangem.blockchain.common.assembly
+
+import com.tangem.blockchain.blockchains.kaspa.KaspaTransactionBuilder
+import com.tangem.blockchain.blockchains.kaspa.KaspaWalletManager
+import com.tangem.blockchain.blockchains.kaspa.network.KaspaNetworkProvider
+import com.tangem.blockchain.blockchains.kaspa.network.KaspaNetworkService
+import com.tangem.blockchain.blockchains.kaspa.network.KaspaRestApiNetworkProvider
+import com.tangem.blockchain.network.API_KASPA
+
+object KaspaWalletManagerAssembly : WalletManagerAssembly<KaspaWalletManager>() {
+
+    override fun make(input: WalletManagerAssemblyInput): KaspaWalletManager {
+        val providers: List<KaspaNetworkProvider> = buildList {
+            add(KaspaRestApiNetworkProvider(API_KASPA))
+
+            input.config.kaspaSecondaryApiUrl
+                ?.takeIf { it.isNotBlank() }
+                ?.let { url ->
+                    add(KaspaRestApiNetworkProvider("$url/"))
+                }
+        }
+
+        return KaspaWalletManager(
+            wallet = input.wallet,
+            transactionBuilder = KaspaTransactionBuilder(),
+            networkProvider = KaspaNetworkService(providers)
+        )
+    }
+
+}
