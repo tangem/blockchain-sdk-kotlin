5f1846ed79801dc61da9d8c5dcc58992fe93a988
Tangem <git@tangem.com>
2023-06-26 15:37:30 +0300

    releases/5.27.0-1144

--- blockchain/src/main/java/com/tangem/blockchain/common/assembly/WalletManagerAssembly.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/assembly/WalletManagerAssembly.kt
@@ -5,9 +5,9 @@ import com.tangem.blockchain.common.*
 /**
  * Creates WalletManager instance
  */
-abstract class WalletManagerAssembly<out T : WalletManager> {
+internal abstract class WalletManagerAssembly<out T : WalletManager> {
 
-    abstract fun make(input: WalletManagerAssemblyInput): T
+    internal abstract fun make(input: WalletManagerAssemblyInput): T
 
 }
 
