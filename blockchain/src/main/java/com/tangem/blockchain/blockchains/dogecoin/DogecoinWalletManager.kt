433bbfa636edd5f6dcdd3d1491dcad038fdd68b9
Tangem <git@tangem.com>
2023-06-29 00:12:29 +0500

    Updated on 2026-02-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/dogecoin/DogecoinWalletManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/dogecoin/DogecoinWalletManager.kt
@@ -6,6 +6,7 @@ import com.tangem.blockchain.blockchains.bitcoin.network.BitcoinFee
 import com.tangem.blockchain.blockchains.bitcoin.network.BitcoinNetworkProvider
 import com.tangem.blockchain.common.TransactionSender
 import com.tangem.blockchain.common.Wallet
+import com.tangem.blockchain.common.txhistory.TransactionHistoryProvider
 import com.tangem.blockchain.extensions.Result
 import java.math.BigDecimal
 
@@ -13,7 +14,8 @@ class DogecoinWalletManager(
     wallet: Wallet,
     transactionBuilder: BitcoinTransactionBuilder,
     networkProvider: BitcoinNetworkProvider,
-) : BitcoinWalletManager(wallet, transactionBuilder, networkProvider), TransactionSender {
+    transactionHistoryProvider: TransactionHistoryProvider,
+) : BitcoinWalletManager(wallet, transactionHistoryProvider, transactionBuilder, networkProvider), TransactionSender {
 
     override suspend fun getBitcoinFeePerKb(): Result<BitcoinFee> {
         return Result.Success(
