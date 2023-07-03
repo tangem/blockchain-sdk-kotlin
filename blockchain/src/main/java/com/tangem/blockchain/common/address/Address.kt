35e438adb3a4244748c78b2bc1735811e3859a35
Tangem <git@tangem.com>
2023-07-03 11:04:17 +0300

    Updated on 2026-06-27

--- blockchain/src/main/java/com/tangem/blockchain/common/address/Address.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/address/Address.kt
@@ -1,6 +1,5 @@
 package com.tangem.blockchain.common.address
 
-// PlainAddress — ios
 open class Address(
         val value: String,
         val type: AddressType = AddressType.Default
--- blockchain/src/main/java/com/tangem/blockchain/common/address/AddressService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/address/AddressService.kt
@@ -1,18 +1,49 @@
 package com.tangem.blockchain.common.address
 
+import com.tangem.blockchain.common.Wallet
 import com.tangem.common.card.EllipticCurve
 
-abstract class AddressService {
-    abstract fun makeAddress(walletPublicKey: ByteArray, curve: EllipticCurve? = EllipticCurve.Secp256k1): String
-    abstract fun validate(address: String): Boolean
-    open fun makeAddresses(walletPublicKey: ByteArray, curve: EllipticCurve? = EllipticCurve.Secp256k1): Set<Address> =
-            setOf(Address(makeAddress(walletPublicKey, curve)))
+interface AddressService : AddressProvider {
+
+    // address making, old style
+    fun makeAddress(walletPublicKey: ByteArray, curve: EllipticCurve? = EllipticCurve.Secp256k1): String
+
+    fun validate(address: String): Boolean
+
+    override fun makeAddress(publicKey: Wallet.PublicKey, addressType: AddressType): AddressPublicKeyPair {
+        //TODO() check publicKey.blockchainKey
+        val address = makeAddress(publicKey.blockchainKey)
+
+        return AddressPublicKeyPair(address, publicKey, addressType)
+    }
+
+    // we need start use this method and smoothly move to this
+    // remove "newStyle" later
+    // made this way because return type is not signature part
+    fun makeAddressNewStyle(walletPublicKey: ByteArray, curve: EllipticCurve? = EllipticCurve.Secp256k1): Address {
+        return Address(makeAddress(walletPublicKey, curve))
+    }
+
+}
+
+interface AddressProvider {
+    fun makeAddress(publicKey: Wallet.PublicKey, addressType: AddressType): AddressPublicKeyPair
+}
+
+
+@Deprecated("Use AddressProvider.makeAddress instead of this")
+interface MultipleAddressProvider : AddressService {
+
+    fun makeAddresses(walletPublicKey: ByteArray, curve: EllipticCurve? = EllipticCurve.Secp256k1): Set<Address> {
+        return setOf(Address(makeAddress(walletPublicKey, curve)))
+    }
 }
 
 interface MultisigAddressProvider {
+
     fun makeMultisigAddresses(
-            walletPublicKey: ByteArray,
-            pairPublicKey: ByteArray,
-            curve: EllipticCurve? = null
+        walletPublicKey: ByteArray,
+        pairPublicKey: ByteArray,
+        curve: EllipticCurve? = null,
     ): Set<Address>
 }
