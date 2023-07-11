9b2ac68619a124cc9611ac56bbcd99ef1cd7b8e1
Tangem <git@tangem.com>
2023-07-11 11:42:26 +0400

    Updated on 2025-11-28

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/ducatus/network/DucatusNetworkService.kt
+++
@@ -1,6 +0,0 @@
-package com.tangem.blockchain.blockchains.ducatus.network
-
-import com.tangem.blockchain.blockchains.ducatus.network.bitcore.BitcoreNetworkProvider
-import com.tangem.blockchain.network.API_DUCATUS
-
-class DucatusNetworkService : BitcoreNetworkProvider(API_DUCATUS)
