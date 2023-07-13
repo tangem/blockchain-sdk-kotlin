72ebedf24097abecec952fc85a14722530e16a39
Tangem <git@tangem.com>
2023-07-13 14:11:40 +0300

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/ducatus/network/DucatusNetworkService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ducatus/network/DucatusNetworkService.kt
@@ -9,14 +9,13 @@ import com.tangem.blockchain.network.API_DUCATUS
 class DucatusNetworkService : BitcoreNetworkProvider(API_DUCATUS) {
 
     override suspend fun getFee(): Result<BitcoinFee> {
-        // TODO Bitcore is used only in Ducatus and fee is hardcoded there
+        // Bitcore is used only in Ducatus and fee is hardcoded there
         return Result.Failure(BlockchainSdkError.CustomError("Fee should be hardcoded in ducatus"))
     }
 
     override suspend fun getSignatureCount(address: String): Result<Int> {
-        // TODO Bitcore is used only in Ducatus and we don't check signature count
-        return Result.Failure(BlockchainSdkError.CustomError("Not yet implemented"))
+        // Bitcore is used only in Ducatus and we don't check signature count
+        return Result.Failure(BlockchainSdkError.CustomError("Signature count is not appliable for ducatus"))
     }
-
-
+    
 }
