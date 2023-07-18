54ba9585ae90f3d9edd1c615c18a9186e6a27f38
Tangem <git@tangem.com>
2023-07-18 20:01:27 +0300

    Updated on 2025-12-22

--- blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/BitcoinWalletManagerAssembly.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/BitcoinWalletManagerAssembly.kt
@@ -16,8 +16,7 @@ internal object BitcoinWalletManagerAssembly : WalletManagerAssembly<BitcoinWall
                 transactionBuilder = BitcoinTransactionBuilder(
                     walletPublicKey = publicKey.blockchainKey,
                     blockchain = blockchain,
-                    walletAddresses = emptySet()
-                    // TODO refactoring, make wallet hold address instead of addresskeypair in next task
+                    walletAddresses = input.wallet.addresses
                 ),
                 networkProvider = BitcoinNetworkService(
                     providers = blockchain.getBitcoinNetworkProviders(blockchain, input.config)
