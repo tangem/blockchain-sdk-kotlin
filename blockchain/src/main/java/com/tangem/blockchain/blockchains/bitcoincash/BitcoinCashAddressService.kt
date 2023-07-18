418df31d7028858b732fe2df97dff1af8f434f6b
Tangem <git@tangem.com>
2023-07-18 14:37:48 +0300

    Updated on 2025-12-22

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoincash/BitcoinCashAddressService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoincash/BitcoinCashAddressService.kt
@@ -1,44 +1,61 @@
 package com.tangem.blockchain.blockchains.bitcoincash
 
 import com.tangem.blockchain.blockchains.bitcoin.BitcoinAddressService
+import com.tangem.blockchain.blockchains.bitcoin.BitcoinLegacyAddressService
+import com.tangem.blockchain.blockchains.bitcoin.BitcoinScriptAddress
 import com.tangem.blockchain.blockchains.bitcoincash.cashaddr.BitcoinCashAddressType
 import com.tangem.blockchain.blockchains.bitcoincash.cashaddr.CashAddr
 import com.tangem.blockchain.common.Blockchain
-import com.tangem.blockchain.common.address.Address
-import com.tangem.blockchain.common.address.AddressType
-import com.tangem.blockchain.common.address.MultipleAddressProvider
-import com.tangem.blockchain.common.address.PlainAddress
+import com.tangem.blockchain.common.Wallet
+import com.tangem.blockchain.common.address.*
 import com.tangem.common.card.EllipticCurve
 import com.tangem.common.extensions.calculateRipemd160
 import com.tangem.common.extensions.calculateSha256
 import com.tangem.common.extensions.toCompressedPublicKey
+import org.bitcoinj.params.MainNetParams
+import java.lang.IllegalStateException
 
-class BitcoinCashAddressService(blockchain: Blockchain) : MultipleAddressProvider {
+class BitcoinCashAddressService(blockchain: Blockchain) : AddressService {
 
     private val cashAddr = when (blockchain) {
         Blockchain.BitcoinCash -> CashAddr(false)
         Blockchain.BitcoinCashTestnet -> CashAddr(true)
-        else -> throw Exception("${blockchain.fullName} blockchain is not supported by ${this::class.simpleName}")
+        else -> throw IllegalStateException(
+            "${blockchain.fullName} blockchain is not supported by ${this::class.simpleName}"
+        )
     }
 
-    private val bitcoinAddressService = BitcoinAddressService(Blockchain.Bitcoin)
+    private val legacyService = BitcoinAddressService(Blockchain.Bitcoin).legacy
 
-    override fun makeAddress(walletPublicKey: ByteArray, curve: EllipticCurve?) =
-        makeCashAddrAddress(walletPublicKey).value
+    override fun makeAddress(publicKey: Wallet.PublicKey, addressType: AddressType): PlainAddress {
+        return if (addressType == AddressType.Default) {
+            makeCashAddrAddress(publicKey)
+        } else {
+            val address = legacyService.makeAddressOldStyle(publicKey.blockchainKey.toCompressedPublicKey()).value
+            PlainAddress(
+                value = address,
+                type = AddressType.Legacy,
+                publicKey = publicKey
+            )
+        }
+    }
 
-    override fun makeAddresses(walletPublicKey: ByteArray, curve: EllipticCurve?) =
-        setOf(
-            bitcoinAddressService.makeLegacyAddress(walletPublicKey.toCompressedPublicKey()),
-            makeCashAddrAddress(walletPublicKey)
-        )
+    // override fun makeAddress(walletPublicKey: ByteArray, curve: EllipticCurve?) =
+    //     makeCashAddrAddress(walletPublicKey).value
+
+    // override fun makeAddresses(walletPublicKey: ByteArray, curve: EllipticCurve?) =
+    //     setOf(
+    //         bitcoinAddressService.makeLegacyAddress(walletPublicKey.toCompressedPublicKey()),
+    //         makeCashAddrAddress(walletPublicKey)
+    //     )
 
     override fun validate(address: String) =
-        validateCashAddrAddress(address) || bitcoinAddressService.validateLegacyAddress(address)
+        validateCashAddrAddress(address) || legacyService.validate(address)
 
-    private fun makeCashAddrAddress(walletPublicKey: ByteArray): Address {
-        val publicKeyHash = walletPublicKey.toCompressedPublicKey().calculateSha256().calculateRipemd160()
+    private fun makeCashAddrAddress(publicKey: Wallet.PublicKey): PlainAddress {
+        val publicKeyHash = publicKey.blockchainKey.toCompressedPublicKey().calculateSha256().calculateRipemd160()
         val address = cashAddr.toCashAddress(BitcoinCashAddressType.P2PKH, publicKeyHash)
-        return PlainAddress(address, AddressType.Default)
+        return PlainAddress(address, AddressType.Default, publicKey)
     }
 
     fun validateCashAddrAddress(address: String) = cashAddr.isValidCashAddress(address)
@@ -46,5 +63,4 @@ class BitcoinCashAddressService(blockchain: Blockchain) : MultipleAddressProvide
     fun getPublicKeyHash(address: String): ByteArray {
         return cashAddr.decodeCashAddress(address).hash
     }
-
 }
--- blockchain/src/main/java/com/tangem/blockchain/blockchains/stellar/StellarAddressService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/stellar/StellarAddressService.kt
@@ -1,13 +1,26 @@
 package com.tangem.blockchain.blockchains.stellar
 
+import com.tangem.blockchain.common.Wallet
 import com.tangem.blockchain.common.address.AddressService
+import com.tangem.blockchain.common.address.AddressType
+import com.tangem.blockchain.common.address.PlainAddress
 import com.tangem.common.card.EllipticCurve
 import org.stellar.sdk.KeyPair
 
 class StellarAddressService: AddressService {
-    override fun makeAddress(walletPublicKey: ByteArray, curve: EllipticCurve?): String {
-        val kp = KeyPair.fromPublicKey(walletPublicKey)
-        return kp.accountId
+
+    // override fun makeAddress(walletPublicKey: ByteArray, curve: EllipticCurve?): String {
+    //     val kp = KeyPair.fromPublicKey(walletPublicKey)
+    //     return kp.accountId
+    // }
+
+    override fun makeAddress(publicKey: Wallet.PublicKey, addressType: AddressType): PlainAddress {
+        val kp = KeyPair.fromPublicKey(publicKey.blockchainKey)
+        return PlainAddress(
+            value = kp.accountId,
+            type = addressType,
+            publicKey = publicKey
+        )
     }
 
     override fun validate(address: String): Boolean {
