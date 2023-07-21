902709e7081c5fc0689b9d3c88624bcdbd652d09
Tangem <git@tangem.com>
2023-07-21 10:10:09 +0300

    Updated on 2026-02-11

--- blockchain/src/main/java/com/tangem/blockchain/common/address/AddressService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/address/AddressService.kt
@@ -1,16 +1,3 @@
 package com.tangem.blockchain.common.address
 
-import com.tangem.common.card.EllipticCurve
-
-
 interface AddressService : AddressProvider, AddressValidator
-
-interface MultisigAddressProvider {
-
-    fun makeMultisigAddresses(
-        walletPublicKey: ByteArray,
-        pairPublicKey: ByteArray,
-        curve: EllipticCurve? = null,
-    ): Set<Address>
-
-}
