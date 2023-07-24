037b501389fe0412018ba50b1bb1c58a11241636
Tangem <git@tangem.com>
2023-07-24 16:41:24 +0300

    releases/5.27.0-1144

--- blockchain/src/test/java/com/tangem/blockchain/blockchains/cardano/CardanoAddressTest.kt
+++ blockchain/src/test/java/com/tangem/blockchain/blockchains/cardano/CardanoAddressTest.kt
@@ -1,10 +1,10 @@
 package com.tangem.blockchain.blockchains.cardano
 
-
 import com.google.common.truth.Truth
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.address.AddressType
 import com.tangem.blockchain.wrapInObject
+import com.tangem.common.card.EllipticCurve
 import com.tangem.common.extensions.hexToBytes
 import org.junit.Test
 
@@ -15,12 +15,14 @@ class CardanoAddressTest {
     @Test
     fun makeAddressesFromCorrectPublicKey() {
         val walletPublicKey = "EC5387D8B38BD9EF80BDBC78D0D7E1C53F08E269436C99D5B3C2DF4B2CE73012"
-                .hexToBytes()
+            .hexToBytes()
         val expectedByronAddress = "Ae2tdPwUPEZB972NhMM1dqixaUjnveaic6A23bprgrhvvgbkx2zaezrLY2Y"
         val expectedShelleyAddress = "addr1vyfgrxddyvyaqhr4jprr655s8ehzna9nehanx3fmu9280cgxxg2zc"
 
-        val byronAddress = addressService.makeAddress(walletPublicKey.wrapInObject(), AddressType.Legacy)
-        val shelleyAddress = addressService.makeAddress(walletPublicKey.wrapInObject(), AddressType.Default)
+        val byronAddress =
+            addressService.makeAddress(walletPublicKey.wrapInObject(), AddressType.Legacy, EllipticCurve.Secp256k1)
+        val shelleyAddress =
+            addressService.makeAddress(walletPublicKey.wrapInObject(), AddressType.Default, EllipticCurve.Secp256k1)
 
         Truth.assertThat(byronAddress.value).isEqualTo(expectedByronAddress)
         Truth.assertThat(shelleyAddress.value).isEqualTo(expectedShelleyAddress)
