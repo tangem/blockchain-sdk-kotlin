102cf147aef90f3761b7fbf1d109b78f4ae18fc6
Tangem <git@tangem.com>
2023-07-24 16:41:24 +0300

    Updated on 2026-02-02

--- blockchain/src/test/java/com/tangem/blockchain/blockchains/dogecoin/DogecoinTransactionTest.kt
+++ blockchain/src/test/java/com/tangem/blockchain/blockchains/dogecoin/DogecoinTransactionTest.kt
@@ -13,6 +13,7 @@ import com.tangem.blockchain.common.address.AddressType
 import com.tangem.blockchain.common.transaction.Fee
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.makeAddressWithDefaultType
+import com.tangem.common.card.EllipticCurve
 import com.tangem.common.extensions.hexToBytes
 import org.junit.Test
 import org.libdohj.params.DogecoinMainNetParams
@@ -35,7 +36,11 @@ class DogecoinTransactionTest {
         val feeValue = "0.01".toBigDecimal()
         val destinationAddress = "DRgF4iLXRhnYeQEV9kHmkvvnz128uCFZXL"
 
-        val address = BitcoinAddressService(blockchain).makeAddress(Wallet.PublicKey(walletPublicKey, null), AddressType.Legacy)
+        val address = BitcoinAddressService(blockchain).makeAddress(
+            publicKey = Wallet.PublicKey(walletPublicKey, null),
+            addressType = AddressType.Legacy,
+            curve = EllipticCurve.Secp256k1
+        )
 
         val transactionBuilder = BitcoinTransactionBuilder(walletPublicKey, blockchain, listOf(address))
         transactionBuilder.unspentOutputs =
