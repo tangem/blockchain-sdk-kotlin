4bab499757cb011276585ec04cbda26bd0d2c886
Tangem <git@tangem.com>
2023-08-02 17:09:25 +0300

    Updated on 2026-02-02

--- blockchain/src/androidTest/kotlin/CosmosTransactionTest.kt
+++ blockchain/src/androidTest/kotlin/CosmosTransactionTest.kt
@@ -15,6 +15,7 @@ import wallet.core.jni.proto.Cosmos
 import wallet.core.jni.proto.Cosmos.SigningOutput
 import wallet.core.jni.proto.Cosmos.SigningMode
 import wallet.core.java.AnySigner
+import java.math.BigDecimal
 
 class CosmosTransactionTest {
 
@@ -22,17 +23,21 @@ class CosmosTransactionTest {
         System.loadLibrary("TrustWalletCore")
     }
 
-    val seedKey = byteArrayOf(
+    private val seedKey = byteArrayOf(
         2, -30, -43, 25, 85, 93, 103, -26, 0, -37, 27, -102, 51, 106, 41, -109, 82,
         -21, -26, -73, -71, -28, 127, 110, 110, -99, 89, 94, -7, 85, 25, -20, -90
     )
 
-    val derivedKey = byteArrayOf(
+    private val derivedKey = byteArrayOf(
         2, -40, -68, 33, -71, -109, 85, -40, 123, 22, -61, -40, 47, 23, -112, -48, 74,
         -84, -43, 87, -75, 56, -93, -89, 33, 57, -21, -12, -31, 71, 111, 49, 26
     )
 
-    val publicKey = Wallet.PublicKey(seedKey, derivedKey,null)
+    private val signature = byteArrayOf(
+        65, 26, -22, 18, -112, -122, -4, -4, 104, -53, 89, 92, -113, 58, 123, 88, -5, 17, 45, 1, 15, 25, 75, 2, 48, 81, 105, 70, -39, 78, 1, -73, 67, 106, 51, 104, 91, -102, -125, 97, -49, 90, 55, 62, -48, 110, -14, 31, -15, -92, 19, -62, -110, 51, -86, -53, -55, -93, 122, 108, -68, 72, -56, 6
+    )
+
+    private val publicKey = Wallet.PublicKey(seedKey, derivedKey, null)
 
     @Test
     fun testTransaction() {
@@ -41,33 +46,32 @@ class CosmosTransactionTest {
             publicKey = publicKey
         )
 
-        val pseudoRandomBytes = byteArrayOf(
-            24, 104, 112, -7, 100, -64, 95, 12, -105, -67, 127, -37, -74, 61, 17, -30,
-            -56, -90, 61, -15, 26, 24, 94, -29, -91, -7, -54, -17, -120, -3, 2, -49
-        )
-
-        val input = transactionBuilder.buildForSign(
-            amount = Amount(
-                currencySymbol = "ATOM",
-                value = "0.005".toBigDecimal(),
-                decimals = 6,
-                type = AmountType.Coin
-
-            ),
-            source = "cosmos1tqksn8j4kj0feed2sglhfujp5amkndyac4z8jy",
-            destination = "cosmos1z56v8wqvgmhm3hmnffapxujvd4w4rkw6cxr8xy",
-            accountNumber = 726521,
-            sequenceNumber = 7,
-            feeAmount = null,
-            gas = null,
-            extras = null,
-            randomByteString = ByteString.copyFrom(pseudoRandomBytes)
-        )
+        // val input = transactionBuilder.buildForSign(
+        //     amount = Amount(
+        //         currencySymbol = "ATOM",
+        //         value = "0.05".toBigDecimal(),
+        //         decimals = 6,
+        //         type = AmountType.Coin
+        //
+        //     ),
+        //     source = "cosmos1tqksn8j4kj0feed2sglhfujp5amkndyac4z8jy",
+        //     destination = "cosmos1z56v8wqvgmhm3hmnffapxujvd4w4rkw6cxr8xy",
+        //     accountNumber = 726521,
+        //     sequenceNumber = 15,
+        //     feeAmount = Amount(
+        //         currencySymbol = "ATOM",
+        //         value = BigDecimal.valueOf(0.002717),
+        //         decimals = 6,
+        //         type = AmountType.Coin
+        //     ),
+        //     gas = 108700,
+        //     extras = null,
+        // )
 
         val message = transactionBuilder.buildForSend(
             amount = Amount(
                 currencySymbol = "ATOM",
-                value = "0.005".toBigDecimal(),
+                value = "0.05".toBigDecimal(),
                 decimals = 6,
                 type = AmountType.Coin
 
@@ -75,17 +79,21 @@ class CosmosTransactionTest {
             source = "cosmos1tqksn8j4kj0feed2sglhfujp5amkndyac4z8jy",
             destination = "cosmos1z56v8wqvgmhm3hmnffapxujvd4w4rkw6cxr8xy",
             accountNumber = 726521,
-            sequenceNumber = 7,
-            feeAmount = null,
-            gas = null,
+            sequenceNumber = 16,
+            feeAmount = Amount(
+                currencySymbol = "ATOM",
+                value = BigDecimal.valueOf(0.002717),
+                decimals = 6,
+                type = AmountType.Coin
+            ),
+            gas = 108700,
             extras = null,
-            randomByteString = ByteString.copyFrom(pseudoRandomBytes),
-            signature = signature = signer.sign(hash, wallet.publicKey)
+            signature = signature
         )
 
-
-        val expected = "{\"mode\":\"BROADCAST_MODE_SYNC\",\"tx_bytes\":\"CpABCo0BChwvY29zbW9zLmJhbmsudjFiZXRhMS5Nc2dTZW5kEm0KLWNvc21vczF0cWtzbjhqNGtqMGZlZWQyc2dsaGZ1anA1YW1rbmR5YWM0ejhqeRItY29zbW9zMXo1NnY4d3F2Z21obTNobW5mZmFweHVqdmQ0dzRya3c2Y3hyOHh5Gg0KBXVhdG9tEgQ1MDAwElQKUApGCh8vY29zbW9zLmNyeXB0by5zZWNwMjU2azEuUHViS2V5EiMKIQMNVLUV+7OcpvLZLd0m80ON/U+gpU1uj+GME8zarWeDohIECgIIARgHEgAaQHSfcPEKcVYjnQla00uI/EGD6eB6gxmHW2688G3sbqk9V+8SZEOoPqrM36pynuO2emI2+1YU0O6c2bkaXXUVi2E=\"}"
-        val actual = message.successOr { "" }
+        val expected =
+            "{\"mode\":\"BROADCAST_MODE_SYNC\",\"tx_bytes\":\"CpEBCo4BChwvY29zbW9zLmJhbmsudjFiZXRhMS5Nc2dTZW5kEm4KLWNvc21vczF0cWtzbjhqNGtqMGZlZWQyc2dsaGZ1anA1YW1rbmR5YWM0ejhqeRItY29zbW9zMXo1NnY4d3F2Z21obTNobW5mZmFweHVqdmQ0dzRya3c2Y3hyOHh5Gg4KBXVhdG9tEgU1MDAwMBJnClAKRgofL2Nvc21vcy5jcnlwdG8uc2VjcDI1NmsxLlB1YktleRIjCiEC2LwhuZNV2HsWw9gvF5DQSqzVV7U4o6chOev04UdvMRoSBAoCCAEYEBITCg0KBXVhdG9tEgQyNzE3EJzRBhpAQRrqEpCG/Pxoy1lcjzp7WPsRLQEPGUsCMFFpRtlOAbdDajNoW5qDYc9aNz7QbvIf8aQTwpIzqsvJo3psvEjIBg==\"}"
+        val actual = message
 
         assertEquals(expected, actual)
     }
