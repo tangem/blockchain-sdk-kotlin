edd2dd6eb6725c57e2012a00797163544be00e56
Tangem <git@tangem.com>
2023-08-03 20:11:41 +0400

    Updated on 2025-11-03

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/chia/network/ChiaResponse.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/chia/network/ChiaResponse.kt
@@ -11,17 +11,24 @@ data class ChiaCoinRecordsResponse(
 
 @JsonClass(generateAdapter = true)
 data class ChiaEstimateFeeResponse(
+    @Json(name = "estimates")
     val estimates: List<Long>
 )
 
 @JsonClass(generateAdapter = true)
 data class ChiaSendTransactionResponse(
+    @Json(name = "success")
     val success: Boolean,
+
+    @Json(name = "status")
     val status: String?,
+
+    @Json(name = "error")
     val error: String?
 )
 
 @JsonClass(generateAdapter = true)
 data class ChiaCoinRecord(
+    @Json(name = "coin")
     val coin: ChiaCoin
 )
