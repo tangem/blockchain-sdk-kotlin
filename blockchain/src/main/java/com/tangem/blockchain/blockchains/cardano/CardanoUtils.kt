be766b9f34798dbd08983a6e0bfce33609f72ac8
Tangem <git@tangem.com>
2023-08-16 23:44:52 +0300

    Updated on 2025-11-28

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/CardanoUtils.kt
@@ -0,0 +1,29 @@
+package com.tangem.blockchain.blockchains.cardano
+
+import com.tangem.crypto.hdWallet.DerivationNode
+import com.tangem.crypto.hdWallet.DerivationPath
+import com.tangem.crypto.hdWallet.bip32.ExtendedPublicKey
+
+object CardanoUtils {
+
+    // Link to the original code:
+    // https://github.com/trustwallet/wallet-core/blob/aa7475536e8c5b0383b1553073139b3498a9e35f/src/HDWallet.cpp#L163
+    fun extendedDerivationPath(derivationPath: DerivationPath): DerivationPath {
+        val nodes = derivationPath.nodes.toMutableList()
+        if (nodes.size != 5) {
+            error("Derivation path is short")
+        }
+
+
+        nodes[3] = DerivationNode.NonHardened(2)
+        nodes[4] = DerivationNode.NonHardened(0)
+
+        return DerivationPath(nodes)
+    }
+
+    // Method for computing an extended public key
+    fun extendPublicKey(publicKey: ExtendedPublicKey, extendedPublicKey: ExtendedPublicKey): ByteArray {
+        return publicKey.publicKey + publicKey.chainCode + extendedPublicKey.publicKey + extendedPublicKey.chainCode
+    }
+
+}
