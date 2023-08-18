14668e23db3e7b4bed5ba71677852a1912a22c34
Tangem <git@tangem.com>
2023-08-18 16:37:48 +0300

    Updated on 2026-06-27

--- blockchain/src/main/java/com/tangem/blockchain/common/CardanoAddressConfig.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/CardanoAddressConfig.kt
@@ -6,6 +6,6 @@ object CardanoAddressConfig {
      * Indicates whether to use the new 128-byte address instead of the old 32-byte one
      * We should keep old 32 byte addresses for backward compatibility
      */
-    var useExtendedAddressation = false
+    var useExtendedAddressing = false
 
 }
