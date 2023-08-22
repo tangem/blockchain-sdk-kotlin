5a1efe947c1d7c56c4ac36b1632b7d8be168b748
Tangem <git@tangem.com>
2023-08-22 14:03:09 +0300

    Updated on 2025-11-03

--- blockchain/src/main/java/com/tangem/blockchain/common/Blockchain.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/Blockchain.kt
@@ -275,7 +275,7 @@ enum class Blockchain(
         RavencoinTestnet -> "https://testnet.ravencoin.network/"
         CosmosTestnet -> "https://explorer.theta-testnet.polypore.xyz/accounts/"
         Cosmos -> "https://www.mintscan.io/cosmos/account/"
-        TerraV1 -> "https://finder.terra.money/classic/"
+        TerraV1 -> "https://atomscan.com/terra/accounts/"
         TerraV2 -> "https://terrasco.pe/mainnet/"
         Cronos -> "https://cronoscan.com/"
         AlephZero -> "https://alephzero.subscan.io/"
@@ -313,6 +313,7 @@ enum class Blockchain(
             Tezos -> "$baseUrl$address"
             Kaspa -> "${baseUrl}addresses/$address"
             Cosmos -> "${baseUrl}$address"
+            TerraV1 -> "${baseUrl}$address"
             CosmosTestnet -> "${baseUrl}$address"
             else -> fullUrl
         }
