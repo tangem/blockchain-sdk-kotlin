99d1cfe3f7dab53bce8957ba78c49b8f86187995
Tangem <git@tangem.com>
2023-09-14 18:25:33 +0500

    Updated on 2025-11-03

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/EthereumUtils.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/EthereumUtils.kt
@@ -3,11 +3,9 @@ package com.tangem.blockchain.blockchains.ethereum
 import com.tangem.blockchain.blockchains.ethereum.eip712.EthEip712Util
 import com.tangem.blockchain.common.*
 import com.tangem.blockchain.common.transaction.Fee
+import com.tangem.blockchain.extensions.isValidHex
 import com.tangem.blockchain.extensions.toBigDecimalOrDefault
-import com.tangem.common.extensions.hexToBytes
-import com.tangem.common.extensions.toByteArray
-import com.tangem.common.extensions.toDecompressedPublicKey
-import com.tangem.common.extensions.toHexString
+import com.tangem.common.extensions.*
 import org.kethereum.DEFAULT_GAS_LIMIT
 import org.kethereum.crypto.api.ec.ECDSASignature
 import org.kethereum.crypto.determineRecId
@@ -43,8 +41,7 @@ class EthereumUtils {
         }
 
         internal fun parseEthereumDecimal(value: String, decimalsCount: Int): BigDecimal? {
-            val data = value.hexToBytes()
-            val zeroInByte: Byte = 0
+            val data = value.removePrefix(HEX_PREFIX).asciiHexToBytes() ?: return null
 
             val balanceData = when {
                 data.size <= UInt256Size -> data
@@ -59,8 +56,17 @@ class EthereumUtils {
                 .movePointLeft(decimalsCount)
         }
 
+        private fun String.asciiHexToBytes(): ByteArray? {
+            var trimmedString = this.remove(" ")
+            if (trimmedString.length % 2 != 0) {
+                trimmedString = "0$trimmedString"
+            }
+
+            return if (trimmedString.isValidHex()) trimmedString.hexToBytes() else null
+        }
+
         private fun ByteArray.allOutOfRangeIsEqualTo(range: Int, equal: Byte): Boolean =
-            this.copyOfRange(range, this.size).all{ it == equal }
+            this.copyOfRange(range, this.size).all { it == equal }
 
         fun prepareSignedMessageData(
             signedHash: ByteArray,
