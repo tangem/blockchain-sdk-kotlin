773933fb598a50c52379b302ec3b297f23815a1f
Doston Kamalov <dkamalov@tangem.com>
2023-09-21 09:26:25 +0500

    AND-3671 Transaction history for Ethereum

--- blockchain/src/main/java/com/tangem/blockchain/network/blockbook/config/BlockBookRequest.kt
+++ blockchain/src/main/java/com/tangem/blockchain/network/blockbook/config/BlockBookRequest.kt
@@ -1,5 +1,27 @@
 package com.tangem.blockchain.network.blockbook.config
 
-enum class BlockBookRequest {
-    GET_ADDRESS, GET_FEE, SEND_TRANSACTION, GET_UTXO
+import com.tangem.blockchain.common.txhistory.TransactionHistoryRequest
+
+sealed class BlockBookRequest {
+    data class GetAddress(
+        val page: Int? = null,
+        val pageSize: Int? = null,
+        val filterType: TransactionHistoryRequest.FilterType? = null,
+    ) : BlockBookRequest() {
+
+        fun params(): String = buildString {
+            if (page != null) append("&page=$page")
+            if (pageSize != null) append("&pageSize=$pageSize")
+            if (filterType != null) append(filterType.toParam())
+        }
+
+        private fun TransactionHistoryRequest.FilterType.toParam(): String = when (this) {
+            TransactionHistoryRequest.FilterType.Coin -> "&filter=0"
+            is TransactionHistoryRequest.FilterType.Contract -> "&contract=$address"
+        }
+    }
+
+    object GetFee : BlockBookRequest()
+    object SendTransaction : BlockBookRequest()
+    object GetUTXO : BlockBookRequest()
 }
