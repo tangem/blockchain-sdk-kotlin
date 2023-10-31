37d28179a6aef6c9a0567e88030ef8ae2a29bfa0
Tangem <git@tangem.com>
2023-10-31 22:40:10 +0300

    Updated on 2025-08-28

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/xrp/network/XrpNetworkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/xrp/network/XrpNetworkProvider.kt
@@ -5,23 +5,30 @@ import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.SimpleResult
 import java.math.BigDecimal
 
-interface XrpNetworkProvider: NetworkProvider {
+interface XrpNetworkProvider : NetworkProvider {
     suspend fun getInfo(address: String): Result<XrpInfoResponse>
     suspend fun sendTransaction(transaction: String): SimpleResult
     suspend fun getFee(): Result<XrpFeeResponse>
     suspend fun checkIsAccountCreated(address: String): Boolean
+    suspend fun encodeAddress(address: String, tag: String): Result<XrpAddressResponse>
+    suspend fun decodeAddress(address: String): Result<XrpAddressResponse>
 }
 
 data class XrpInfoResponse(
-        val balance: BigDecimal = BigDecimal.ZERO,
-        val sequence: Long = 0,
-        val hasUnconfirmed: Boolean = false,
-        val reserveBase: BigDecimal,
-        val accountFound: Boolean = true
+    val balance: BigDecimal = BigDecimal.ZERO,
+    val sequence: Long = 0,
+    val hasUnconfirmed: Boolean = false,
+    val reserveBase: BigDecimal,
+    val accountFound: Boolean = true,
 )
 
 data class XrpFeeResponse(
-        val minimalFee: BigDecimal,
-        val normalFee: BigDecimal,
-        val priorityFee: BigDecimal
+    val minimalFee: BigDecimal,
+    val normalFee: BigDecimal,
+    val priorityFee: BigDecimal,
+)
+
+data class XrpAddressResponse(
+    val address: String,
+    val tag: String? = null,
 )
