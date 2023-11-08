1687c286f3b8d4fcda1864718e9b86f737331b50
Tangem <git@tangem.com>
2023-11-08 16:23:48 +0200

    Updated on 2026-06-27

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/CardanoAddressServiceFacade.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/CardanoAddressServiceFacade.kt
@@ -1,34 +1,29 @@
 package com.tangem.blockchain.blockchains.cardano
 
 import com.tangem.blockchain.common.Blockchain
-import com.tangem.blockchain.common.address.Address
-import com.tangem.blockchain.common.address.AddressService
-import com.tangem.blockchain.common.address.TrustWalletAddressService
+import com.tangem.blockchain.common.Wallet
+import com.tangem.blockchain.common.address.*
 import com.tangem.common.card.EllipticCurve
 
-class CardanoAddressServiceFacade : AddressService() {
+class CardanoAddressServiceFacade : AddressService {
 
     private val legacyService = CardanoAddressService(Blockchain.Cardano)
-    private val trustWalletService = TrustWalletAddressService(Blockchain.Cardano)
+    private val trustWalletService = WalletCoreAddressService(Blockchain.Cardano)
 
-    override fun makeAddress(walletPublicKey: ByteArray, curve: EllipticCurve?): String {
-        return if (walletPublicKey.isExtendedPublicKey()) {
-            trustWalletService.makeAddress(walletPublicKey, curve)
+    override fun makeAddress(
+        publicKey: Wallet.PublicKey,
+        addressType: AddressType,
+        curve: EllipticCurve
+    ): PlainAddress {
+        return if (publicKey.blockchainKey.isExtendedPublicKey()) {
+            trustWalletService.makeAddress(publicKey, addressType, curve)
         } else {
-            legacyService.makeAddress(walletPublicKey, curve)
+            legacyService.makeAddress(publicKey, addressType, curve)
         }
     }
 
     override fun validate(address: String): Boolean {
         return trustWalletService.validate(address) || legacyService.validate(address)
-    }
-
-    override fun makeAddresses(walletPublicKey: ByteArray, curve: EllipticCurve?): Set<Address> {
-        return if (walletPublicKey.isExtendedPublicKey()) {
-            trustWalletService.makeAddresses(walletPublicKey, curve)
-        } else {
-            legacyService.makeAddresses(walletPublicKey, curve)
-        }
     }
 
     private fun ByteArray.isExtendedPublicKey() = this.size == 128
