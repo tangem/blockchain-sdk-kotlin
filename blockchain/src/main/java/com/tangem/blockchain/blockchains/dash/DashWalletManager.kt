74bd1068504e6b08737bc6932236a133b723e14f
Tangem <git@tangem.com>
2023-12-18 21:33:06 +0200

    Updated on 2025-12-30

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/dash/DashWalletManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/dash/DashWalletManager.kt
@@ -4,11 +4,12 @@ import com.tangem.blockchain.blockchains.bitcoin.BitcoinTransactionBuilder
 import com.tangem.blockchain.blockchains.bitcoin.BitcoinWalletManager
 import com.tangem.blockchain.blockchains.bitcoin.network.BitcoinFee
 import com.tangem.blockchain.blockchains.bitcoin.network.BitcoinNetworkProvider
+import com.tangem.blockchain.common.Amount
 import com.tangem.blockchain.common.TransactionSender
 import com.tangem.blockchain.common.Wallet
+import com.tangem.blockchain.common.transaction.TransactionFee
 import com.tangem.blockchain.common.txhistory.TransactionHistoryProvider
 import com.tangem.blockchain.extensions.Result
-import java.math.BigDecimal
 
 class DashWalletManager(
     wallet: Wallet,
@@ -17,6 +18,10 @@ class DashWalletManager(
     transactionHistoryProvider: TransactionHistoryProvider,
 ) : BitcoinWalletManager(wallet, transactionHistoryProvider, transactionBuilder, networkProvider), TransactionSender {
 
-    override val addressToEstimateFee = "XcN7YdKmSshcFSkL5vjJfnWUhnH2Uxqbm4"
+    private val addressToEstimateFee = "XcN7YdKmSshcFSkL5vjJfnWUhnH2Uxqbm4"
+
+    override suspend fun estimateFee(amount: Amount): Result<TransactionFee> {
+        return getFee(amount, addressToEstimateFee)
+    }
 
 }
