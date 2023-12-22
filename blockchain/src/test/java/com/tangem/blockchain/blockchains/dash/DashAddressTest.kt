3da6539f2f0cc76a5cb14b52f0fb56c9f7cc7a4f
Tangem <git@tangem.com>
2023-12-22 16:19:43 +0300

    Updated on 2025-11-28

--- blockchain/src/test/java/com/tangem/blockchain/blockchains/dash/DashAddressTest.kt
+++ blockchain/src/test/java/com/tangem/blockchain/blockchains/dash/DashAddressTest.kt
@@ -12,7 +12,10 @@ class DashAddressTest {
 
     @Test
     fun makeAddressFromCorrectPublicKey() {
-        val walletPublicKey = "0441DCD64B5F4A039FC339A16300A833A883B218909F2EBCAF3906651C76842C45E3D67E8D2947E6FEE8B62D3D3B6A4D5F212DA23E478DD69A2C6CCC851F300D80".hexToBytes()
+        val walletPublicKey = (
+            "0441DCD64B5F4A039FC339A16300A833A883B218909F2EBCAF3906651C76842C45E3D67E8D2947E6FEE8B62D3D3B6A4D5F212DA" +
+                "23E478DD69A2C6CCC851F300D80"
+            ).hexToBytes()
         val expected = "Xs92pJsKUXRpbwzxDjBjApiwMK6JysNntG"
 
         Truth.assertThat(addressService.makeAddress(walletPublicKey)).isEqualTo(expected)
