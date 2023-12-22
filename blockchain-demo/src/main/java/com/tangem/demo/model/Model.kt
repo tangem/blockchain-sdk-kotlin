8fa3b81534a5112ae8b9f59350cd9252068c66fa
Tangem <git@tangem.com>
2023-12-22 14:54:19 +0300

    Updated on 2026-02-11

--- blockchain-demo/src/main/java/com/tangem/demo/model/Model.kt
+++ blockchain-demo/src/main/java/com/tangem/demo/model/Model.kt
@@ -1,12 +1,12 @@
-package com.tangem.blockchain_demo.model
+package com.tangem.demo.model
 
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.Token
-import com.tangem.blockchain_demo.extensions.derivationStyle
-import com.tangem.blockchain_demo.extensions.getTangemNoteBlockchain
 import com.tangem.common.card.Card
 import com.tangem.common.card.WalletData
 import com.tangem.common.extensions.ByteArrayKey
+import com.tangem.demo.extensions.derivationStyle
+import com.tangem.demo.extensions.getTangemNoteBlockchain
 import com.tangem.operations.CommandResponse
 import com.tangem.operations.backup.PrimaryCard
 import com.tangem.operations.derivation.ExtendedPublicKeysMap
@@ -46,7 +46,11 @@ data class BlockchainNetwork(
 ) {
     constructor(blockchain: Blockchain, card: Card) : this(
         blockchain = blockchain,
-        derivationPath = if (card.settings.isHDWalletAllowed) blockchain.derivationPath(card.derivationStyle)?.rawPath else null,
+        derivationPath = if (card.settings.isHDWalletAllowed) {
+            blockchain.derivationPath(card.derivationStyle)?.rawPath
+        } else {
+            null
+        },
         tokens = emptyList(),
     )
 }
