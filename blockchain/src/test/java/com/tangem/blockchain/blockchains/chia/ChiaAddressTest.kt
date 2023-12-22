cdc0b968bda739842b2b2da932682eef02169a9c
Tangem <git@tangem.com>
2023-12-22 16:19:43 +0300

    Updated on 2025-11-03

--- blockchain/src/test/java/com/tangem/blockchain/blockchains/chia/ChiaAddressTest.kt
+++ blockchain/src/test/java/com/tangem/blockchain/blockchains/chia/ChiaAddressTest.kt
@@ -12,7 +12,9 @@ class ChiaAddressTest {
 
     @Test
     fun makeAddressFromCorrectPublicKey() {
-        val walletPublicKey = "b8f7dd239557ff8c49d338f89ac1a258a863fa52cd0a502e3aaae4b6738ba39ac8d982215aa3fa16bc5f8cb7e44b954d".hexToBytes()
+        val walletPublicKey = (
+            "b8f7dd239557ff8c49d338f89ac1a258a863fa52cd0a502e3aaae4b6738ba39ac8d982215aa3fa16bc5f8cb7e44b954d"
+            ).hexToBytes()
         val expected = "xch14gxuvfmw2xdxqnws5agt3ma483wktd2lrzwvpj3f6jvdgkmf5gtq20kt0z"
         val expectedTestnet = "txch14gxuvfmw2xdxqnws5agt3ma483wktd2lrzwvpj3f6jvdgkmf5gtq8g3aw3"
 
