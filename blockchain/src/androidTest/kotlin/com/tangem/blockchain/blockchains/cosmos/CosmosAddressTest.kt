65dba50dac909e55d83d29613de7b77983dbe847
Tangem <git@tangem.com>
2023-12-22 16:19:43 +0300

    Updated on 2026-06-27

--- blockchain/src/androidTest/kotlin/com/tangem/blockchain/blockchains/cosmos/CosmosAddressTest.kt
+++ blockchain/src/androidTest/kotlin/com/tangem/blockchain/blockchains/cosmos/CosmosAddressTest.kt
@@ -16,8 +16,7 @@ class CosmosAddressTest {
 
     @Test
     fun testAddressValidation1() {
-        val walletPublicKey = "038C869947BB27B78D36F2D60158CAF295EA874943D9574DC9C2228DB152D9A1C2"
-            .hexToBytes()
+        val walletPublicKey = "038C869947BB27B78D36F2D60158CAF295EA874943D9574DC9C2228DB152D9A1C2".hexToBytes()
         val expected = "cosmos1edt8c8lqghvj94tcqy3yc8afy0xyv2563q0nhm"
 
         assertEquals(addressService.makeAddress(walletPublicKey), expected)
@@ -25,8 +24,7 @@ class CosmosAddressTest {
 
     @Test
     fun testAddressValidation2() {
-        val walletPublicKey = "02D8BC21B99355D87B16C3D82F1790D04AACD557B538A3A72139EBF4E1476F311A"
-            .hexToBytes()
+        val walletPublicKey = "02D8BC21B99355D87B16C3D82F1790D04AACD557B538A3A72139EBF4E1476F311A".hexToBytes()
         val expected = "cosmos1tqksn8j4kj0feed2sglhfujp5amkndyac4z8jy"
 
         assertEquals(addressService.makeAddress(walletPublicKey), expected)
