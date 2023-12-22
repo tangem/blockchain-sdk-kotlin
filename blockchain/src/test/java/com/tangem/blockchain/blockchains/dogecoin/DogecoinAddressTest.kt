8984bd51e9a64fd7a2efb4cc0d962e9bbb291a17
Tangem <git@tangem.com>
2023-12-22 16:19:43 +0300

    Updated on 2025-12-30

--- blockchain/src/test/java/com/tangem/blockchain/blockchains/dogecoin/DogecoinAddressTest.kt
+++ blockchain/src/test/java/com/tangem/blockchain/blockchains/dogecoin/DogecoinAddressTest.kt
@@ -12,8 +12,10 @@ class DogecoinAddressTest {
 
     @Test
     fun makeAddressFromCorrectPublicKey() {
-        val walletPublicKey =
-            "04BE37CD5251C8999EDBBFC759D800EB41E4DCB718289601EB15819404E1B2F2ED90FE50C2A481D06EC790D1EF6184974EB655ABAE4BE56A6D1C9E1A17B1EFDF02".hexToBytes()
+        val walletPublicKey = (
+            "04BE37CD5251C8999EDBBFC759D800EB41E4DCB718289601EB15819404E1B2F2ED90FE50C2A481D06EC790D1EF6184974EB655A" +
+                "BAE4BE56A6D1C9E1A17B1EFDF02"
+            ).hexToBytes()
         val expected = "DRgF4iLXRhnYeQEV9kHmkvvnz128uCFZXL"
 
         Truth.assertThat(addressService.makeAddress(walletPublicKey))
