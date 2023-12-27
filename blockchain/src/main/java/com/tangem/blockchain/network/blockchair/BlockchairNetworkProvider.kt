b37e207097c254ba6e36192f1dcaa6f4f7c14ddd
Tangem <git@tangem.com>
2023-12-27 12:48:10 +0300

    develop-1150

--- blockchain/src/main/java/com/tangem/blockchain/network/blockchair/BlockchairNetworkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/network/blockchair/BlockchairNetworkProvider.kt
@@ -6,6 +6,7 @@ import com.tangem.blockchain.blockchains.bitcoin.network.BitcoinFee
 import com.tangem.blockchain.blockchains.bitcoin.network.BitcoinNetworkProvider
 import com.tangem.blockchain.common.BasicTransactionData
 import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchain.common.BlockchainSdkError
 import com.tangem.blockchain.common.toBlockchainSdkError
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.SimpleResult
@@ -144,10 +145,15 @@ open class BlockchairNetworkProvider(
 
     override suspend fun sendTransaction(transaction: String): SimpleResult {
         return try {
-            makeRequestUsingKeyOnlyWhenNeeded {
+            val response = makeRequestUsingKeyOnlyWhenNeeded {
                 api.sendTransaction(BlockchairBody(transaction), apiKey, authorizationToken)
             }
-            SimpleResult.Success
+
+            if (response.transactionData.hash.isNotBlank()) {
+                SimpleResult.Success
+            } else {
+                SimpleResult.Failure(BlockchainSdkError.FailedToSendException)
+            }
         } catch (exception: Exception) {
             SimpleResult.Failure(exception.toBlockchainSdkError())
         }
