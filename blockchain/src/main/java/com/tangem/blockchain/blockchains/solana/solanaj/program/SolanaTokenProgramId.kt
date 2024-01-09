14b77f3bb419819ecd139cc9a414064e952b04d7
Tangem <git@tangem.com>
2024-01-09 17:06:59 +0300

    Updated on 2026-02-02

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/solana/solanaj/program/SolanaTokenProgramId.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/solana/solanaj/program/SolanaTokenProgramId.kt
@@ -3,7 +3,7 @@ package com.tangem.blockchain.blockchains.solana.solanaj.program
 import org.p2p.solanaj.core.PublicKey
 import org.p2p.solanaj.programs.Program
 
-internal enum class TokenProgramId(val value: PublicKey) {
+internal enum class SolanaTokenProgramId(val value: PublicKey) {
     TOKEN(value = Program.Id.token),
     TOKEN_2022(value = PublicKey("TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb")),
 }
