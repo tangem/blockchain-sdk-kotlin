5f715b8262e6ebf0b55741ca6c4f67b30a5eadcb
Tangem <git@tangem.com>
2024-01-16 18:31:03 +0300

    Updated on 2026-01-28

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/tron/TronWalletManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/tron/TronWalletManager.kt
@@ -59,7 +59,7 @@ class TronWalletManager(
 
     private fun updateError(error: BlockchainError) {
         Log.e(this::class.java.simpleName, error.customMessage)
-        if (error is BlockchainSdkError) error("Error isn't BlockchainSdkError")
+        if (error is BlockchainSdkError) throw error
     }
 
     override suspend fun send(transactionData: TransactionData, signer: TransactionSigner): SimpleResult {
