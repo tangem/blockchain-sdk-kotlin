5b8e26f6919c51b7825cfcc53da9807bddd20a91
Tangem <git@tangem.com>
2024-01-16 11:18:48 +0300

    Updated on 2026-02-02

--- blockchain/src/main/java/com/tangem/blockchain/network/RetrofitBuilder.kt
+++ blockchain/src/main/java/com/tangem/blockchain/network/RetrofitBuilder.kt
@@ -2,6 +2,7 @@ package com.tangem.blockchain.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.tangem.blockchain.blockchains.aptos.network.response.AptosResource
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -62,6 +63,7 @@ data class Timeout(
 internal val moshi: Moshi by lazy {
     Moshi.Builder()
         .add(BigDecimal::class.java, BigDecimalAdapter)
+        .add(AptosResource.createPolymorphicJsonAdapterFactory())
         .add(KotlinJsonAdapterFactory())
         .build()
 }
