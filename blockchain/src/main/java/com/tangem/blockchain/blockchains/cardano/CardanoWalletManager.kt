105d28a5d98dbe6ec98d01f402445ec570bcc477
Tangem <git@tangem.com>
2024-01-16 18:31:03 +0300

    releases/5.27.0-1144

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/CardanoWalletManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/CardanoWalletManager.kt
@@ -62,7 +62,7 @@ class CardanoWalletManager(
 
     private fun updateError(error: BlockchainError) {
         Log.e(this::class.java.simpleName, error.customMessage)
-        if (error is BlockchainSdkError) error("Error isn't BlockchainSdkError")
+        if (error is BlockchainSdkError) throw error
     }
 
     override suspend fun send(transactionData: TransactionData, signer: TransactionSigner): SimpleResult {
