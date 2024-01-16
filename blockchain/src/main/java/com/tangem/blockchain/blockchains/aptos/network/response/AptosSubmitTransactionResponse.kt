09f095b5a7a36cf6767a072ca09014e5a98306c0
Tangem <git@tangem.com>
2024-01-16 13:52:58 +0300

    Updated on 2025-08-28

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/aptos/network/response/AptosSubmitTransactionResponse.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/aptos/network/response/AptosSubmitTransactionResponse.kt
@@ -4,7 +4,7 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-internal data class SubmitTransactionResponse(
+internal data class AptosSubmitTransactionResponse(
     @Json(name = "hash") val hash: String,
     @Json(name = "sequence_number") val sequenceNumber: String,
 )
