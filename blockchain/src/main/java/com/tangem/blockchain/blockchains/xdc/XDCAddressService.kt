a72796948dd1d3d9b40d457c26a4a0a128d0bb7e
Tangem <git@tangem.com>
2024-01-19 14:27:32 +0100

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/xdc/XDCAddressService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/xdc/XDCAddressService.kt
@@ -1,25 +1,42 @@
 package com.tangem.blockchain.blockchains.xdc
 
 import com.tangem.blockchain.blockchains.ethereum.EthereumAddressService
+import com.tangem.blockchain.common.address.Address
+import com.tangem.blockchain.common.address.AddressType
 import com.tangem.common.card.EllipticCurve
 
 internal class XDCAddressService : EthereumAddressService() {
 
-    override fun makeAddress(walletPublicKey: ByteArray, curve: EllipticCurve?): String {
-        val ethAddress = super.makeAddress(walletPublicKey, curve)
+    override fun makeAddresses(walletPublicKey: ByteArray, curve: EllipticCurve?): Set<Address> {
+        return setOf(
+            Address(makeAddressWithXdcPrefix(walletPublicKey, curve), AddressType.Default),
+            Address(makeAddressWith0xPrefix(walletPublicKey, curve), AddressType.Legacy),
+        )
+    }
 
-        return makeWithXdcPrefix(ethAddress)
+    override fun makeAddress(walletPublicKey: ByteArray, curve: EllipticCurve?): String {
+        return makeAddressWithXdcPrefix(walletPublicKey, curve)
     }
 
     override fun validate(address: String): Boolean {
         return super.validate(address.replace(XDC_PREFIX, ETH_PREFIX))
     }
 
+    private fun makeAddressWith0xPrefix(walletPublicKey: ByteArray, curve: EllipticCurve?) : String {
+        return super.makeAddress(walletPublicKey, curve)
+    }
+
+    private fun makeAddressWithXdcPrefix(walletPublicKey: ByteArray, curve: EllipticCurve?) : String {
+        val ethAddress = makeAddressWith0xPrefix(walletPublicKey, curve)
+
+        return formatWithXdcPrefix(ethAddress)
+    }
+
     companion object {
         private const val ETH_PREFIX = "0x"
         private const val XDC_PREFIX = "xdc"
 
-        fun makeWith0xPrefix(address: String): String {
+        fun formatWith0xPrefix(address: String): String {
             return if (address.startsWith(XDC_PREFIX)) {
                 address.replace(XDC_PREFIX, ETH_PREFIX)
             } else {
@@ -27,7 +44,7 @@ internal class XDCAddressService : EthereumAddressService() {
             }
         }
 
-        fun makeWithXdcPrefix(address: String): String {
+        fun formatWithXdcPrefix(address: String): String {
             return if (address.startsWith(ETH_PREFIX)) {
                 address.replace(ETH_PREFIX, XDC_PREFIX)
             } else {
