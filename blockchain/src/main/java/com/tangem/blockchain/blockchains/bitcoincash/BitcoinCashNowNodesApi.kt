6db3b6cc52f5877de8b9386eba3145647d51fa7f
Tangem <git@tangem.com>
2024-01-31 10:44:46 +0100

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoincash/BitcoinCashNowNodesApi.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoincash/BitcoinCashNowNodesApi.kt
@@ -1,8 +1,9 @@
 package com.tangem.blockchain.blockchains.bitcoincash
 
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.adapter
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.tangem.blockchain.blockchains.bitcoincash.apiservice.BitcoinCashApiService
+import com.tangem.blockchain.blockchains.bitcoincash.apiservice.BitcoinCashBlockBookApiService
 import com.tangem.blockchain.blockchains.bitcoincash.network.BitconCashGetFeeResponse
 import com.tangem.blockchain.blockchains.bitcoincash.network.SendTransactionRequest
 import com.tangem.blockchain.extensions.AddHeaderInterceptor
@@ -11,21 +12,18 @@ import com.tangem.blockchain.network.blockbook.network.requests.GetFeeRequest
 import com.tangem.blockchain.network.blockbook.network.responses.GetAddressResponse
 import com.tangem.blockchain.network.blockbook.network.responses.GetUtxoResponseItem
 import com.tangem.blockchain.network.blockbook.network.responses.SendTransactionResponse
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.Request
-import okhttp3.RequestBody.Companion.toRequestBody
-import okhttp3.Response
-import ru.gildor.coroutines.okhttp.await
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import okhttp3.OkHttpClient
+import retrofit2.Response
 import java.io.IOException
 
-@OptIn(ExperimentalStdlibApi::class)
 internal class BitcoinCashNowNodesApi(
-    private val bchBookUrl: String,
-    private val bchUrl: String,
+    bchBookUrl: String,
+    bchUrl: String,
     credentials: Pair<String, String>,
 ) {
-
-    private val client = BlockchainSdkRetrofitBuilder.build(
+    private val client: OkHttpClient = BlockchainSdkRetrofitBuilder.build(
         internalInterceptors = listOf(
             AddHeaderInterceptor(mapOf("Content-Type" to "application/json")),
             AddHeaderInterceptor(mapOf(credentials)),
@@ -34,83 +32,55 @@ internal class BitcoinCashNowNodesApi(
 
     private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
+    private val retrofitBlockBook: Retrofit = Retrofit.Builder()
+        .client(client)
+        .baseUrl(bchBookUrl)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
+
+    private val retrofitBch: Retrofit = Retrofit.Builder()
+        .client(client)
+        .baseUrl(bchUrl)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
+
+    private val blockBookService: BitcoinCashBlockBookApiService =
+        retrofitBlockBook.create(BitcoinCashBlockBookApiService::class.java)
+    private val bchService: BitcoinCashApiService = retrofitBch.create(BitcoinCashApiService::class.java)
+
     suspend fun getAddress(address: String): GetAddressResponse {
-        return client
-            .newCall(
-                request = Request.Builder()
-                    .get()
-                    .url("${bchBookUrl}api/v2/address/$address?details=txs")
-                    .build(),
-            )
-            .await()
-            .unpack()
+        val response = blockBookService.getAddress(address)
+        return response.unpack()
     }
 
     suspend fun getFee(): BitconCashGetFeeResponse {
-        return client
-            .newCall(
-                request = Request.Builder()
-                    .post(
-                        moshi
-                            .adapter<GetFeeRequest>()
-                            .toJson(
-                                GetFeeRequest.getFee(
-                                    paramsList = emptyList(),
-                                    method = "estimatefee",
-                                ),
-                            )
-                            .toRequestBody(APPLICATION_JSON_MEDIA_TYPE.toMediaTypeOrNull()),
-                    )
-                    .url(bchUrl)
-                    .build(),
-            )
-            .await()
-            .unpack()
+        val request = GetFeeRequest.getFee(
+            paramsList = emptyList(),
+            method = "estimatefee",
+        )
+        val response = bchService.getFee(request)
+        return response.unpack()
     }
 
     suspend fun sendTransaction(txHex: String): SendTransactionResponse {
-        return client
-            .newCall(
-                request = Request.Builder()
-                    .post(
-                        moshi
-                            .adapter<SendTransactionRequest>()
-                            .toJson(SendTransactionRequest.getSendRequest(txHex))
-                            .toRequestBody(APPLICATION_JSON_MEDIA_TYPE.toMediaTypeOrNull()),
-                    )
-                    .url(bchUrl)
-                    .build(),
-            )
-            .await()
-            .unpack()
+        val request = SendTransactionRequest.getSendRequest(txHex)
+        val response = bchService.sendTransaction(request)
+        return response.unpack()
     }
 
     suspend fun getUtxo(address: String): List<GetUtxoResponseItem> {
-        return client
-            .newCall(
-                request = Request.Builder()
-                    .get()
-                    .url("$bchBookUrl/api/v2/utxo/$address")
-                    .build(),
-            )
-            .await()
-            .unpack<List<GetUtxoResponseItem>>()
-            .filter {
-                // filter unconfirmed UTXOs, to not block sending tx
-                it.confirmations > 0
-            }
-    }
-
-    private inline fun <reified T> Response.unpack(): T {
-        val responseBody = body?.string()
-        return if (isSuccessful && responseBody != null) {
-            moshi.adapter<T>().fromJson(responseBody) ?: throw IOException("Response is null")
-        } else {
-            throw IOException("Response is null")
+        val response = blockBookService.getUtxo(address)
+        return response.unpack<List<GetUtxoResponseItem>>().filter {
+            it.confirmations > 0  // filter unconfirmed UTXOs, to not block sending tx
         }
     }
 
-    private companion object {
-        const val APPLICATION_JSON_MEDIA_TYPE = "application/json"
+    private inline fun <reified T> Response<T>.unpack(): T {
+        val responseBody = body()
+        return if (isSuccessful && responseBody != null) {
+            responseBody
+        } else {
+            throw IOException("Error: ${errorBody()?.string()}")
+        }
     }
 }
