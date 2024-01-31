af84a1ca56d1d8aa9a809f6aa5b2db68a5380ee8
Tangem <git@tangem.com>
2024-01-31 10:44:46 +0100

    releases/5.27.0-1144

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoincash/apiservice/BitcoinCashApiService.kt
@@ -0,0 +1,21 @@
+package com.tangem.blockchain.blockchains.bitcoincash.apiservice
+
+import com.tangem.blockchain.blockchains.bitcoincash.network.BitconCashGetFeeResponse
+import com.tangem.blockchain.blockchains.bitcoincash.network.SendTransactionRequest
+import com.tangem.blockchain.network.blockbook.network.requests.GetFeeRequest
+import com.tangem.blockchain.network.blockbook.network.responses.SendTransactionResponse
+import retrofit2.http.POST
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.Response
+
+internal interface BitcoinCashApiService {
+
+    @Headers("Content-Type: application/json")
+    @POST("/")
+    suspend fun getFee(@Body request: GetFeeRequest): Response<BitconCashGetFeeResponse>
+
+    @Headers("Content-Type: application/json")
+    @POST("/")
+    suspend fun sendTransaction(@Body request: SendTransactionRequest): Response<SendTransactionResponse>
+}
