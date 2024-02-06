494c60552150f4e68f59a98ee7f8f57a7efb220e
Tangem <git@tangem.com>
2024-02-06 12:38:11 +0100

    Updated on 2025-12-22

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoincash/network/BitconCashGetFeeResponse.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoincash/network/BitconCashGetFeeResponse.kt
@@ -4,7 +4,7 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class BitconCashGetFeeResponse(
+internal data class BitconCashGetFeeResponse(
     @Json(name = "id") val id: String,
     @Json(name = "result") val result: Double,
 )
