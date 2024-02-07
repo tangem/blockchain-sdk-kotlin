70c65a1807339172e18f722f765df63a3b9a4587
Tangem <git@tangem.com>
2024-02-07 18:06:53 +0500

    releases/5.27.0-1144

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/algorand/AlgorandWalletManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/algorand/AlgorandWalletManager.kt
@@ -1,16 +1,38 @@
 package com.tangem.blockchain.blockchains.algorand
 
+import android.util.Log
+import com.tangem.blockchain.blockchains.algorand.models.AlgorandAccountModel
+import com.tangem.blockchain.blockchains.algorand.network.AlgorandNetworkService
 import com.tangem.blockchain.common.*
 import com.tangem.blockchain.common.transaction.TransactionFee
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.SimpleResult
 
-class AlgorandWalletManager(wallet: Wallet) : WalletManager(wallet) {
-    override val currentHost: String
-        get() = TODO("Not yet implemented")
+internal class AlgorandWalletManager(
+    wallet: Wallet,
+    private val networkService: AlgorandNetworkService,
+) : WalletManager(wallet) {
+    override val currentHost: String get() = networkService.host
 
     override suspend fun updateInternal() {
-        TODO("Not yet implemented")
+        when (val result = networkService.getAccount(wallet.address)) {
+            is Result.Success -> updateWallet(result.data)
+            is Result.Failure -> updateError(result.error)
+        }
+    }
+
+    private fun updateWallet(accountModel: AlgorandAccountModel) {
+        wallet.setCoinValue(accountModel.coinValue)
+        wallet.setReserveValue(accountModel.reserveValue)
+
+        if (accountModel.coinValue < accountModel.existentialDeposit) {
+            updateError(BlockchainSdkError.AccountNotFound)
+        }
+    }
+
+    private fun updateError(error: BlockchainError) {
+        Log.e(this::class.java.simpleName, error.customMessage)
+        if (error is BlockchainSdkError) throw error
     }
 
     override suspend fun send(transactionData: TransactionData, signer: TransactionSigner): SimpleResult {
