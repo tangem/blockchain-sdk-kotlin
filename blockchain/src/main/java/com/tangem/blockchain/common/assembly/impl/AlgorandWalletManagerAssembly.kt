625402892d6678396a70fc7bf13923b297c479a4
Tangem <git@tangem.com>
2024-02-07 18:06:53 +0500

    Updated on 2025-08-28

--- blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/AlgorandWalletManagerAssembly.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/AlgorandWalletManagerAssembly.kt
@@ -1,14 +1,24 @@
 package com.tangem.blockchain.common.assembly.impl
 
+import com.tangem.blockchain.blockchains.algorand.AlgorandNetworkProvidersBuilder
 import com.tangem.blockchain.blockchains.algorand.AlgorandWalletManager
+import com.tangem.blockchain.blockchains.algorand.network.AlgorandNetworkService
 import com.tangem.blockchain.common.assembly.WalletManagerAssembly
 import com.tangem.blockchain.common.assembly.WalletManagerAssemblyInput
 
 internal object AlgorandWalletManagerAssembly : WalletManagerAssembly<AlgorandWalletManager>() {
 
     override fun make(input: WalletManagerAssemblyInput): AlgorandWalletManager {
+        val blockchain = input.wallet.blockchain
         return AlgorandWalletManager(
             wallet = input.wallet,
+            networkService = AlgorandNetworkService(
+                networkProviders = AlgorandNetworkProvidersBuilder(
+                    blockchain = blockchain,
+                    config = input.config,
+                ).build(),
+                blockchain = blockchain,
+            )
         )
     }
 }
