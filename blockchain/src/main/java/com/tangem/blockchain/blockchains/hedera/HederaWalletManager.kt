45cfeb1c9ee0288eb6c04aafd176dc23e224d23d
Tangem <git@tangem.com>
2024-02-12 20:12:06 +0400

    Updated on 2025-11-03

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/hedera/HederaWalletManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/hedera/HederaWalletManager.kt
@@ -23,6 +23,7 @@ internal class HederaWalletManager(
     private val transactionBuilder: HederaTransactionBuilder,
     private val networkProvider: HederaNetworkProvider,
     private val dataStorage: AdvancedDataStorage,
+    private val accountCreator: AccountCreator
 ) : WalletManager(wallet), TransactionSender {
 
     private val blockchain = wallet.blockchain
@@ -37,6 +38,7 @@ internal class HederaWalletManager(
             is Result.Failure -> updateError(getAccountIdResult.error)
         }
     }
+
 
     private fun updateBalance(accountId: String) {
         try {
@@ -98,39 +100,55 @@ internal class HederaWalletManager(
     }
 
     private suspend fun getAccountId(): Result<String> {
-        val accountId = wallet.address.ifBlank {
-            dataStorage.getOrNull<BlockchainSavedData.Hedera>(wallet.publicKey)?.accountId.orEmpty()
-        }
-
-        return if (accountId.isEmpty()) {
-            when (
-                val getAccountIdResult =
-                    networkProvider.getAccountId(wallet.publicKey.blockchainKey.toCompressedPublicKey())
-            ) {
+        return if (wallet.address.isBlank()) {
+            return when (val getCachedAccountIdResult = getCachedAccountId()) {
                 is Result.Success -> {
-                    val fetchedAccountId = getAccountIdResult.data
-                    wallet.addresses = setOf(Address(fetchedAccountId))
+                    wallet.addresses = setOf(Address(getCachedAccountIdResult.data))
 
-                    dataStorage.store(wallet.publicKey, BlockchainSavedData.Hedera(fetchedAccountId))
-
-                    getAccountIdResult
+                    getCachedAccountIdResult
                 }
-                is Result.Failure -> {
-                    if (getAccountIdResult.error is BlockchainSdkError.AccountNotFound) {
-                        requestCreateAccount()
-                    } else {
-                        getAccountIdResult
-                    }
-                }
+                is Result.Failure -> getCachedAccountIdResult
             }
         } else {
-            Result.Success(accountId)
+            Result.Success(wallet.address)
         }
     }
 
-    private fun requestCreateAccount(): Result<String> {
-        // TODO create account with our backend service
-        return Result.Failure(BlockchainSdkError.CustomError("Account creation is not implemented yet"))
+    private suspend fun getCachedAccountId(): Result<String> {
+        val cachedAccountId = dataStorage.getOrNull<BlockchainSavedData.Hedera>(wallet.publicKey)?.accountId
+
+        return if (cachedAccountId.isNullOrBlank()) {
+            return when (val fetchAccountIdResult = fetchAccountId()) {
+                is Result.Success -> {
+                    dataStorage.store(wallet.publicKey, BlockchainSavedData.Hedera(fetchAccountIdResult.data))
+
+                    fetchAccountIdResult
+                }
+                is Result.Failure -> fetchAccountIdResult
+            }
+        } else {
+            Result.Success(cachedAccountId)
+        }
+    }
+
+    private suspend fun fetchAccountId(): Result<String> {
+        return when (
+            val getAccountIdResult =
+                networkProvider.getAccountId(wallet.publicKey.blockchainKey.toCompressedPublicKey())
+        ) {
+            is Result.Success -> getAccountIdResult
+            is Result.Failure -> {
+                if (getAccountIdResult.error is BlockchainSdkError.AccountNotFound) {
+                    requestCreateAccount()
+                } else {
+                    getAccountIdResult
+                }
+            }
+        }
+    }
+
+    private suspend fun requestCreateAccount(): Result<String> {
+        return accountCreator.createAccount(blockchain = blockchain, walletPublicKey = wallet.publicKey.blockchainKey)
     }
 
     companion object {
