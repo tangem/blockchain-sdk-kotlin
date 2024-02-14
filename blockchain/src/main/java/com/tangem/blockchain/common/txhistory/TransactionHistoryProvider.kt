ef02f0013b6bb93f4ceacf3b36d684a68974fecb
Tangem <git@tangem.com>
2024-02-14 15:10:44 +0500

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/common/txhistory/TransactionHistoryProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/txhistory/TransactionHistoryProvider.kt
@@ -1,6 +1,6 @@
 package com.tangem.blockchain.common.txhistory
 
-import com.tangem.blockchain.common.PaginationWrapper
+import com.tangem.blockchain.common.pagination.PaginationWrapper
 import com.tangem.blockchain.extensions.Result
 
 interface TransactionHistoryProvider {
