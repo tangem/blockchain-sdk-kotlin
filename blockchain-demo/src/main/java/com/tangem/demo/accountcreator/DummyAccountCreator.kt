1e5c1472ef2f94106800b21b64b558abd59ddc84
Tangem <git@tangem.com>
2024-02-14 17:16:58 +0300

    Updated on 2025-08-28

--- blockchain-demo/src/main/java/com/tangem/demo/accountcreator/DummyAccountCreator.kt
+++ blockchain-demo/src/main/java/com/tangem/demo/accountcreator/DummyAccountCreator.kt
@@ -2,11 +2,11 @@ package com.tangem.demo.accountcreator
 
 import com.tangem.blockchain.common.AccountCreator
 import com.tangem.blockchain.common.Blockchain
-import com.tangem.blockchain.common.toBlockchainSdkError
+import com.tangem.blockchain.common.BlockchainSdkError
 import com.tangem.blockchain.extensions.Result
 
 object DummyAccountCreator : AccountCreator {
 
     override suspend fun createAccount(blockchain: Blockchain, walletPublicKey: ByteArray): Result<String> =
-        Result.Failure(Exception("Dummy account creator").toBlockchainSdkError())
+        Result.Failure(BlockchainSdkError.WrappedThrowable(NotImplementedError("Dummy account creator")))
 }
