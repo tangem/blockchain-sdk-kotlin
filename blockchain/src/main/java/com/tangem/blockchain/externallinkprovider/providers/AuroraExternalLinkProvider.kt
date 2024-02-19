cb6efdb874d5271ab2845989639cb2a9e35d8b94
Tangem <git@tangem.com>
2024-02-19 17:43:38 +0300

    Updated on 2025-11-28

---
+++ blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/AuroraExternalLinkProvider.kt
@@ -0,0 +1,23 @@
+package com.tangem.blockchain.externallinkprovider.providers
+
+import com.tangem.blockchain.externallinkprovider.ExternalLinkProvider
+import com.tangem.blockchain.externallinkprovider.TxExploreState
+
+internal class AuroraExternalLinkProvider(isTestnet: Boolean) : ExternalLinkProvider {
+
+    override val testNetTopUpUrl: String = "https://aurora.dev/faucet/"
+
+    override val explorerBaseUrl: String = if (isTestnet) {
+        "https://explorer.testnet.aurora.dev/"
+    } else {
+        "https://explorer.aurora.dev/"
+    }
+
+    override fun explorerUrl(walletAddress: String, contractAddress: String?): String {
+        return "${explorerBaseUrl}address/$walletAddress"
+    }
+
+    override fun getExplorerTxUrl(transactionHash: String): TxExploreState {
+        return TxExploreState.Url(url = "${explorerBaseUrl}tx/$transactionHash")
+    }
+}
