224a1e87475a18302d395305d51098df3347d8d3
Tangem <git@tangem.com>
2024-02-21 19:43:26 +0000

    Updated on 2025-11-28

--- blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/TronExternalLinkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/TronExternalLinkProvider.kt
@@ -1,6 +1,7 @@
 package com.tangem.blockchain.externallinkprovider.providers
 
 import com.tangem.blockchain.externallinkprovider.ExternalLinkProvider
+import com.tangem.blockchain.externallinkprovider.TxExploreState
 
 internal class TronExternalLinkProvider(isTestnet: Boolean) : ExternalLinkProvider {
 
@@ -12,7 +13,7 @@ internal class TronExternalLinkProvider(isTestnet: Boolean) : ExternalLinkProvid
         return explorerBaseUrl + "address/$walletAddress"
     }
 
-    override fun explorerTransactionUrl(transactionHash: String): String {
-        return explorerBaseUrl + "transaction/$transactionHash"
+    override fun getExplorerTxUrl(transactionHash: String): TxExploreState {
+        return TxExploreState.Url(explorerBaseUrl + "transaction/$transactionHash")
     }
 }
