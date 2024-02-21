0d83489570402d6735c11377cc0a64ef84237499
Tangem <git@tangem.com>
2024-02-21 19:43:26 +0000

    Updated on 2026-03-11

--- blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/GnosisExternalLinkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/GnosisExternalLinkProvider.kt
@@ -1,6 +1,7 @@
 package com.tangem.blockchain.externallinkprovider.providers
 
 import com.tangem.blockchain.externallinkprovider.ExternalLinkProvider
+import com.tangem.blockchain.externallinkprovider.TxExploreState
 
 internal class GnosisExternalLinkProvider : ExternalLinkProvider {
 
@@ -10,7 +11,7 @@ internal class GnosisExternalLinkProvider : ExternalLinkProvider {
         return explorerBaseUrl + "address/$walletAddress"
     }
 
-    override fun explorerTransactionUrl(transactionHash: String): String {
-        return explorerBaseUrl + "tx/$transactionHash"
+    override fun getExplorerTxUrl(transactionHash: String): TxExploreState {
+        return TxExploreState.Url(explorerBaseUrl + "tx/$transactionHash")
     }
 }
