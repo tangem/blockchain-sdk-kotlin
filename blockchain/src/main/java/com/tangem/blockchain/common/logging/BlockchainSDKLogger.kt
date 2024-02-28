7432281ec1e189ea073ce77e3afd4216f334cb82
Tangem <git@tangem.com>
2024-02-28 14:27:32 +0800

    Updated on 2026-07-23

---
+++ blockchain/src/main/java/com/tangem/blockchain/common/logging/BlockchainSDKLogger.kt
@@ -0,0 +1,15 @@
+package com.tangem.blockchain.common.logging
+
+/**
+ * External logger
+ *
+[REDACTED_AUTHOR]
+ */
+interface BlockchainSDKLogger {
+
+    fun log(level: Level, message: String)
+
+    enum class Level {
+        NETWORK,
+    }
+}
