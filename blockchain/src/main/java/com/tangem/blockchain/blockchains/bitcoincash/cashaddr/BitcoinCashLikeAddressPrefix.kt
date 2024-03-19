56198400e7f57f5669e6af492226d23079c9acdb
Tangem <git@tangem.com>
2024-03-19 17:17:52 +0300

    Updated on 2026-06-27

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoincash/cashaddr/BitcoinCashLikeAddressPrefix.kt
+++
@@ -1,8 +0,0 @@
-package com.tangem.blockchain.blockchains.bitcoincash.cashaddr
-
-internal enum class BitcoinCashLikeAddressPrefix(val addressPrefix: String) {
-    BitcoinCash("bitcoincash"),
-    BitcoinCashTestnet("bchtest"),
-    Nexa("nexa"),
-    NexaTestnet("nexatest"),
-}
