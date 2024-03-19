60edff73a2ef9dbd454deec76c9a2516b21e2437
Tangem <git@tangem.com>
2024-03-19 17:17:52 +0300

    Updated on 2025-11-28

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/nexa/NexaAddressService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/nexa/NexaAddressService.kt
@@ -1,9 +1,7 @@
 package com.tangem.blockchain.blockchains.nexa
 
-import com.tangem.blockchain.blockchains.bitcoincash.cashaddr.BitcoinCashAddressType
-import com.tangem.blockchain.blockchains.bitcoincash.cashaddr.BitcoinCashLikeAddressPrefix
-import com.tangem.blockchain.blockchains.bitcoincash.cashaddr.CashAddr
-import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchain.blockchains.nexa.cashaddr.NexaAddr
+import com.tangem.blockchain.blockchains.nexa.cashaddr.NexaAddressType
 import com.tangem.blockchain.common.address.Address
 import com.tangem.blockchain.common.address.AddressService
 import com.tangem.blockchain.common.address.AddressType
@@ -11,28 +9,65 @@ import com.tangem.common.card.EllipticCurve
 import com.tangem.common.extensions.calculateRipemd160
 import com.tangem.common.extensions.calculateSha256
 import com.tangem.common.extensions.toCompressedPublicKey
+import com.tangem.common.extensions.toHexString
+import org.bitcoinj.script.ScriptBuilder
+import org.bitcoinj.script.ScriptChunk
+import org.bitcoinj.script.ScriptOpCodes
 
 class NexaAddressService(
-    blockchain: Blockchain,
+    isTestNet: Boolean,
 ) : AddressService() {
 
-    private val cashAddr = when (blockchain) {
-        Blockchain.Nexa -> CashAddr(BitcoinCashLikeAddressPrefix.Nexa)
-        Blockchain.NexaTestnet -> CashAddr(BitcoinCashLikeAddressPrefix.NexaTestnet)
-        else -> error("${blockchain.fullName} blockchain is not supported by ${this::class.simpleName}")
-    }
+    private val cashAddr = NexaAddr(isTestNet)
+
     override fun makeAddress(walletPublicKey: ByteArray, curve: EllipticCurve?) =
-        makeCashAddrAddress(walletPublicKey).value
+        makeTemplateNexaAddress(walletPublicKey).value
 
     override fun validate(address: String) = cashAddr.isValidCashAddress(address)
 
-    private fun makeCashAddrAddress(walletPublicKey: ByteArray): Address {
-        val publicKeyHash = walletPublicKey.toCompressedPublicKey().calculateSha256().calculateRipemd160()
-        val address = cashAddr.toCashAddress(BitcoinCashAddressType.P2PKH, publicKeyHash)
+    fun getPublicKey(address: String): ByteArray {
+        return cashAddr.decodeNexaAddress(address).hash
+    }
+
+    /**
+     * @see <a href="https://explorer.nexa.org/">explorer</a>
+     * @return script public key hash (as in the Explorer)
+     */
+    fun getScriptPublicKey(address: String): ByteArray {
+        return cashAddr.decodeNexaAddress(address).scriptPublicKeyHash
+    }
+
+    private fun makeTemplateNexaAddress(walletPublicKey: ByteArray): Address {
+        val scriptTemplate = scriptTemplate(walletPublicKey)
+
+        val hash = ScriptBuilder()
+            .data(scriptTemplate)
+            .build().program
+
+        val address = cashAddr.toNexaAddress(NexaAddressType.TEMPLATE, hash)
+
         return Address(address, AddressType.Default)
     }
 
-    fun getPublicKeyHash(address: String): ByteArray {
-        return cashAddr.decodeCashAddress(address).hash
+    companion object {
+        fun getScriptHash(walletPublicKey: ByteArray): String {
+            return scriptTemplate(walletPublicKey).calculateSha256().reversedArray().toHexString()
+        }
+
+        private fun scriptTemplate(walletPublicKey: ByteArray): ByteArray {
+            val publicKeyHash = walletPublicKey.toCompressedPublicKey()
+
+            val constraint = ScriptBuilder()
+                .data(publicKeyHash)
+                .build().program
+
+            val constraintHash = constraint.calculateSha256().calculateRipemd160()
+
+            return ScriptBuilder()
+                .addChunk(ScriptChunk(ScriptOpCodes.OP_FALSE, null))
+                .addChunk(ScriptChunk(ScriptOpCodes.OP_1, null))
+                .data(constraintHash)
+                .build().program
+        }
     }
 }
