8322d4c70c68427a3adb13dfef4aea0add329c30
Tangem <git@tangem.com>
2024-03-25 13:16:37 +0500

    Updated on 2025-11-03

--- blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/PolkadotWalletManagerAssembly.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/PolkadotWalletManagerAssembly.kt
@@ -1,9 +1,11 @@
 package com.tangem.blockchain.common.assembly.impl
 
 import com.tangem.blockchain.blockchains.polkadot.PolkadotWalletManager
+import com.tangem.blockchain.blockchains.polkadot.extensions.getPolkadotExtrinsicCheckHost
 import com.tangem.blockchain.blockchains.polkadot.extensions.getPolkadotHosts
 import com.tangem.blockchain.blockchains.polkadot.network.PolkadotCombinedProvider
 import com.tangem.blockchain.blockchains.polkadot.network.PolkadotNetworkService
+import com.tangem.blockchain.blockchains.polkadot.network.accounthealthcheck.PolkadotAccountHealthCheckNetworkService
 import com.tangem.blockchain.common.assembly.WalletManagerAssembly
 import com.tangem.blockchain.common.assembly.WalletManagerAssemblyInput
 
@@ -15,6 +17,9 @@ internal object PolkadotWalletManagerAssembly : WalletManagerAssembly<PolkadotWa
             networkProvider = PolkadotNetworkService(
                 providers = input.wallet.blockchain.getPolkadotHosts()
                     .map { PolkadotCombinedProvider(input.wallet.blockchain.decimals(), it) },
+            ),
+            extrinsicCheckNetworkProvider = PolkadotAccountHealthCheckNetworkService(
+                input.wallet.blockchain.getPolkadotExtrinsicCheckHost(),
             ),
         )
     }
