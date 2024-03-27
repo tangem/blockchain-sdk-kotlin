4a267492daae10cc9c0d83fb99a66cffa7e26fee
Tangem <git@tangem.com>
2024-03-27 18:31:40 +0800

    Updated on 2025-08-28

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/CardanoUnspentOutput.kt
+++
@@ -1,8 +0,0 @@
-package com.tangem.blockchain.blockchains.cardano
-
-class CardanoUnspentOutput(
-    val address: String,
-    val amount: Long,
-    val outputIndex: Long,
-    val transactionHash: ByteArray,
-)
