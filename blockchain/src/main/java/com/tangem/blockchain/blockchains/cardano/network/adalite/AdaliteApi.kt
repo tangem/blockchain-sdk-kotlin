5551b4367f3739bca76d1834bf336531361bbfd4
Tangem <git@tangem.com>
2024-03-27 18:31:40 +0800

    Updated on 2026-06-27

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/network/adalite/AdaliteApi.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/network/adalite/AdaliteApi.kt
@@ -1,16 +1,20 @@
 package com.tangem.blockchain.blockchains.cardano.network.adalite
 
+import com.tangem.blockchain.blockchains.cardano.network.adalite.repsonse.AdaliteAddressResponse
+import com.tangem.blockchain.blockchains.cardano.network.adalite.repsonse.AdaliteUnspentOutputsResponse
+import com.tangem.blockchain.blockchains.cardano.network.adalite.request.AdaliteSendBody
 import retrofit2.http.*
 
-interface AdaliteApi {
+internal interface AdaliteApi {
+
     @GET("api/addresses/summary/{address}")
-    suspend fun getAddressData(@Path("address") address: String): AdaliteAddress
+    suspend fun getAddressData(@Path("address") address: String): AdaliteAddressResponse
 
     @Headers("Content-Type: application/json")
     @POST("api/bulk/addresses/utxo")
-    suspend fun getUnspents(@Body addresses: List<String>): AdaliteUnspents
+    suspend fun getUnspentOutputs(@Body addresses: List<String>): AdaliteUnspentOutputsResponse
 
     @Headers("Content-Type: application/json")
     @POST("api/v2/txs/signed")
-    suspend fun sendTransaction(@Body adaliteBody: AdaliteSendBody): String // List<Any>?
+    suspend fun sendTransaction(@Body body: AdaliteSendBody): String
 }
