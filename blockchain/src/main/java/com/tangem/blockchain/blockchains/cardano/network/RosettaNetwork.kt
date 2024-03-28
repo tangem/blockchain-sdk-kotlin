6ab489a2b2d769cc3969181b6a080f2d71f5078f
Tangem <git@tangem.com>
2024-03-28 17:04:06 +0800

    Updated on 2026-01-28

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/network/RosettaNetwork.kt
+++
@@ -1,7 +0,0 @@
-package com.tangem.blockchain.blockchains.cardano.network
-
-sealed class RosettaNetwork(val url: String) {
-
-    data class RosettaGetblock(val accessToken: String) : RosettaNetwork(url = "https://go.getblock.io/$accessToken/")
-    object RosettaTangem : RosettaNetwork(url = "https://ada.tangem.com/")
-}
