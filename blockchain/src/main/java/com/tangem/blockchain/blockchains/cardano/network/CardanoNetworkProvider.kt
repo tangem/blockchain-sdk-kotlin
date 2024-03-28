0677430e89716bac9163fab77c9714192aee0a7b
Tangem <git@tangem.com>
2024-03-28 19:03:46 +0800

    Updated on 2026-02-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/network/CardanoNetworkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/network/CardanoNetworkProvider.kt
@@ -1,11 +1,12 @@
 package com.tangem.blockchain.blockchains.cardano.network
 
+import com.tangem.blockchain.blockchains.cardano.network.common.models.CardanoAddressResponse
 import com.tangem.blockchain.common.NetworkProvider
 import com.tangem.blockchain.common.Token
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.SimpleResult
 
-interface CardanoNetworkProvider : NetworkProvider {
+internal interface CardanoNetworkProvider : NetworkProvider {
 
     suspend fun getInfo(input: InfoInput): Result<CardanoAddressResponse>
 
@@ -13,25 +14,3 @@ interface CardanoNetworkProvider : NetworkProvider {
 }
 
 data class InfoInput(val addresses: Set<String>, val tokens: Set<Token>)
-
-data class CardanoAddressResponse(
-    val balance: Long,
-    val tokenBalances: Map<Token, Long> = emptyMap(),
-    val unspentOutputs: List<CardanoUnspentOutput>,
-    val recentTransactionsHashes: List<String>,
-)
-
-class CardanoUnspentOutput(
-    val address: String,
-    val amount: Long,
-    val outputIndex: Long,
-    val transactionHash: ByteArray,
-    val assets: List<Asset> = emptyList(),
-) {
-
-    data class Asset(
-        val policyID: String,
-        val assetNameHex: String,
-        val amount: Long,
-    )
-}
--- blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/network/adalite/converters/AdaliteUnspentOutputConverter.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/network/adalite/converters/AdaliteUnspentOutputConverter.kt
@@ -1,28 +1,28 @@
 package com.tangem.blockchain.blockchains.cardano.network.adalite.converters
 
-import com.tangem.blockchain.blockchains.cardano.network.CardanoUnspentOutput
-import com.tangem.blockchain.blockchains.cardano.network.adalite.repsonse.AdaliteBalance
 import com.tangem.blockchain.blockchains.cardano.network.adalite.repsonse.AdaliteUnspentOutputsResponse
+import com.tangem.blockchain.blockchains.cardano.network.common.models.CardanoUnspentOutput
 import com.tangem.common.extensions.hexToBytes
+import com.tangem.blockchain.blockchains.cardano.network.adalite.repsonse.AdaliteUnspentOutputsResponse.Utxo.Amount.Token as AdaliteUtxoToken
 
 internal object AdaliteUnspentOutputConverter {
 
     fun convert(response: AdaliteUnspentOutputsResponse): List<CardanoUnspentOutput> {
         return response.successData
             // we need to ignore unspent outputs with tokens (until we start supporting tokens)
-            .filter { it.amountData.tokens.isEmpty() }
+            .filter { it.amount.tokens.isEmpty() }
             .map { utxo ->
                 CardanoUnspentOutput(
                     address = utxo.address,
-                    amount = utxo.amountData.amount,
+                    amount = utxo.amount.value,
                     outputIndex = utxo.outputIndex.toLong(),
                     transactionHash = utxo.hash.hexToBytes(),
-                    assets = utxo.amountData.tokens.mapToAsset(),
+                    assets = utxo.amount.tokens.mapToAsset(),
                 )
             }
     }
 
-    private fun List<AdaliteBalance.Token>.mapToAsset(): List<CardanoUnspentOutput.Asset> {
+    private fun List<AdaliteUtxoToken>.mapToAsset(): List<CardanoUnspentOutput.Asset> {
         return map { token ->
             CardanoUnspentOutput.Asset(
                 policyID = token.policyId,
