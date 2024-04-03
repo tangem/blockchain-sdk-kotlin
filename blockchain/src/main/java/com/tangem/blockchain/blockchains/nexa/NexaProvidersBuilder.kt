2d2defd8bc428b398fd1bfef7094a5e546c76b70
Tangem <git@tangem.com>
2024-04-03 21:22:56 +0800

    releases/5.27.0-1144

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/nexa/NexaProvidersBuilder.kt
@@ -0,0 +1,21 @@
+package com.tangem.blockchain.blockchains.nexa
+
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchain.common.network.providers.NetworkProvidersBuilder
+import com.tangem.blockchain.network.electrum.ElectrumNetworkProvider
+import com.tangem.blockchain.network.electrum.ElectrumNetworkProviderFactory
+
+internal object NexaProvidersBuilder : NetworkProvidersBuilder<ElectrumNetworkProvider>() {
+
+    override val supportedBlockchains: List<Blockchain> = listOf(Blockchain.Nexa, Blockchain.NexaTestnet)
+
+    override fun createProviders(blockchain: Blockchain): List<ElectrumNetworkProvider> {
+        return listOf(
+            ElectrumNetworkProviderFactory.create(
+                wssUrl = "wss://onekey-electrum.bitcoinunlimited.info:20004",
+                blockchain = blockchain,
+            ),
+            ElectrumNetworkProviderFactory.create(wssUrl = "wss://electrum.nexa.org:20004", blockchain = blockchain),
+        )
+    }
+}
