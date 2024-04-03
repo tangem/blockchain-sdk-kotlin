a18bcbcf2bc2d0dc0230abea8c6906d3f5b9dd19
Tangem <git@tangem.com>
2024-04-03 21:22:56 +0800

    Updated on 2026-06-27

--- blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/RadiantWalletManagerAssembly.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/RadiantWalletManagerAssembly.kt
@@ -1,13 +1,14 @@
 package com.tangem.blockchain.common.assembly.impl
 
+import com.tangem.blockchain.blockchains.radiant.RadiantProvidersBuilder
 import com.tangem.blockchain.blockchains.radiant.RadiantWalletManager
 import com.tangem.blockchain.common.assembly.WalletManagerAssembly
 import com.tangem.blockchain.common.assembly.WalletManagerAssemblyInput
-import com.tangem.blockchain.network.electrum.getElectrumNetworkProviders
 
 internal object RadiantWalletManagerAssembly : WalletManagerAssembly<RadiantWalletManager>() {
+
     override fun make(input: WalletManagerAssemblyInput): RadiantWalletManager = RadiantWalletManager(
         wallet = input.wallet,
-        networkProviders = input.wallet.blockchain.getElectrumNetworkProviders(),
+        networkProviders = RadiantProvidersBuilder.build(input.wallet.blockchain),
     )
 }
