2a34ea3a9ef52b91265feb4f0556df236688246a
Tangem <git@tangem.com>
2024-04-03 21:42:50 +0800

    Updated on 2025-11-03

--- blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/DashWalletManagerAssembly.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/DashWalletManagerAssembly.kt
@@ -2,8 +2,8 @@ package com.tangem.blockchain.common.assembly.impl
 
 import com.tangem.blockchain.blockchains.bitcoin.BitcoinTransactionBuilder
 import com.tangem.blockchain.blockchains.bitcoin.BitcoinWalletManager
-import com.tangem.blockchain.blockchains.bitcoin.getBitcoinNetworkProviders
 import com.tangem.blockchain.blockchains.bitcoin.network.BitcoinNetworkService
+import com.tangem.blockchain.blockchains.dash.DashProvidersBuilder
 import com.tangem.blockchain.blockchains.dash.DashWalletManager
 import com.tangem.blockchain.common.assembly.WalletManagerAssembly
 import com.tangem.blockchain.common.assembly.WalletManagerAssemblyInput
@@ -21,7 +21,7 @@ internal object DashWalletManagerAssembly : WalletManagerAssembly<BitcoinWalletM
                     walletAddresses = addresses,
                 ),
                 networkProvider = BitcoinNetworkService(
-                    providers = blockchain.getBitcoinNetworkProviders(blockchain, input.config),
+                    providers = DashProvidersBuilder(input.config).build(blockchain),
                 ),
                 transactionHistoryProvider = blockchain.getTransactionHistoryProvider(input.config),
             )
