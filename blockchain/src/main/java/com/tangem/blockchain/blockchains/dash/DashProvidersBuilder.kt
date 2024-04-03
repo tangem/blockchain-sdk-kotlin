4710a6a400d24aa176ea8e48cea3910982f6ea7f
Tangem <git@tangem.com>
2024-04-03 21:42:50 +0800

    Updated on 2026-06-27

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/dash/DashProvidersBuilder.kt
@@ -0,0 +1,28 @@
+package com.tangem.blockchain.blockchains.dash
+
+import com.tangem.blockchain.blockchains.bitcoin.network.BitcoinNetworkProvider
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchain.common.BlockchainSdkConfig
+import com.tangem.blockchain.common.network.providers.NetworkProvidersBuilder
+import com.tangem.blockchain.network.blockbook.BlockBookNetworkProviderFactory
+import com.tangem.blockchain.network.blockchair.BlockchairNetworkProviderFactory
+import com.tangem.blockchain.network.blockcypher.BlockcypherNetworkProviderFactory
+
+internal class DashProvidersBuilder(
+    private val config: BlockchainSdkConfig,
+) : NetworkProvidersBuilder<BitcoinNetworkProvider>() {
+
+    override val supportedBlockchains: List<Blockchain> = listOf(Blockchain.Dash)
+
+    private val blockBookNetworkProviderFactory by lazy { BlockBookNetworkProviderFactory(config) }
+    private val blockchairNetworkProviderFactory by lazy { BlockchairNetworkProviderFactory(config) }
+    private val blockcypherNetworkProviderFactory by lazy { BlockcypherNetworkProviderFactory(config) }
+
+    override fun createProviders(blockchain: Blockchain): List<BitcoinNetworkProvider> {
+        return listOfNotNull(
+            blockBookNetworkProviderFactory.createNowNodesProvider(blockchain),
+            *blockchairNetworkProviderFactory.createProviders(blockchain).toTypedArray(),
+            blockcypherNetworkProviderFactory.create(blockchain),
+        )
+    }
+}
