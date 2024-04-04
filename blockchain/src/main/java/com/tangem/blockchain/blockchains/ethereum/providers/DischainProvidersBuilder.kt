a7ed21767c3db3818105c1bbec49afe8d651bc5d
Tangem <git@tangem.com>
2024-04-04 22:07:48 +0800

    Updated on 2025-10-09

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/providers/DischainProvidersBuilder.kt
@@ -0,0 +1,19 @@
+package com.tangem.blockchain.blockchains.ethereum.providers
+
+import com.tangem.blockchain.blockchains.ethereum.EthereumLikeProvidersBuilder
+import com.tangem.blockchain.blockchains.ethereum.network.EthereumJsonRpcProvider
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchain.common.BlockchainSdkConfig
+
+internal class DischainProvidersBuilder(
+    override val config: BlockchainSdkConfig,
+) : EthereumLikeProvidersBuilder(config) {
+
+    override val supportedBlockchains: List<Blockchain> = listOf(Blockchain.Dischain)
+
+    override fun createProviders(blockchain: Blockchain): List<EthereumJsonRpcProvider> {
+        return listOf(
+            EthereumJsonRpcProvider(baseUrl = "https://rpc.dischain.xyz/"),
+        )
+    }
+}
