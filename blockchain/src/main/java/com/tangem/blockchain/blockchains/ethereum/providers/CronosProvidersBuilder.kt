15bc4d2e464d95abb3474f5e8b77419e4d183d05
Tangem <git@tangem.com>
2024-04-04 22:07:48 +0800

    Updated on 2026-02-11

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/providers/CronosProvidersBuilder.kt
@@ -0,0 +1,23 @@
+package com.tangem.blockchain.blockchains.ethereum.providers
+
+import com.tangem.blockchain.blockchains.ethereum.EthereumLikeProvidersBuilder
+import com.tangem.blockchain.blockchains.ethereum.network.EthereumJsonRpcProvider
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchain.common.BlockchainSdkConfig
+
+internal class CronosProvidersBuilder(
+    override val config: BlockchainSdkConfig,
+) : EthereumLikeProvidersBuilder(config) {
+
+    override val supportedBlockchains: List<Blockchain> = listOf(Blockchain.Cronos)
+
+    override fun createProviders(blockchain: Blockchain): List<EthereumJsonRpcProvider> {
+        return listOfNotNull(
+            EthereumJsonRpcProvider(baseUrl = "https://evm.cronos.org/"),
+            EthereumJsonRpcProvider(baseUrl = "https://evm-cronos.crypto.org/"),
+            ethereumProviderFactory.getGetBlockProvider { cronos?.jsonRpc },
+            EthereumJsonRpcProvider(baseUrl = "https://cronos.blockpi.network/v1/rpc/public/"),
+            EthereumJsonRpcProvider(baseUrl = "https://cronos-evm.publicnode.com/"),
+        )
+    }
+}
