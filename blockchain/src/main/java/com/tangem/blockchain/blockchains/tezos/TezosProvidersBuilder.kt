a9d400d835c9b8e2d115286f1ae811eff53b9f63
Mama1emon <andrew.khokhlove@gmail.com>
2024-04-10 23:22:14 +0800

    AND-6638 Create providers according to the list of provider types (1)

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/tezos/TezosProvidersBuilder.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/tezos/TezosProvidersBuilder.kt
@@ -2,19 +2,12 @@ package com.tangem.blockchain.blockchains.tezos
 
 import com.tangem.blockchain.blockchains.tezos.network.TezosJsonRpcNetworkProvider
 import com.tangem.blockchain.blockchains.tezos.network.TezosNetworkProvider
-import com.tangem.blockchain.common.Blockchain
-import com.tangem.blockchain.common.network.providers.NetworkProvidersBuilder
+import com.tangem.blockchain.common.network.providers.OnlyPublicProvidersBuilder
 import com.tangem.blockchain.common.network.providers.ProviderType
-import com.tangem.blockchain.network.API_TEZOS_BLOCKSCALE
-import com.tangem.blockchain.network.API_TEZOS_ECAD
-import com.tangem.blockchain.network.API_TEZOS_SMARTPY
 
 internal class TezosProvidersBuilder(
     override val providerTypes: List<ProviderType>,
-) : NetworkProvidersBuilder<TezosNetworkProvider>() {
+) : OnlyPublicProvidersBuilder<TezosNetworkProvider>(providerTypes) {
 
-    override fun createProviders(blockchain: Blockchain): List<TezosNetworkProvider> {
-        return listOf(API_TEZOS_BLOCKSCALE, API_TEZOS_SMARTPY, API_TEZOS_ECAD)
-            .map(::TezosJsonRpcNetworkProvider)
-    }
+    override fun createProvider(url: String) = TezosJsonRpcNetworkProvider(url)
 }
