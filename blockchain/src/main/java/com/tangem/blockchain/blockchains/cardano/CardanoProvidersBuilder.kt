e0fd953170b96281802fc8f77da063fac5570bb8
Tangem <git@tangem.com>
2024-04-10 21:35:23 +0800

    Updated on 2026-03-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/CardanoProvidersBuilder.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/CardanoProvidersBuilder.kt
@@ -7,10 +7,12 @@ import com.tangem.blockchain.blockchains.cardano.network.rosetta.RosettaNetworkP
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.BlockchainSdkConfig
 import com.tangem.blockchain.common.network.providers.NetworkProvidersBuilder
+import com.tangem.blockchain.common.network.providers.ProviderType
 import com.tangem.blockchain.extensions.letNotBlank
 import com.tangem.blockchain.network.API_ADALITE
 
 internal class CardanoProvidersBuilder(
+    override val providerTypes: List<ProviderType>,
     private val config: BlockchainSdkConfig,
 ) : NetworkProvidersBuilder<CardanoNetworkProvider>() {
 
