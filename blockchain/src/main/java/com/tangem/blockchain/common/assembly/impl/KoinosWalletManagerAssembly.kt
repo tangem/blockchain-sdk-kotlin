d6063684f57fbacae65db7f183f076cc53454a8d
Tangem <git@tangem.com>
2024-04-10 19:35:37 +0300

    Updated on 2025-09-29

--- blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/KoinosWalletManagerAssembly.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/KoinosWalletManagerAssembly.kt
@@ -1,13 +1,22 @@
 package com.tangem.blockchain.common.assembly.impl
 
+import com.tangem.blockchain.blockchains.koinos.KoinosProviderBuilder
+import com.tangem.blockchain.blockchains.koinos.KoinosTransactionBuilder
 import com.tangem.blockchain.blockchains.koinos.KoinosWalletManager
+import com.tangem.blockchain.blockchains.koinos.network.KoinosNetworkService
 import com.tangem.blockchain.common.assembly.WalletManagerAssembly
 import com.tangem.blockchain.common.assembly.WalletManagerAssemblyInput
 
 internal object KoinosWalletManagerAssembly : WalletManagerAssembly<KoinosWalletManager>() {
     override fun make(input: WalletManagerAssemblyInput): KoinosWalletManager {
         return with(input.wallet) {
-            KoinosWalletManager(this)
+            KoinosWalletManager(
+                wallet = this,
+                networkService = KoinosNetworkService(
+                    providers = KoinosProviderBuilder().build(blockchain),
+                ),
+                transactionBuilder = KoinosTransactionBuilder(isTestnet = blockchain.isTestnet()),
+            )
         }
     }
 }
