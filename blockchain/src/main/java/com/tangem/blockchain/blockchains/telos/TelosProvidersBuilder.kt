2a63dceee281913afba4182c33615880ebb5a413
Tangem <git@tangem.com>
2024-04-11 10:22:46 +0800

    Updated on 2026-03-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/telos/TelosProvidersBuilder.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/telos/TelosProvidersBuilder.kt
@@ -1,25 +1,15 @@
 package com.tangem.blockchain.blockchains.telos
 
-import com.tangem.blockchain.blockchains.ethereum.EthereumLikeProvidersBuilder
 import com.tangem.blockchain.blockchains.ethereum.network.EthereumJsonRpcProvider
-import com.tangem.blockchain.common.Blockchain
-import com.tangem.blockchain.common.BlockchainSdkConfig
+import com.tangem.blockchain.common.network.providers.OnlyPublicProvidersBuilder
 import com.tangem.blockchain.common.network.providers.ProviderType
 
 internal class TelosProvidersBuilder(
     override val providerTypes: List<ProviderType>,
-    override val config: BlockchainSdkConfig,
-) : EthereumLikeProvidersBuilder(config) {
+) : OnlyPublicProvidersBuilder<EthereumJsonRpcProvider>(
+    providerTypes = providerTypes,
+    testnetProviders = listOf("https://telos-evm-testnet.rpc.thirdweb.com/"),
+) {
 
-    override fun createProviders(blockchain: Blockchain): List<EthereumJsonRpcProvider> {
-        return listOf(
-            EthereumJsonRpcProvider(baseUrl = "https://mainnet.telos.net", postfixUrl = "evm"),
-            EthereumJsonRpcProvider(baseUrl = "https://api.kainosbp.com", postfixUrl = "evm"),
-            EthereumJsonRpcProvider(baseUrl = "https://telos-evm.rpc.thirdweb.com/"),
-        )
-    }
-
-    override fun createTestnetProviders(blockchain: Blockchain): List<EthereumJsonRpcProvider> {
-        return listOf(EthereumJsonRpcProvider(baseUrl = "https://telos-evm-testnet.rpc.thirdweb.com/"))
-    }
+    override fun createProvider(url: String) = EthereumJsonRpcProvider(baseUrl = url)
 }
