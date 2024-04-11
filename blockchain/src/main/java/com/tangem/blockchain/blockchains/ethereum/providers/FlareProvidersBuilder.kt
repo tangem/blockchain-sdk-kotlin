3abf91eaeb2d6a864ae6a8dad867c64ea69b9405
Tangem <git@tangem.com>
2024-04-11 10:22:46 +0800

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/providers/FlareProvidersBuilder.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/providers/FlareProvidersBuilder.kt
@@ -1,29 +1,14 @@
 package com.tangem.blockchain.blockchains.ethereum.providers
 
-import com.tangem.blockchain.blockchains.ethereum.EthereumLikeProvidersBuilder
 import com.tangem.blockchain.blockchains.ethereum.network.EthereumJsonRpcProvider
-import com.tangem.blockchain.common.Blockchain
-import com.tangem.blockchain.common.BlockchainSdkConfig
+import com.tangem.blockchain.common.network.providers.OnlyPublicProvidersBuilder
 import com.tangem.blockchain.common.network.providers.ProviderType
 
 internal class FlareProvidersBuilder(
     override val providerTypes: List<ProviderType>,
-    override val config: BlockchainSdkConfig,
-) : EthereumLikeProvidersBuilder(config) {
-
-    override fun createProviders(blockchain: Blockchain): List<EthereumJsonRpcProvider> {
-        return listOf(
-            "https://flare-api.flare.network/ext/bc/C/rpc/",
-            "https://flare.rpc.thirdweb.com/",
-            "https://flare-bundler.etherspot.io/",
-            "https://rpc.ankr.com/flare/",
-            "https://flare.solidifi.app/ext/C/rpc/",
-        )
-            .map(::EthereumJsonRpcProvider)
-    }
-
-    override fun createTestnetProviders(blockchain: Blockchain): List<EthereumJsonRpcProvider> {
-        return listOf("https://coston2-api.flare.network/ext/C/rpc/")
-            .map(::EthereumJsonRpcProvider)
-    }
+) : OnlyPublicProvidersBuilder<EthereumJsonRpcProvider>(
+    providerTypes = providerTypes,
+    testnetProviders = listOf("https://coston2-api.flare.network/ext/C/rpc/"),
+) {
+    override fun createProvider(url: String) = EthereumJsonRpcProvider(url)
 }
