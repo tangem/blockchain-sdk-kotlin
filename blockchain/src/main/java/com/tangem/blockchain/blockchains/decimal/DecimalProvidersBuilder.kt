cadfb9b2e3927a94cd4957bd3dd41b6cecfd0ab9
Tangem <git@tangem.com>
2024-04-11 10:22:46 +0800

    Updated on 2025-11-03

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/decimal/DecimalProvidersBuilder.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/decimal/DecimalProvidersBuilder.kt
@@ -1,29 +1,14 @@
 package com.tangem.blockchain.blockchains.decimal
 
-import com.tangem.blockchain.blockchains.ethereum.EthereumLikeProvidersBuilder
 import com.tangem.blockchain.blockchains.ethereum.network.EthereumJsonRpcProvider
-import com.tangem.blockchain.common.Blockchain
-import com.tangem.blockchain.common.BlockchainSdkConfig
+import com.tangem.blockchain.common.network.providers.OnlyPublicProvidersBuilder
 import com.tangem.blockchain.common.network.providers.ProviderType
 
 internal class DecimalProvidersBuilder(
     override val providerTypes: List<ProviderType>,
-    override val config: BlockchainSdkConfig,
-) : EthereumLikeProvidersBuilder(config) {
-
-    override fun createProviders(blockchain: Blockchain): List<EthereumJsonRpcProvider> {
-        return listOf(
-            "https://node.decimalchain.com/web3/",
-            "https://node1-mainnet.decimalchain.com/web3/",
-            "https://node2-mainnet.decimalchain.com/web3/",
-            "https://node3-mainnet.decimalchain.com/web3/",
-            "https://node4-mainnet.decimalchain.com/web3/",
-        )
-            .map(::EthereumJsonRpcProvider)
-    }
-
-    override fun createTestnetProviders(blockchain: Blockchain): List<EthereumJsonRpcProvider> {
-        return listOf("https://testnet-val.decimalchain.com/web3/")
-            .map(::EthereumJsonRpcProvider)
-    }
+) : OnlyPublicProvidersBuilder<EthereumJsonRpcProvider>(
+    providerTypes,
+    listOf("https://testnet-val.decimalchain.com/web3/"),
+) {
+    override fun createProvider(url: String) = EthereumJsonRpcProvider(baseUrl = url)
 }
