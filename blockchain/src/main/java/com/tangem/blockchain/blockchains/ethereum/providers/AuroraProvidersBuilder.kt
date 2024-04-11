73a0d54da4316e87b14dbc9e448997fedeb24751
Mama1emon <andrew.khokhlove@gmail.com>
2024-04-11 10:22:46 +0800

    AND-6638 Create providers according to the list of provider types (3)

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/providers/AuroraProvidersBuilder.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/providers/AuroraProvidersBuilder.kt
@@ -1,26 +1,15 @@
 package com.tangem.blockchain.blockchains.ethereum.providers
 
-import com.tangem.blockchain.blockchains.ethereum.EthereumLikeProvidersBuilder
 import com.tangem.blockchain.blockchains.ethereum.network.EthereumJsonRpcProvider
-import com.tangem.blockchain.common.Blockchain
-import com.tangem.blockchain.common.BlockchainSdkConfig
+import com.tangem.blockchain.common.network.providers.OnlyPublicProvidersBuilder
 import com.tangem.blockchain.common.network.providers.ProviderType
 
 internal class AuroraProvidersBuilder(
     override val providerTypes: List<ProviderType>,
-    override val config: BlockchainSdkConfig,
-) : EthereumLikeProvidersBuilder(config) {
+) : OnlyPublicProvidersBuilder<EthereumJsonRpcProvider>(
+    providerTypes = providerTypes,
+    testnetProviders = listOf("https://testnet.aurora.dev/"),
+) {
 
-    override fun createProviders(blockchain: Blockchain): List<EthereumJsonRpcProvider> {
-        return listOfNotNull(
-            EthereumJsonRpcProvider(baseUrl = "https://mainnet.aurora.dev/"),
-            EthereumJsonRpcProvider(baseUrl = "https://aurora.drpc.org/"),
-        )
-    }
-
-    override fun createTestnetProviders(blockchain: Blockchain): List<EthereumJsonRpcProvider> {
-        return listOf(
-            EthereumJsonRpcProvider(baseUrl = "https://testnet.aurora.dev/"),
-        )
-    }
+    override fun createProvider(url: String) = EthereumJsonRpcProvider(url)
 }
