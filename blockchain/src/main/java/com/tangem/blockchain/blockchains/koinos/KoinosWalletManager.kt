d4518b8a8c85a7df64e82ff557e7e2a5f0080846
Nikita Samoylov <nikit.samoylov@gmail.com>
2024-04-12 15:40:11 +0300

    AND-6735 [Koinos] Transaction History (Draft) + outgoing transaction tracking

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/koinos/KoinosWalletManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/koinos/KoinosWalletManager.kt
@@ -4,16 +4,20 @@ import com.tangem.blockchain.blockchains.koinos.network.KoinosNetworkService
 import com.tangem.blockchain.common.*
 import com.tangem.blockchain.common.transaction.Fee
 import com.tangem.blockchain.common.transaction.TransactionFee
+import com.tangem.blockchain.common.txhistory.TransactionHistoryProvider
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.SimpleResult
 import com.tangem.blockchain.extensions.successOr
 import com.tangem.blockchain.extensions.toSimpleResult
+import java.math.RoundingMode
+import java.util.EnumSet
 
 internal class KoinosWalletManager(
     wallet: Wallet,
+    transactionHistoryProvider: TransactionHistoryProvider,
     private val networkService: KoinosNetworkService,
     private val transactionBuilder: KoinosTransactionBuilder,
-) : WalletManager(wallet = wallet) {
+) : WalletManager(wallet = wallet, transactionHistoryProvider = transactionHistoryProvider) {
 
     override val currentHost: String
         get() = networkService.baseUrl
@@ -22,6 +26,22 @@ internal class KoinosWalletManager(
         val accountInfo = networkService.getInfo(wallet.address)
             .successOr { return }
 
+        val unconfirmedOutgoingTransaction = wallet.recentTransactions.firstOrNull {
+            it.status == TransactionStatus.Unconfirmed
+        }
+
+        if (unconfirmedOutgoingTransaction != null && wallet.getCoinAmount().value != accountInfo.koinBalance) {
+            val recentTransactions = networkService.getTransactionHistory(
+                address = wallet.address,
+                pageSize = 20,
+                sequenceNum = 0,
+            ).successOr { null }
+
+            if (recentTransactions != null && recentTransactions.any { it.id == unconfirmedOutgoingTransaction.hash }) {
+                unconfirmedOutgoingTransaction.status = TransactionStatus.Confirmed
+            }
+        }
+
         wallet.changeAmountValue(AmountType.Coin, newValue = accountInfo.koinBalance)
 
         // TODO maybe change interface (AND-6736)
@@ -29,8 +49,13 @@ internal class KoinosWalletManager(
             mana = accountInfo.mana,
             timeToChargeMillis = 0L,
         )
+        // TODO AND-6736
+        // wallet.changeAmountValue(AmountType.KoinosMana, newValue = accountInfo.mana)
+    }
 
-        // TODO add transaction history update (AND-6735)
+    override fun validateTransaction(amount: Amount, fee: Amount?): EnumSet<TransactionError> {
+        // TODO AND-6736 validate with mana amount
+        return super.validateTransaction(amount, fee)
     }
 
     override suspend fun send(transactionData: TransactionData, signer: TransactionSigner): SimpleResult {
@@ -40,7 +65,11 @@ internal class KoinosWalletManager(
         // FIXME remove after UI requirements is ready (AND-6736)
         val testManaLimit = (wallet.additionalInfo as WalletAdditionalInfo.Koinos)
             .mana.multiply(0.1.toBigDecimal())
-            .setScale(Blockchain.Koinos.decimals())
+            .setScale(Blockchain.Koinos.decimals(), RoundingMode.UP)
+
+        // val testManaLimit = wallet.amounts[AmountType.KoinosMana]!!.value!!
+        //     .multiply(0.1.toBigDecimal())
+        //     .setScale(Blockchain.Koinos.decimals(), RoundingMode.UP)
 
         // FIXME use this instead (AND-6736)
         // val manaLimit = (transactionData.extras as? KoinosTransactionExtras)?.manaLimit
@@ -60,16 +89,32 @@ internal class KoinosWalletManager(
 
         val signedTransaction = transactionBuilder.buildToSend(transaction, signature)
 
-        networkService.submitTransaction(signedTransaction)
+        val transactionRes = networkService.submitTransaction(signedTransaction)
             .successOr { return it.toSimpleResult() }
 
-        // TODO add transaction to history (AND-6735)
+        wallet.addOutgoingTransaction(
+            transactionData.copy(
+                hash = transactionRes.id,
+            ),
+        )
 
         return SimpleResult.Success
     }
 
     override suspend fun getFee(amount: Amount, destination: String): Result<TransactionFee> {
-        // TODO no fee child class?
+        // TODO (AND-6736)
+        // val currentMana = wallet.amounts[AmountType.KoinosMana]!!.value!!
+        //     .multiply(0.1.toBigDecimal())
+        //     .setScale(Blockchain.Koinos.decimals(), RoundingMode.UP)
+        //
+        // return Result.Success(
+        //     TransactionFee.Single(
+        //         normal = Fee.Common(
+        //             amount = Amount(currentMana, Blockchain.Koinos, AmountType.KoinosMana)
+        //         )
+        //     )
+        // )
+
         return Result.Success(TransactionFee.Single(normal = Fee.Common(amount = Amount(Blockchain.Koinos))))
     }
 }
--- blockchain/src/main/java/com/tangem/blockchain/transactionhistory/koinos/KoinosHistoryProviderFactory.kt
+++ blockchain/src/main/java/com/tangem/blockchain/transactionhistory/koinos/KoinosHistoryProviderFactory.kt
@@ -1,18 +1,22 @@
 package com.tangem.blockchain.transactionhistory.koinos
 
-import com.tangem.blockchain.blockchains.koinos.KoinosProviderBuilder
-import com.tangem.blockchain.blockchains.koinos.network.KoinosNetworkService
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.BlockchainSdkConfig
+import com.tangem.blockchain.common.txhistory.DefaultTransactionHistoryProvider
 import com.tangem.blockchain.common.txhistory.TransactionHistoryProvider
 import com.tangem.blockchain.transactionhistory.TransactionHistoryProviderFactory
 
 internal class KoinosHistoryProviderFactory : TransactionHistoryProviderFactory {
     override fun makeProvider(config: BlockchainSdkConfig, blockchain: Blockchain): TransactionHistoryProvider {
-        return KoinosTransactionHistoryProvider(
-            networkService = KoinosNetworkService(
-                KoinosProviderBuilder().build(blockchain),
-            ),
-        )
+        return DefaultTransactionHistoryProvider
+
+        // No implementation for now
+        // See KoinosTransactionHistoryProvider deprecation doc
+        //
+        // return KoinosTransactionHistoryProvider(
+        //     networkService = KoinosNetworkService(
+        //         KoinosProviderBuilder().build(blockchain),
+        //     ),
+        // )
     }
 }
