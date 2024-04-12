70d2d3dee7f749831e39c2f6d8b479d2ae95146a
Tangem <git@tangem.com>
2024-04-12 15:40:11 +0300

    Updated on 2025-09-29

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/koinos/network/KoinosNetworkProvier.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/koinos/network/KoinosNetworkProvier.kt
@@ -52,11 +52,15 @@ internal class KoinosNetworkProvier(
 
         // Sometimes one request is not enough
         return retryCall {
-            api.send(apiKey, request).parseResult<KoinosMethod.ReadContract.Response>()
+            api.send(apiKey, request)
+                .parseResult<KoinosMethod.ReadContract.Response>()
         }.map { response ->
-            response.result?.let {
-                koinContractAbi.balanceOf.decodeResult(it)?.balance
-            } ?: return decodeFailure(koinContractAbi.balanceOf.resultName)
+            if (response.result == null) {
+                0L
+            } else {
+                koinContractAbi.balanceOf.decodeResult(response.result)?.balance
+                    ?: return decodeFailure(koinContractAbi.balanceOf.resultName)
+            }
         }
     }
 
@@ -98,16 +102,116 @@ internal class KoinosNetworkProvier(
 
             KoinosTransactionEntry(
                 id = response.receipt.id,
+                sequenceNum = Long.MAX_VALUE,
                 payerAddress = response.receipt.payer,
                 maxPayerRC = response.receipt.maxPayerRc,
                 rcLimit = response.receipt.rcLimit,
                 rcUsed = response.receipt.rcUsed,
-                transferEvent = KoinosTransactionEntry.KoinTransferEvent(
-                    fromAddress = decodedEvent.fromAccount,
-                    toAddress = decodedEvent.toAccount,
+                event = KoinosTransactionEntry.Event.KoinTransferEvent(
+                    fromAddress = KoinosTransactionEntry.Address.Single(decodedEvent.fromAccount),
+                    toAddress = KoinosTransactionEntry.Address.Single(decodedEvent.toAccount),
                     value = decodedEvent.value,
                 ),
             )
+        }
+    }
+
+    suspend fun getTransactionHistory(request: TransactionHistoryRequest): Result<List<KoinosTransactionEntry>> {
+        val seqNum = if (request.sequenceNum == 0L) null else request.sequenceNum.toString()
+
+        val jsonRequest = KoinosMethod.GetAccountHistory(
+            address = request.address,
+            seqNumber = seqNum,
+            limit = request.pageSize.toString(),
+            ascending = false,
+            irreversible = false,
+        ).asRequest()
+
+        return catchNetworkError {
+            api.send(apiKey, jsonRequest).parseResult<KoinosMethod.GetAccountHistory.Response>()
+        }.map { response ->
+            response.values.mapNotNull {
+                if (it.transaction != null) {
+                    val receipt = it.transaction.receipt
+                    val transferEvent = receipt.events.toTxEntryEvent()
+
+                    KoinosTransactionEntry(
+                        id = it.transaction.transaction.id,
+                        sequenceNum = it.seqNum,
+                        payerAddress = receipt.payer,
+                        maxPayerRC = receipt.maxPayerRc,
+                        rcLimit = receipt.rcLimit,
+                        rcUsed = receipt.rcUsed,
+                        event = transferEvent,
+                    )
+                } else {
+                    null
+                }
+            }
+        }
+    }
+
+    private fun List<KoinosProtocol.EventData>.toTxEntryEvent(): KoinosTransactionEntry.Event {
+        // Filter all non koin transfer events
+        val list = this.filter { it.name == koinContractAbi.transfer.eventName }
+        return when {
+            list.size == 1 -> {
+                val eventData = list[0]
+                koinContractAbi.transfer.decodeEvent(eventData.eventData)?.let { transfer ->
+                    // the most likely case when we have one transfer event
+                    KoinosTransactionEntry.Event.KoinTransferEvent(
+                        fromAddress = KoinosTransactionEntry.Address.Single(transfer.fromAccount),
+                        toAddress = KoinosTransactionEntry.Address.Single(transfer.toAccount),
+                        value = transfer.value,
+                    )
+                } // if it's not transfer event
+                    ?: KoinosTransactionEntry.Event.Unsupported
+            }
+            list.size > 1 -> {
+                val decodedEvents = list.mapNotNull {
+                    koinContractAbi.transfer.decodeEvent(it.eventData)
+                }
+
+                // if all events is not transfer
+                if (decodedEvents.isEmpty()) {
+                    return KoinosTransactionEntry.Event.Unsupported
+                }
+
+                val transfersMap = decodedEvents.groupBy { it.fromAccount }
+
+                // if we found transfer event that came from the same address
+                if (transfersMap.entries.size == 1) {
+                    val entry = transfersMap.entries.first()
+                    KoinosTransactionEntry.Event.KoinTransferEvent(
+                        fromAddress = KoinosTransactionEntry.Address.Single(entry.key),
+                        toAddress = KoinosTransactionEntry.Address.Multiple(entry.value.map { it.toAccount }),
+                        value = entry.value.sumOf { it.value }, // sum up all transfers amounts
+                    )
+                } else { // multiple transfer events with different source addresses
+                    val fromAddress = KoinosTransactionEntry.Address.Multiple(transfersMap.keys.toList())
+
+                    // sum up all transfers amounts
+                    val transfersSum = transfersMap.entries.sumOf { en -> en.value.sumOf { it.value } }
+                    val destAddresses = transfersMap.entries
+                        .map { en -> en.value.map { it.toAccount } }
+                        .flatten()
+                        .toSet()
+
+                    val toAddress = if (destAddresses.size == 1) {
+                        // if all transfers have one destination address
+                        KoinosTransactionEntry.Address.Single(destAddresses.first())
+                    } else {
+                        KoinosTransactionEntry.Address.Multiple(destAddresses.toList())
+                    }
+
+                    KoinosTransactionEntry.Event.KoinTransferEvent(
+                        fromAddress = fromAddress,
+                        toAddress = toAddress,
+                        value = transfersSum,
+                    )
+                }
+            }
+            else -> KoinosTransactionEntry.Event.Unsupported
         }
     }
 
