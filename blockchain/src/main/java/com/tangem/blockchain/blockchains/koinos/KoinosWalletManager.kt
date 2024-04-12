6caffcc008865dacc04885d0675bc6ef902794cf
Tangem <git@tangem.com>
2024-04-12 15:40:11 +0300

    releases/5.27.0-1144

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/koinos/KoinosWalletManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/koinos/KoinosWalletManager.kt
@@ -4,16 +4,20 @@ import com.tangem.blockchain.blockchains.koinos.network.KoinosNetworkService
 import com.tangem.blockchain.common.*
 import com.tangem.blockchain.common.transaction.Fee
 import com.tangem.blockchain.common.transaction.TransactionFee
+import com.tangem.blockchain.common.txhistory.TransactionHistoryProvider
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.SimpleResult
 import com.tangem.blockchain.extensions.successOr
 import com.tangem.blockchain.extensions.toSimpleResult
+import java.math.RoundingMode
+import java.util.EnumSet
 
 internal class KoinosWalletManager(
     wallet: Wallet,
+    transactionHistoryProvider: TransactionHistoryProvider,
     private val networkService: KoinosNetworkService,
     private val transactionBuilder: KoinosTransactionBuilder,
-) : WalletManager(wallet = wallet) {
+) : WalletManager(wallet = wallet, transactionHistoryProvider = transactionHistoryProvider) {
 
     override val currentHost: String
         get() = networkService.baseUrl
@@ -22,6 +26,22 @@ internal class KoinosWalletManager(
         val accountInfo = networkService.getInfo(wallet.address)
             .successOr { return }
 
+        val unconfirmedOutgoingTransaction = wallet.recentTransactions.firstOrNull {
+            it.status == TransactionStatus.Unconfirmed
+        }
+
+        if (unconfirmedOutgoingTransaction != null && wallet.getCoinAmount().value != accountInfo.koinBalance) {
+            val recentTransactions = networkService.getTransactionHistory(
+                address = wallet.address,
+                pageSize = 20,
+                sequenceNum = 0,
+            ).successOr { null }
+
+            if (recentTransactions != null && recentTransactions.any { it.id == unconfirmedOutgoingTransaction.hash }) {
+                unconfirmedOutgoingTransaction.status = TransactionStatus.Confirmed
+            }
+        }
+
         wallet.changeAmountValue(AmountType.Coin, newValue = accountInfo.koinBalance)
 // [REDACTED_TODO_COMMENT]
         wallet.additionalInfo = WalletAdditionalInfo.Koinos(
@@ -29,6 +49,12 @@ internal class KoinosWalletManager(
             timeToChargeMillis = 0L,
         )
 // [REDACTED_TODO_COMMENT]
+        // wallet.changeAmountValue(AmountType.KoinosMana, newValue = accountInfo.mana)
+    }
+
+    override fun validateTransaction(amount: Amount, fee: Amount?): EnumSet<TransactionError> {
+// [REDACTED_TODO_COMMENT]
+        return super.validateTransaction(amount, fee)
     }
 
     override suspend fun send(transactionData: TransactionData, signer: TransactionSigner): SimpleResult {
@@ -37,7 +63,11 @@ internal class KoinosWalletManager(
 // [REDACTED_TODO_COMMENT]
         val testManaLimit = (wallet.additionalInfo as WalletAdditionalInfo.Koinos)
             .mana.multiply(0.1.toBigDecimal())
-            .setScale(Blockchain.Koinos.decimals())
+            .setScale(Blockchain.Koinos.decimals(), RoundingMode.UP)
+
+        // val testManaLimit = wallet.amounts[AmountType.KoinosMana]!!.value!!
+        //     .multiply(0.1.toBigDecimal())
+        //     .setScale(Blockchain.Koinos.decimals(), RoundingMode.UP)
 // [REDACTED_TODO_COMMENT]
         // val manaLimit = (transactionData.extras as? KoinosTransactionExtras)?.manaLimit
         //     ?: return SimpleResult.fromTangemSdkError(BlockchainSdkError.FailedToBuildTx)
@@ -56,15 +86,32 @@ internal class KoinosWalletManager(
 
         val signedTransaction = transactionBuilder.buildToSend(transaction, signature)
 
-        networkService.submitTransaction(signedTransaction)
+        val transactionRes = networkService.submitTransaction(signedTransaction)
             .successOr { return it.toSimpleResult() }
-// [REDACTED_TODO_COMMENT]
+
+        wallet.addOutgoingTransaction(
+            transactionData.copy(
+                hash = transactionRes.id,
+            ),
+        )
 
         return SimpleResult.Success
     }
 
     override suspend fun getFee(amount: Amount, destination: String): Result<TransactionFee> {
 // [REDACTED_TODO_COMMENT]
+        // val currentMana = wallet.amounts[AmountType.KoinosMana]!!.value!!
+        //     .multiply(0.1.toBigDecimal())
+        //     .setScale(Blockchain.Koinos.decimals(), RoundingMode.UP)
+        //
+        // return Result.Success(
+        //     TransactionFee.Single(
+        //         normal = Fee.Common(
+        //             amount = Amount(currentMana, Blockchain.Koinos, AmountType.KoinosMana)
+        //         )
+        //     )
+        // )
+
         return Result.Success(TransactionFee.Single(normal = Fee.Common(amount = Amount(Blockchain.Koinos))))
     }
 }
