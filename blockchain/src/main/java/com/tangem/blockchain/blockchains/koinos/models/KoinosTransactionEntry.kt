3d88b77ea93e4d35fd7a9f86b0010f4517a73c60
Tangem <git@tangem.com>
2024-04-12 15:40:11 +0300

    Updated on 2026-01-28

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/koinos/models/KoinosTransactionEntry.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/koinos/models/KoinosTransactionEntry.kt
@@ -1,17 +1,30 @@
 package com.tangem.blockchain.blockchains.koinos.models
 
-internal class KoinosTransactionEntry(
+internal data class KoinosTransactionEntry(
     val id: String,
+    val sequenceNum: Long,
     val payerAddress: String,
     val maxPayerRC: Long,
     val rcLimit: Long,
     val rcUsed: Long,
-    val transferEvent: KoinTransferEvent,
+    val event: Event,
 ) {
 
-    data class KoinTransferEvent(
-        val fromAddress: String,
-        val toAddress: String,
-        val value: Long,
-    )
+    sealed interface Event {
+        data class KoinTransferEvent(
+            val fromAddress: Address,
+            val toAddress: Address,
+            val value: Long,
+        ) : Event
+
+        object Unsupported : Event
+    }
+
+    sealed interface Address {
+        @JvmInline
+        value class Single(val address: String) : Address
+
+        @JvmInline
+        value class Multiple(val addresses: List<String>) : Address
+    }
 }
