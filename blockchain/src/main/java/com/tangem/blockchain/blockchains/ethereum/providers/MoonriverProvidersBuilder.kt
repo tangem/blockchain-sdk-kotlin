ae6b965692c5de988f32b9a12cc6d9732ccb1b45
Tangem <git@tangem.com>
2024-04-15 16:32:58 +0800

    develop-1150

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/providers/MoonriverProvidersBuilder.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/providers/MoonriverProvidersBuilder.kt
@@ -1,6 +1,7 @@
 package com.tangem.blockchain.blockchains.ethereum.providers
 
 import com.tangem.blockchain.blockchains.ethereum.network.EthereumJsonRpcProvider
+import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.network.providers.OnlyPublicProvidersBuilder
 import com.tangem.blockchain.common.network.providers.ProviderType
 
@@ -11,5 +12,5 @@ internal class MoonriverProvidersBuilder(
     testnetProviders = listOf("https://rpc.api.moonbase.moonbeam.network/"),
 ) {
 
-    override fun createProvider(url: String) = EthereumJsonRpcProvider(url)
+    override fun createProvider(url: String, blockchain: Blockchain) = EthereumJsonRpcProvider(url)
 }
