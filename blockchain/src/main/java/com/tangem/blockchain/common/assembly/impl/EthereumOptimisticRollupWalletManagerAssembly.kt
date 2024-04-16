e9c485a9dfc0cda1ca194f8f77d11b0226e0d2de
Tangem <git@tangem.com>
2024-04-16 13:27:33 +0100

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/EthereumOptimisticRollupWalletManagerAssembly.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/EthereumOptimisticRollupWalletManagerAssembly.kt
@@ -3,15 +3,16 @@ package com.tangem.blockchain.common.assembly.impl
 import com.tangem.blockchain.blockchains.ethereum.EthereumTransactionBuilder
 import com.tangem.blockchain.blockchains.ethereum.getEthereumJsonRpcProviders
 import com.tangem.blockchain.blockchains.ethereum.network.EthereumNetworkService
-import com.tangem.blockchain.blockchains.optimism.OptimismWalletManager
+import com.tangem.blockchain.blockchains.optimism.EthereumOptimisticRollupWalletManager
 import com.tangem.blockchain.common.assembly.WalletManagerAssembly
 import com.tangem.blockchain.common.assembly.WalletManagerAssemblyInput
 
-internal object OptimismWalletManagerAssembly : WalletManagerAssembly<OptimismWalletManager>() {
+internal object EthereumOptimisticRollupWalletManagerAssembly :
+    WalletManagerAssembly<EthereumOptimisticRollupWalletManager>() {
 
-    override fun make(input: WalletManagerAssemblyInput): OptimismWalletManager {
+    override fun make(input: WalletManagerAssemblyInput): EthereumOptimisticRollupWalletManager {
         with(input.wallet) {
-            return OptimismWalletManager(
+            return EthereumOptimisticRollupWalletManager(
                 wallet = this,
                 transactionBuilder = EthereumTransactionBuilder(
                     walletPublicKey = publicKey.blockchainKey,
