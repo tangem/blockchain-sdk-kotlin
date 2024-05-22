28e658f3cd0de46312aa36070338336a668d4080
Tangem <git@tangem.com>
2024-05-22 12:47:08 +0500

    Updated on 2026-06-27

--- blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/PolkadotWalletManagerAssembly.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/PolkadotWalletManagerAssembly.kt
@@ -4,11 +4,13 @@ import com.tangem.blockchain.blockchains.polkadot.PolkadotWalletManager
 import com.tangem.blockchain.blockchains.polkadot.network.PolkadotNetworkProvider
 import com.tangem.blockchain.blockchains.polkadot.network.PolkadotNetworkService
 import com.tangem.blockchain.blockchains.polkadot.network.accounthealthcheck.PolkadotAccountHealthCheckNetworkService
+import com.tangem.blockchain.blockchains.polkadot.providers.*
 import com.tangem.blockchain.blockchains.polkadot.providers.AlephZeroProvidersBuilder
 import com.tangem.blockchain.blockchains.polkadot.providers.JoyStreamProvidersBuilder
 import com.tangem.blockchain.blockchains.polkadot.providers.KusamaProvidersBuilder
 import com.tangem.blockchain.blockchains.polkadot.providers.PolkadotProvidersBuilder
 import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchain.common.BlockchainSdkConfig
 import com.tangem.blockchain.common.assembly.WalletManagerAssembly
 import com.tangem.blockchain.common.assembly.WalletManagerAssemblyInput
 import com.tangem.blockchain.common.network.providers.NetworkProvidersBuilder
@@ -24,7 +26,11 @@ internal object PolkadotWalletManagerAssembly : WalletManagerAssembly<PolkadotWa
             PolkadotWalletManager(
                 wallet = this,
                 networkProvider = PolkadotNetworkService(
-                    providers = getNetworkProvidersBuilder(input.providerTypes, blockchain).build(blockchain),
+                    providers = getNetworkProvidersBuilder(
+                        providerTypes = input.providerTypes,
+                        config = input.config,
+                        blockchain = blockchain,
+                    ).build(blockchain),
                 ),
                 extrinsicCheckNetworkProvider = healthCheckService,
             )
@@ -33,6 +39,7 @@ internal object PolkadotWalletManagerAssembly : WalletManagerAssembly<PolkadotWa
 
     private fun getNetworkProvidersBuilder(
         providerTypes: List<ProviderType>,
+        config: BlockchainSdkConfig,
         blockchain: Blockchain,
     ): NetworkProvidersBuilder<PolkadotNetworkProvider> {
         return when (blockchain) {
@@ -40,6 +47,7 @@ internal object PolkadotWalletManagerAssembly : WalletManagerAssembly<PolkadotWa
             Blockchain.AlephZero, Blockchain.AlephZeroTestnet -> AlephZeroProvidersBuilder(providerTypes)
             Blockchain.Kusama -> KusamaProvidersBuilder(providerTypes)
             Blockchain.Joystream -> JoyStreamProvidersBuilder(providerTypes)
+            Blockchain.Bittensor -> BittensorProvidersBuilder(providerTypes, config)
             else -> error("$blockchain isn't supported")
         }
     }
