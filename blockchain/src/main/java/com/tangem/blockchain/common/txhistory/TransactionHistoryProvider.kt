61b429d536e2337755c712cec533998fcdfd9949
Tangem <git@tangem.com>
2024-05-26 15:58:39 +0500

    Updated on 2026-02-11

--- blockchain/src/main/java/com/tangem/blockchain/common/txhistory/TransactionHistoryProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/txhistory/TransactionHistoryProvider.kt
@@ -1,5 +1,6 @@
 package com.tangem.blockchain.common.txhistory
 
+import com.tangem.blockchain.common.Amount
 import com.tangem.blockchain.common.pagination.PaginationWrapper
 import com.tangem.blockchain.extensions.Result
 
@@ -13,4 +14,12 @@ interface TransactionHistoryProvider {
     suspend fun getTransactionsHistory(
         request: TransactionHistoryRequest,
     ): Result<PaginationWrapper<TransactionHistoryItem>>
+
+    // TODO: Move to inside mappers - [REDACTED_JIRA]
+    fun shouldExcludeFromHistory(filterType: TransactionHistoryRequest.FilterType, amount: Amount): Boolean {
+        return when (filterType) {
+            TransactionHistoryRequest.FilterType.Coin -> return false
+            is TransactionHistoryRequest.FilterType.Contract -> amount.value == null || amount.value.signum() == 0
+        }
+    }
 }
