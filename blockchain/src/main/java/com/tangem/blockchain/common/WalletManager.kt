43c49e049792d91f7621c33baf87982e420e7f48
Tangem <git@tangem.com>
2024-05-30 15:19:39 +0500

    releases/5.27.0-1144

--- blockchain/src/main/java/com/tangem/blockchain/common/WalletManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/WalletManager.kt
@@ -204,3 +204,11 @@ interface Approver {
 
     fun getApproveData(spenderAddress: String, value: Amount? = null): String
 }
+
+/**
+ * Common interface for UTXO blockchains
+ */
+interface UtxoBlockchain {
+    /** Indicates allowance of self sending */
+    val allowConsolidation: Boolean
+}
