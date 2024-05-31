e73cc7d76f8bb163e00d6c8b925c81d4afeb5835
Tangem <git@tangem.com>
2024-05-31 11:37:18 +0500

    Updated on 2025-12-22

--- blockchain/src/main/java/com/tangem/blockchain/transactionhistory/DefaultTransactionHistoryProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/transactionhistory/DefaultTransactionHistoryProvider.kt
@@ -1,8 +1,10 @@
-package com.tangem.blockchain.common.txhistory
+package com.tangem.blockchain.transactionhistory
 
 import com.tangem.blockchain.common.pagination.Page
 import com.tangem.blockchain.common.pagination.PaginationWrapper
 import com.tangem.blockchain.extensions.Result
+import com.tangem.blockchain.transactionhistory.models.TransactionHistoryItem
+import com.tangem.blockchain.transactionhistory.models.TransactionHistoryRequest
 
 internal object DefaultTransactionHistoryProvider : TransactionHistoryProvider {
     override suspend fun getTransactionHistoryState(
