b846b576d37e13e83b213b5493905872dd929805
Tangem <git@tangem.com>
2024-06-05 22:55:25 +0400

    Updated on 2026-02-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/ton/network/TonNetworkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ton/network/TonNetworkProvider.kt
@@ -2,6 +2,8 @@ package com.tangem.blockchain.blockchains.ton.network
 
 import com.tangem.blockchain.common.NetworkProvider
 import com.tangem.blockchain.extensions.Result
+import java.math.BigDecimal
+import java.math.BigInteger
 
 interface TonNetworkProvider : NetworkProvider {
 
@@ -10,4 +12,13 @@ interface TonNetworkProvider : NetworkProvider {
     suspend fun getFee(address: String, message: String): Result<TonGetFeeResponse>
 
     suspend fun send(message: String): Result<TonSendBocResponse>
+
+    suspend fun getJettonWalletAddress(input: GetJettonWalletAddressInput): Result<String>
+
+    suspend fun getJettonBalance(jettonWalletAddress: String): Result<BigInteger>
 }
+
+data class GetJettonWalletAddressInput(
+    val ownerAddress: String,
+    val jettonMinterAddress: String,
+)
