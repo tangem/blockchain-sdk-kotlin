1a79446138ad4679aa5ee77a0da54c47333e39f4
Tangem <git@tangem.com>
2024-06-07 19:48:12 +0800

    Updated on 2026-06-27

--- blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/EthereumOptimisticRollupWalletManagerAssembly.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/EthereumOptimisticRollupWalletManagerAssembly.kt
@@ -1,11 +1,18 @@
 package com.tangem.blockchain.common.assembly.impl
 
 import com.tangem.blockchain.blockchains.ethereum.EthereumTransactionBuilder
+import com.tangem.blockchain.blockchains.ethereum.network.EthereumJsonRpcProvider
 import com.tangem.blockchain.blockchains.ethereum.network.EthereumNetworkService
+import com.tangem.blockchain.blockchains.ethereum.providers.BaseProvidersBuilder
+import com.tangem.blockchain.blockchains.ethereum.providers.MantaProvidersBuilder
 import com.tangem.blockchain.blockchains.optimism.EthereumOptimisticRollupWalletManager
 import com.tangem.blockchain.blockchains.optimism.OptimismProvidersBuilder
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchain.common.BlockchainSdkConfig
 import com.tangem.blockchain.common.assembly.WalletManagerAssembly
 import com.tangem.blockchain.common.assembly.WalletManagerAssemblyInput
+import com.tangem.blockchain.common.network.providers.NetworkProvidersBuilder
+import com.tangem.blockchain.common.network.providers.ProviderType
 
 internal object EthereumOptimisticRollupWalletManagerAssembly :
     WalletManagerAssembly<EthereumOptimisticRollupWalletManager>() {
@@ -19,9 +26,23 @@ internal object EthereumOptimisticRollupWalletManagerAssembly :
                     blockchain = blockchain,
                 ),
                 networkProvider = EthereumNetworkService(
-                    jsonRpcProviders = OptimismProvidersBuilder(input.providerTypes, input.config).build(blockchain),
+                    jsonRpcProviders = getProvidersBuilder(blockchain, input.providerTypes, input.config)
+                        .build(blockchain),
                 ),
             )
+        }
+    }
+
+    private fun getProvidersBuilder(
+        blockchain: Blockchain,
+        providerTypes: List<ProviderType>,
+        config: BlockchainSdkConfig,
+    ): NetworkProvidersBuilder<EthereumJsonRpcProvider> {
+        return when (blockchain) {
+            Blockchain.Optimism, Blockchain.OptimismTestnet -> OptimismProvidersBuilder(providerTypes, config)
+            Blockchain.Base, Blockchain.BaseTestnet -> BaseProvidersBuilder(providerTypes, config)
+            Blockchain.Manta, Blockchain.MantaTestnet -> MantaProvidersBuilder(providerTypes)
+            else -> error("Unsupported blockchain: $blockchain")
         }
     }
 }
