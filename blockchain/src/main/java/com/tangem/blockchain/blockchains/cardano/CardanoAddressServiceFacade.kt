144e9215c006f5081cf17a362bf839291b2b8e04
Tangem <git@tangem.com>
2024-06-26 22:25:50 +0800

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/CardanoAddressServiceFacade.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/cardano/CardanoAddressServiceFacade.kt
@@ -3,10 +3,13 @@ package com.tangem.blockchain.blockchains.cardano
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.address.Address
 import com.tangem.blockchain.common.address.AddressService
+import com.tangem.blockchain.common.address.ContractAddressValidator
 import com.tangem.blockchain.common.address.TrustWalletAddressService
+import com.tangem.blockchain.extensions.decodeBech32
+import com.tangem.blockchain.extensions.isValidHex
 import com.tangem.common.card.EllipticCurve
 
-internal class CardanoAddressServiceFacade : AddressService() {
+internal class CardanoAddressServiceFacade : AddressService(), ContractAddressValidator {
 
     private val legacyService = CardanoAddressService(Blockchain.Cardano)
     private val trustWalletService = TrustWalletAddressService(Blockchain.Cardano)
@@ -29,5 +32,28 @@ internal class CardanoAddressServiceFacade : AddressService() {
         } else {
             legacyService.makeAddresses(walletPublicKey, curve)
         }
+    }
+
+    /**
+     * Validate contract [address]
+     *
+     * @see <a href="https://tangem.slack.com/archives/GMXC6PP71/p1718980475109089?thread_ts=1718980364.635649&
+     * cid=GMXC6PP71">Algorithm</a>
+     */
+    override fun validateContractAddress(address: String): Boolean {
+        // is fingerprint?
+        if (address.decodeBech32() != null) return true
+
+        if (!address.isValidHex()) return false
+
+        /*
+         * PolicyID: length == CONTRACT_ADDRESS_MIN_LENGTH
+         * AssetID:  length > CONTRACT_ADDRESS_MIN_LENGTH
+         */
+        return address.length >= CONTRACT_ADDRESS_MIN_LENGTH
+    }
+
+    private companion object {
+        const val CONTRACT_ADDRESS_MIN_LENGTH = 56
     }
 }
