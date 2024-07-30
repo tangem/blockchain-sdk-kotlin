1f40a08db4004cbaff1087f02a1980f0ad991b19
Mama1emon <andrew.khokhlove@gmail.com>
2024-07-30 11:20:53 +0300

    AND-7714 Add Filecoin's API

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/filecoin/network/request/FilecoinRpcBody.kt
@@ -0,0 +1,33 @@
+package com.tangem.blockchain.blockchains.filecoin.network.request
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+/**
+ * Filecoin RPC body
+ *
+ * @author Andrew Khokhlov on 24/07/2024
+ */
+@JsonClass(generateAdapter = true)
+internal data class FilecoinRpcBody(
+    @Json(name = "id") val id: Int,
+    @Json(name = "jsonrpc") val jsonrpc: String,
+    @Json(name = "method") val method: Method,
+    @Json(name = "params") val params: List<Any?>,
+) {
+
+    enum class Method {
+
+        @Json(name = "Filecoin.StateGetActor")
+        GetActorInfo,
+
+        @Json(name = "Filecoin.GasEstimateFeeCap")
+        GetGasUnitPrice,
+
+        @Json(name = "Filecoin.GasEstimateGasLimit")
+        GetGasLimit,
+
+        @Json(name = "Filecoin.MpoolPush")
+        SubmitTransaction,
+    }
+}
