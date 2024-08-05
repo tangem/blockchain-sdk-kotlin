1dddac40abfa608b5edde557ca0a17bdbd3fff5c
Tangem <git@tangem.com>
2024-08-05 18:16:09 +0300

    Updated on 2026-02-11

--- blockchain/src/main/java/com/tangem/blockchain/common/NetworkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/NetworkProvider.kt
@@ -13,12 +13,52 @@ interface NetworkProvider {
  * Example: https://base-url/postfix-url        – isn't working
  *          https://base-url/   +   postfix-url – is working
  */
-fun <T : NetworkProvider> createWithPostfixIfContained(
+internal fun <T : NetworkProvider> createWithPostfixIfContained(
     baseUrl: String,
     postfixUrl: String,
     create: (baseUrl: String, postfixUrl: String) -> T,
 ): T {
-    return if (baseUrl.endsWith(suffix = "/$postfixUrl/")) {
+    return createWithPostfixIfContainedInternal(
+        isUrlWithPostfix = baseUrl.endsWith(suffix = "/$postfixUrl/"),
+        baseUrl = baseUrl,
+        postfixUrl = postfixUrl,
+        create = create,
+    )
+}
+
+/**
+ * Create instance [NetworkProvider].
+
+ * and [postfixUrl] will be put in each request.
+ *
+ * This mechanism helps to support urls without '/' in the end.
+ * Example: https://base-url/postfix-url        – isn't working
+ *          https://base-url/   +   postfix-url – is working
+ */
+internal fun <T : NetworkProvider> createWithPostfixIfContained(
+    baseUrl: String,
+    vararg postfixUrl: String,
+    create: (baseUrl: String, postfixUrl: String) -> T,
+): T {
+    val postfix = postfixUrl
+        .filter(String::isNotBlank)
+        .firstOrNull { baseUrl.endsWith(suffix = "/$it/") }
+
+    return createWithPostfixIfContainedInternal(
+        isUrlWithPostfix = postfix != null,
+        baseUrl = baseUrl,
+        postfixUrl = postfix.orEmpty(),
+        create = create,
+    )
+}
+
+private fun <T> createWithPostfixIfContainedInternal(
+    isUrlWithPostfix: Boolean,
+    baseUrl: String,
+    postfixUrl: String,
+    create: (baseUrl: String, postfixUrl: String) -> T,
+): T {
+    return if (isUrlWithPostfix) {
         create(baseUrl.substringBeforeLast(delimiter = "$postfixUrl/"), postfixUrl)
     } else {
         create(baseUrl, "")
