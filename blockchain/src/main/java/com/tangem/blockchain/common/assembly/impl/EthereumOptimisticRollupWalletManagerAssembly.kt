829b074ae7dbfdc567d8066d28a9a6d967feeed2
Tangem <git@tangem.com>
2024-08-05 16:08:40 +0300

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/EthereumOptimisticRollupWalletManagerAssembly.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/EthereumOptimisticRollupWalletManagerAssembly.kt
@@ -5,6 +5,7 @@ import com.tangem.blockchain.blockchains.ethereum.network.EthereumJsonRpcProvide
 import com.tangem.blockchain.blockchains.ethereum.network.EthereumNetworkService
 import com.tangem.blockchain.blockchains.ethereum.providers.BaseProvidersBuilder
 import com.tangem.blockchain.blockchains.ethereum.providers.BlastProvidersBuilder
+import com.tangem.blockchain.blockchains.ethereum.providers.CyberProvidersBuilder
 import com.tangem.blockchain.blockchains.ethereum.providers.MantaProvidersBuilder
 import com.tangem.blockchain.blockchains.optimism.EthereumOptimisticRollupWalletManager
 import com.tangem.blockchain.blockchains.optimism.OptimismProvidersBuilder
@@ -44,6 +45,7 @@ internal object EthereumOptimisticRollupWalletManagerAssembly :
             Blockchain.Base, Blockchain.BaseTestnet -> BaseProvidersBuilder(providerTypes, config)
             Blockchain.Manta, Blockchain.MantaTestnet -> MantaProvidersBuilder(providerTypes)
             Blockchain.Blast, Blockchain.BlastTestnet -> BlastProvidersBuilder(providerTypes, config)
+            Blockchain.Cyber, Blockchain.CyberTestnet -> CyberProvidersBuilder(providerTypes)
             else -> error("Unsupported blockchain: $blockchain")
         }
     }
