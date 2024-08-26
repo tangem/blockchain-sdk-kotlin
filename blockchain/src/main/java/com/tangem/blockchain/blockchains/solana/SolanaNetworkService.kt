10c0fdf9d09080b23a1b925eeb2c3074c628f266
Tangem <git@tangem.com>
2024-08-26 14:14:36 +0500

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/solana/SolanaNetworkService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/solana/SolanaNetworkService.kt
@@ -1,5 +1,6 @@
 package com.tangem.blockchain.blockchains.solana
 
+import android.os.SystemClock
 import com.tangem.blockchain.blockchains.solana.solanaj.core.SolanaTransaction
 import com.tangem.blockchain.blockchains.solana.solanaj.model.*
 import com.tangem.blockchain.blockchains.solana.solanaj.program.SolanaTokenProgram
@@ -9,10 +10,7 @@ import com.tangem.blockchain.common.BlockchainSdkError.Solana
 import com.tangem.blockchain.common.NetworkProvider
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.successOr
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import org.p2p.solanaj.core.PublicKey
 import org.p2p.solanaj.rpc.types.SignatureStatuses
 import org.p2p.solanaj.rpc.types.config.Commitment
@@ -160,7 +158,7 @@ internal class SolanaNetworkService(
 
     suspend fun getFeeForMessage(transaction: SolanaTransaction): Result<FeeInfo> = withContext(Dispatchers.IO) {
         try {
-            val params = provider.api.getFeeForMessage(transaction, Commitment.PROCESSED)
+            val params = provider.api.getFeeForMessage(transaction, Commitment.CONFIRMED)
             Result.Success(params)
         } catch (ex: Exception) {
             Result.Failure(Solana.Api(ex))
@@ -176,20 +174,40 @@ internal class SolanaNetworkService(
         }
     }
 
-    suspend fun sendTransaction(signedTransaction: ByteArray): Result<String> = withContext(Dispatchers.IO) {
-        try {
-            val result = provider.api.sendSignedTransaction(signedTransaction)
-            Result.Success(result)
-        } catch (ex: Exception) {
-            Result.Failure(Solana.Api(ex))
+    suspend fun sendTransaction(signedTransaction: ByteArray, startSendingTimestamp: Long): Result<String> {
+        return withContext(Dispatchers.IO) {
+            val elapsedTime = SystemClock.elapsedRealtime() - startSendingTimestamp
+
+            delay(START_DELAY - elapsedTime)
+            trySend(signedTransaction)
         }
     }
 
-    suspend fun getRecentBlockhash(commitment: Commitment? = null): Result<String> = withContext(Dispatchers.IO) {
-        try {
-            Result.Success(provider.api.getRecentBlockhash(commitment))
-        } catch (ex: Exception) {
-            Result.Failure(Solana.Api(ex))
+    private suspend fun trySend(signedTransaction: ByteArray, repeat: Int = MAX_RETRY_COUNT): Result<String> {
+        return try {
+            val result = provider.api.sendSignedTransaction(signedTransaction)
+            Result.Success(result)
+        } catch (e: Exception) {
+            if (!e.isBlockhashNotFound()) { // retry only if blockchain not found
+                delay(RETRY_DELAY)
+                if (repeat > 1) { // limit retry count
+                    trySend(signedTransaction, repeat - 1)
+                } else {
+                    Result.Failure(Solana.Api(e))
+                }
+            } else {
+                Result.Failure(Solana.Api(e))
+            }
+        }
+    }
+
+    suspend fun getRecentBlockhash(commitment: Commitment = Commitment.CONFIRMED): Result<String> {
+        return withContext(Dispatchers.IO) {
+            try {
+                Result.Success(provider.api.getRecentBlockhash(commitment))
+            } catch (ex: Exception) {
+                Result.Failure(Solana.Api(ex))
+            }
         }
     }
 
@@ -203,4 +221,17 @@ internal class SolanaNetworkService(
                 Result.Failure(Solana.Api(ex))
             }
         }
+
+    private fun Exception.isBlockhashNotFound(): Boolean {
+        return message?.contains(BLOCKHASH_NOT_FOUND_ERROR) ?: false
+    }
+
+    companion object {
+        // According to blockchain specifications and blockchain analytics
+        const val START_DELAY = 15_000L
+        const val RETRY_DELAY = 3_000L
+        const val MAX_RETRY_COUNT = 5
+        // Error message if blockhash not found, no code provided by RpcException
+        const val BLOCKHASH_NOT_FOUND_ERROR = "Blockhash not found"
+    }
 }
