f209f9090ffd9b26be3137ecbf9366562088a9d5
Tangem <git@tangem.com>
2024-08-28 14:14:20 +0500

    Updated on 2025-10-09

--- blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/SeiWalletManagerAssembly.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/SeiWalletManagerAssembly.kt
@@ -1,12 +1,22 @@
 package com.tangem.blockchain.common.assembly.impl
 
-import com.tangem.blockchain.blockchains.sei.SeiWalletManager
+import com.tangem.blockchain.blockchains.cosmos.CosmosWalletManager
+import com.tangem.blockchain.blockchains.cosmos.network.CosmosChain
+import com.tangem.blockchain.blockchains.sei.SeiProvidersBuilder
 import com.tangem.blockchain.common.assembly.WalletManagerAssembly
 import com.tangem.blockchain.common.assembly.WalletManagerAssemblyInput
 
-internal object SeiWalletManagerAssembly : WalletManagerAssembly<SeiWalletManager>() {
+internal object SeiWalletManagerAssembly : WalletManagerAssembly<CosmosWalletManager>() {
 
-    override fun make(input: WalletManagerAssemblyInput): SeiWalletManager {
-        return SeiWalletManager(wallet = input.wallet)
+    override fun make(input: WalletManagerAssemblyInput): CosmosWalletManager {
+        return with(input.wallet) {
+            val testnet = blockchain.isTestnet()
+
+            CosmosWalletManager(
+                wallet = input.wallet,
+                networkProviders = SeiProvidersBuilder(input.providerTypes).build(blockchain),
+                cosmosChain = CosmosChain.Sei(testnet),
+            )
+        }
     }
 }
