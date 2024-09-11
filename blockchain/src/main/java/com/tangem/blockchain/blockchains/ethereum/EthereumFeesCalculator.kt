5b5eb4cb108f21fa7e2ed56626f08a873ff795e2
Tangem <git@tangem.com>
2024-09-11 15:32:48 +0300

    Updated on 2025-11-03

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/EthereumFeesCalculator.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/EthereumFeesCalculator.kt
@@ -1,5 +1,6 @@
 package com.tangem.blockchain.blockchains.ethereum
 
+import com.tangem.blockchain.blockchains.ethereum.network.EthereumFeeHistory
 import com.tangem.blockchain.common.Amount
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.transaction.Fee
@@ -50,6 +51,47 @@ class EthereumFeesCalculator {
         )
     }
 
+    internal fun calculateEip1559Fees(
+        amountParams: Amount,
+        gasLimit: BigInteger,
+        feeHistory: EthereumFeeHistory,
+    ): TransactionFee.Choosable {
+        val gasLimitDecimal = BigDecimal(gasLimit)
+
+        val minMaxFeePerGas = feeHistory.baseFee * minimalMultiplier + feeHistory.lowPriorityFee
+        val normalMaxFeePerGas = feeHistory.baseFee * normalMultiplier + feeHistory.marketPriorityFee
+        val priorityMaxFeePerGas = feeHistory.baseFee * priorityMultiplier + feeHistory.fastPriorityFee
+
+        val minFee = minMaxFeePerGas * gasLimitDecimal
+        val normalFee = normalMaxFeePerGas * gasLimitDecimal
+        val priorityFee = priorityMaxFeePerGas * gasLimitDecimal
+
+        val minimalFeeBigInt = minFee.toBigInteger()
+        val normalFeeBigInt = normalFee.toBigInteger()
+        val priorityFeeBigInt = priorityFee.toBigInteger()
+
+        return TransactionFee.Choosable(
+            minimum = Fee.Ethereum.EIP1559(
+                amount = createFee(amountParams, minimalFeeBigInt),
+                gasLimit = gasLimit,
+                maxFeePerGas = minMaxFeePerGas.toBigInteger(),
+                priorityFee = feeHistory.lowPriorityFee.toBigInteger(),
+            ),
+            normal = Fee.Ethereum.EIP1559(
+                amount = createFee(amountParams, normalFeeBigInt),
+                gasLimit = gasLimit,
+                maxFeePerGas = normalMaxFeePerGas.toBigInteger(),
+                priorityFee = feeHistory.marketPriorityFee.toBigInteger(),
+            ),
+            priority = Fee.Ethereum.EIP1559(
+                amount = createFee(amountParams, priorityFeeBigInt),
+                gasLimit = gasLimit,
+                maxFeePerGas = priorityMaxFeePerGas.toBigInteger(),
+                priorityFee = feeHistory.fastPriorityFee.toBigInteger(),
+            ),
+        )
+    }
+
     internal fun calculateSingleFee(
         amountParams: Amount,
         gasLimit: BigInteger,
@@ -69,17 +111,38 @@ class EthereumFeesCalculator {
         )
     }
 
+    internal fun calculateEip1559SingleFee(
+        amountParams: Amount,
+        gasLimit: BigInteger,
+        feeHistory: EthereumFeeHistory,
+    ): TransactionFee.Single {
+        val gasLimitDecimal = BigDecimal(gasLimit)
+
+        val normalMaxFeePerGas = feeHistory.baseFee * normalMultiplier + feeHistory.marketPriorityFee
+
+        val normalFee = normalMaxFeePerGas * gasLimitDecimal
+
+        return TransactionFee.Single(
+            normal = Fee.Ethereum.EIP1559(
+                amount = createFee(amountParams, normalFee.toBigInteger()),
+                gasLimit = gasLimit,
+                maxFeePerGas = normalMaxFeePerGas.toBigInteger(),
+                priorityFee = feeHistory.marketPriorityFee.toBigInteger(),
+            ),
+        )
+    }
+
     private fun createFee(amountParams: Amount, value: BigInteger): Amount {
         return Amount(
-            amountParams,
-            value.toBigDecimal(
+            amount = amountParams,
+            value = value.toBigDecimal(
                 scale = Blockchain.Ethereum.decimals(),
                 mathContext = MathContext(Blockchain.Ethereum.decimals(), RoundingMode.HALF_EVEN),
             ),
         )
     }
 
-    companion object {
+    private companion object {
 
         val minimalMultiplier: BigDecimal = BigDecimal.valueOf(1)
 
