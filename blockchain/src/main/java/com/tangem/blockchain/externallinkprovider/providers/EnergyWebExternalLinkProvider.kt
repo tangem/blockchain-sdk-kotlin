19b5c58c13997ec260d3b34cd3cb134d4598b0be
Tangem <git@tangem.com>
2024-09-19 00:35:13 +0500

    Updated on 2025-11-28

---
+++ blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/EnergyWebExternalLinkProvider.kt
@@ -0,0 +1,23 @@
+package com.tangem.blockchain.externallinkprovider.providers
+
+import com.tangem.blockchain.externallinkprovider.ExternalLinkProvider
+import com.tangem.blockchain.externallinkprovider.TxExploreState
+
+internal class EnergyWebExternalLinkProvider(isTestnet: Boolean) : ExternalLinkProvider {
+
+    override val explorerBaseUrl = if (isTestnet) {
+        "https://volta-explorer.energyweb.org/"
+    } else {
+        "https://explorer.energyweb.org/"
+    }
+
+    override val testNetTopUpUrl: String = "https://voltafaucet.energyweb.org/"
+
+    override fun explorerUrl(walletAddress: String, contractAddress: String?): String {
+        return "${explorerBaseUrl}address/$walletAddress"
+    }
+
+    override fun getExplorerTxUrl(transactionHash: String): TxExploreState {
+        return TxExploreState.Url(url = "${explorerBaseUrl}tx/$transactionHash")
+    }
+}
