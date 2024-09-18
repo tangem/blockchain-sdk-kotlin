1f623ddceba3f7dde9f961943d1033e09103616d
Tangem <git@tangem.com>
2024-09-19 00:35:13 +0500

    Updated on 2025-12-30

--- blockchain/src/main/java/com/tangem/blockchain/common/derivation/DerivationConfigV2.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/derivation/DerivationConfigV2.kt
@@ -72,6 +72,7 @@ object DerivationConfigV2 : DerivationConfig() {
             Blockchain.Taraxa,
             Blockchain.Blast,
             Blockchain.Cyber,
+            Blockchain.EnergyWebChain,
             -> mapOf(AddressType.Default to DerivationPath("m/44'/60'/0'/0/0"))
             Blockchain.XDC -> mapOf(AddressType.Default to DerivationPath("m/44'/550'/0'/0/0"))
             Blockchain.Binance -> mapOf(AddressType.Default to DerivationPath("m/44'/714'/0'/0/0"))
@@ -143,6 +144,7 @@ object DerivationConfigV2 : DerivationConfig() {
             Blockchain.TaraxaTestnet,
             Blockchain.BlastTestnet,
             Blockchain.CyberTestnet,
+            Blockchain.EnergyWebChainTestnet,
             -> mapOf(AddressType.Default to DerivationPath("m/44'/1'/0'/0/0"))
             Blockchain.Aptos,
             Blockchain.AptosTestnet,
