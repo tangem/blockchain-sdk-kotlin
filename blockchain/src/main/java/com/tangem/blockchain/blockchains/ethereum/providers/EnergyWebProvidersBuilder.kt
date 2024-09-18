ca0fbea1966ab46a3539043781db8fd52999410b
Tangem <git@tangem.com>
2024-09-19 00:35:13 +0500

    Updated on 2026-05-04

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/providers/EnergyWebProvidersBuilder.kt
@@ -0,0 +1,16 @@
+package com.tangem.blockchain.blockchains.ethereum.providers
+
+import com.tangem.blockchain.blockchains.ethereum.network.EthereumJsonRpcProvider
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchain.common.network.providers.OnlyPublicProvidersBuilder
+import com.tangem.blockchain.common.network.providers.ProviderType
+
+internal class EnergyWebProvidersBuilder(
+    override val providerTypes: List<ProviderType>,
+) : OnlyPublicProvidersBuilder<EthereumJsonRpcProvider>(
+    providerTypes = providerTypes,
+    testnetProviders = listOf("https://volta-rpc.energyweb.org/"),
+) {
+
+    override fun createProvider(url: String, blockchain: Blockchain) = EthereumJsonRpcProvider(url)
+}
