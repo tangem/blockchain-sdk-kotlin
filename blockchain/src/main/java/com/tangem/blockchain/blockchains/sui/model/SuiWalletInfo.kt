e308ec0d850bc812c97a2124b0113190a4953917
Tangem <git@tangem.com>
2024-09-26 18:37:20 +0400

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/sui/model/SuiWalletInfo.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/sui/model/SuiWalletInfo.kt
@@ -3,6 +3,6 @@ package com.tangem.blockchain.blockchains.sui.model
 import java.math.BigDecimal
 
 internal data class SuiWalletInfo(
-    val totalBalance: BigDecimal,
-    val suiCoins: List<SuiCoin>,
+    val suiTotalBalance: BigDecimal,
+    val coins: List<SuiCoin>,
 )
