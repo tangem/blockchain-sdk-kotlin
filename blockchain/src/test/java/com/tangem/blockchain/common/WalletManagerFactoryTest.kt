fc065856bf0074f919992d53c2eb08a9a9be76b6
Tangem <git@tangem.com>
2024-10-01 16:35:17 +0400

    releases/5.27.0-1144

--- blockchain/src/test/java/com/tangem/blockchain/common/WalletManagerFactoryTest.kt
+++ blockchain/src/test/java/com/tangem/blockchain/common/WalletManagerFactoryTest.kt
@@ -203,7 +203,6 @@ internal class WalletManagerFactoryTest {
                     base = GetBlockAccessToken(),
                     blast = GetBlockAccessToken(),
                     filecoin = GetBlockAccessToken(),
-                    sui = GetBlockAccessToken(),
                 ),
                 tronGridApiKey = "",
                 chiaFireAcademyApiKey = "",
