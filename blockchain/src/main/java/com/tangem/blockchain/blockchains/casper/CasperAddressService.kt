b2f62fa9a66b6bd9377463951dc1596a2c2e8931
Tangem <git@tangem.com>
2024-10-11 06:18:17 +0300

    Updated on 2026-01-28

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/casper/CasperAddressService.kt
@@ -0,0 +1,33 @@
+package com.tangem.blockchain.blockchains.casper
+
+import com.tangem.blockchain.blockchains.casper.cashaddr.CasperAddressUtils.checksum
+import com.tangem.blockchain.common.address.AddressService
+import com.tangem.blockchain.extensions.isSameCase
+import com.tangem.common.card.EllipticCurve
+import com.tangem.common.extensions.hexToBytes
+
+internal class CasperAddressService : AddressService() {
+    override fun makeAddress(walletPublicKey: ByteArray, curve: EllipticCurve?): String {
+        val prefix = CasperConstants.getAddressPrefix(curve!!)
+        val bytes = prefix.hexToBytes() + walletPublicKey
+        return bytes.checksum()
+    }
+
+    override fun validate(address: String): Boolean {
+        val isCorrectEd25519Address = address.length == CasperConstants.ED25519_LENGTH &&
+            address.startsWith(CasperConstants.ED25519_PREFIX)
+        val isCorrectSecp256k1Address = address.length == CasperConstants.SECP256K1_LENGTH &&
+            address.startsWith(CasperConstants.SECP256K1_PREFIX)
+
+        if (!isCorrectEd25519Address && !isCorrectSecp256k1Address) {
+            return false
+        }
+
+        // don't check checksum if it's not mixed case
+        if (address.isSameCase()) {
+            return true
+        }
+
+        return address == address.hexToBytes().checksum()
+    }
+}
