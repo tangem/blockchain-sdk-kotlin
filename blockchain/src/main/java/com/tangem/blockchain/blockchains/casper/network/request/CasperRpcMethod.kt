a72259b2f6f5b00769d5f71d37c74c98a6614f87
Tangem <git@tangem.com>
2024-10-16 12:37:30 +0500

    Updated on 2025-11-28

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/casper/network/request/CasperRpcMethod.kt
@@ -0,0 +1,11 @@
+package com.tangem.blockchain.blockchains.casper.network.request
+
+/**
+ * Casper rpc method
+ *
+ * @property name method name
+ */
+internal sealed class CasperRpcMethod(val name: String) {
+
+    data object QueryBalance : CasperRpcMethod(name = "query_balance")
+}
