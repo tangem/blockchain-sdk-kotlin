b2ce796152ada626840f030c9974ebae0d6fbf2c
Tangem <git@tangem.com>
2024-10-16 12:40:37 +0500

    Updated on 2026-05-04

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/casper/network/CasperNetworkService.kt
@@ -0,0 +1,20 @@
+package com.tangem.blockchain.blockchains.casper.network
+
+import com.tangem.blockchain.blockchains.casper.models.CasperBalance
+import com.tangem.blockchain.extensions.Result
+import com.tangem.blockchain.network.MultiNetworkProvider
+import java.math.BigDecimal
+
+internal class CasperNetworkService(
+    providers: List<CasperNetworkProvider>
+): CasperNetworkProvider {
+
+    override val baseUrl: String
+        get() = multiJsonRpcProvider.currentProvider.baseUrl
+
+    private val multiJsonRpcProvider = MultiNetworkProvider(providers)
+
+    override suspend fun getBalance(address: String): Result<CasperBalance> {
+        return multiJsonRpcProvider.performRequest(CasperNetworkProvider::getBalance, address)
+    }
+}
