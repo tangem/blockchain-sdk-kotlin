a350c5cb913583f17d87c49fe4d29db6dfc63dbb
Tangem <git@tangem.com>
2024-10-18 01:43:44 +0500

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/common/derivation/DerivationConfigV2.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/derivation/DerivationConfigV2.kt
@@ -171,7 +171,7 @@ object DerivationConfigV2 : DerivationConfig() {
             -> mapOf(AddressType.Default to DerivationPath("m/44'/246'/0'/0'/0'"))
             Blockchain.Casper,
             Blockchain.CasperTestnet,
-            -> mapOf(AddressType.Default to DerivationPath("m/44'/506'/0'/0'/0'"))
+            -> mapOf(AddressType.Default to DerivationPath("m/44'/506'/0'/0/0"))
         }
     }
 }
