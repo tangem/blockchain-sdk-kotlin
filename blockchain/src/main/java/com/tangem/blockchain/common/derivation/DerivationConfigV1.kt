0dc25bfb4b86c7c098ef152689c84e2554d9b234
Evgenii Kuzovkin <nzeeei@gmail.com>
2024-10-18 01:43:44 +0500

    Use only Secp256k1 and right derivation for it

--- blockchain/src/main/java/com/tangem/blockchain/common/derivation/DerivationConfigV1.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/derivation/DerivationConfigV1.kt
@@ -171,7 +171,7 @@ object DerivationConfigV1 : DerivationConfig() {
             -> mapOf(AddressType.Default to DerivationPath("m/44'/246'/0'/0'/0'"))
             Blockchain.Casper,
             Blockchain.CasperTestnet,
-            -> mapOf(AddressType.Default to DerivationPath("m/44'/506'/0'/0'/0'"))
+            -> mapOf(AddressType.Default to DerivationPath("m/44'/506'/0'/0/0"))
         }
     }
 }
