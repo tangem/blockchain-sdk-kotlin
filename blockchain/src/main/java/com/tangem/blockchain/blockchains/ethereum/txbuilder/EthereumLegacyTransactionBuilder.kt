0e7b52e75b8b22a741d97ca561cd1ab460d8a723
Tangem <git@tangem.com>
2024-10-22 11:07:28 +0400

    Updated on 2025-08-28

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/txbuilder/EthereumLegacyTransactionBuilder.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/txbuilder/EthereumLegacyTransactionBuilder.kt
@@ -2,11 +2,9 @@ package com.tangem.blockchain.blockchains.ethereum.txbuilder
 
 import com.tangem.blockchain.blockchains.ethereum.EthereumUtils
 import com.tangem.blockchain.common.Amount
-import com.tangem.blockchain.common.HEX_PREFIX
 import com.tangem.blockchain.common.TransactionData
 import com.tangem.blockchain.common.Wallet
 import com.tangem.blockchain.common.transaction.Fee
-import com.tangem.common.extensions.hexToBytes
 import org.kethereum.extensions.transactions.encode
 import org.kethereum.model.Address
 import org.kethereum.model.createTransactionWithDefaults
@@ -44,30 +42,16 @@ internal class EthereumLegacyTransactionBuilder(
         data: String?,
         fee: Fee.Ethereum,
     ): ByteArray {
-        val value = amount.value?.toBigInteger() ?: BigInteger.ZERO
         val legacyFee = fee as Fee.Ethereum.Legacy
 
-        return if (data == null) {
-            createTransactionWithDefaults(
-                from = Address(wallet.address),
-                to = Address(destination),
-                value = value, // value is not important for dummy transactions
-                gasPrice = legacyFee.gasPrice,
-                gasLimit = legacyFee.gasLimit,
-                nonce = BigInteger.ONE,
-            )
-        } else {
-            createTransactionWithDefaults(
-                from = Address(wallet.address),
-                to = Address(destination),
-                value = value, // value is not important for dummy transactions
-                gasPrice = legacyFee.gasPrice,
-                gasLimit = legacyFee.gasLimit,
-                nonce = BigInteger.ONE,
-                input = data.removePrefix(HEX_PREFIX).hexToBytes(),
-            )
-        }
-            .encode()
+        return createTransactionWithDefaults(
+            from = Address(wallet.address),
+            to = Address(destination),
+            value = BigInteger.valueOf(1), // value is not important for dummy transactions
+            gasPrice = legacyFee.gasPrice,
+            gasLimit = legacyFee.gasLimit,
+            nonce = BigInteger.ONE,
+        ).encode()
     }
 
     private companion object {
