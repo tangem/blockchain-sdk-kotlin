5135a6459bdd0b422ea5e81e57e45da141306ba1
Tangem <git@tangem.com>
2024-11-07 12:51:39 +0700

    Updated on 2026-07-23

---
+++ blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/ChilizExternalLinkProvider.kt
@@ -0,0 +1,21 @@
+package com.tangem.blockchain.externallinkprovider.providers
+
+import com.tangem.blockchain.externallinkprovider.ExternalLinkProvider
+import com.tangem.blockchain.externallinkprovider.TxExploreState
+
+internal class ChilizExternalLinkProvider(isTestnet: Boolean) : ExternalLinkProvider {
+
+    override val explorerBaseUrl: String = if (isTestnet) {
+        "https://testnet.chiliscan.com/"
+    } else {
+        "https://chiliscan.com/"
+    }
+
+    override fun explorerUrl(walletAddress: String, contractAddress: String?): String {
+        return "${explorerBaseUrl}address/$walletAddress"
+    }
+
+    override fun getExplorerTxUrl(transactionHash: String): TxExploreState {
+        return TxExploreState.Url("${explorerBaseUrl}tx/$transactionHash")
+    }
+}
