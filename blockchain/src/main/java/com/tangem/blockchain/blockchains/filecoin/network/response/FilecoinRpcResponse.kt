82a60eeb9e2263e6272cdf40bb9a9b8a7bc1215e
Tangem <git@tangem.com>
2024-11-12 14:11:58 +0500

    Updated on 2026-02-02

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/filecoin/network/response/FilecoinRpcResponse.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/filecoin/network/response/FilecoinRpcResponse.kt
@@ -7,11 +7,16 @@ import com.squareup.moshi.JsonClass
 internal sealed interface FilecoinRpcResponse {
 
     /** Success response with [result] */
-    data class Success(val result: Any) : FilecoinRpcResponse
+    @JsonClass(generateAdapter = true)
+    data class Success(
+        @Json(name = "result")
+        val result: Any,
+    ) : FilecoinRpcResponse
 
     /** Failure response with error [message] */
     @JsonClass(generateAdapter = true)
     data class Failure(
-        @Json(name = "message") val message: String,
+        @Json(name = "message")
+        val message: String,
     ) : FilecoinRpcResponse
 }
