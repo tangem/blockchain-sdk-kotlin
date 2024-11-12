2eb8ee34c76e712afda42fd7480bf442ae69e093
Tangem <git@tangem.com>
2024-11-12 14:11:58 +0500

    Updated on 2026-02-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/tron/network/TronNetworkModels.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/tron/network/TronNetworkModels.kt
@@ -19,39 +19,47 @@ data class TronChainParameters(
 
 @JsonClass(generateAdapter = true)
 data class TronGetAccountRequest(
+    @Json(name = "address")
     val address: String,
+    @Json(name = "visible")
     val visible: Boolean,
 )
 
 @JsonClass(generateAdapter = true)
 data class TronEnergyFeeData(
+    @Json(name = "energyFee")
     val energyFee: Long,
+    @Json(name = "sunPerEnergyUnit")
     val sunPerEnergyUnit: Long,
 )
 
 @JsonClass(generateAdapter = true)
 data class TronGetAccountResponse(
+    @Json(name = "balance")
     val balance: Long?,
     // We use [address] field to distinguish this response from
     // an empty JSON that we get if account hasn't been activated
+    @Json(name = "address")
     val address: String?,
 )
 
 @JsonClass(generateAdapter = true)
 data class TronGetAccountResourceResponse(
-    val freeNetUsed: Long?,
-    val freeNetLimit: Long,
+    @Json(name = "freeNetUsed") val freeNetUsed: Long?,
+    @Json(name = "freeNetLimit") val freeNetLimit: Long,
     @Json(name = "EnergyLimit") val energyLimit: Long?,
     @Json(name = "EnergyUsed") val energyUsed: Long?,
 )
 
 @JsonClass(generateAdapter = true)
 data class TronTransactionInfoRequest(
+    @Json(name = "value")
     val value: String,
 )
 
 @JsonClass(generateAdapter = true)
 data class TronTransactionInfoResponse(
+    @Json(name = "id")
     val id: String,
 )
 
@@ -63,7 +71,9 @@ data class TronChainParametersResponse(
 
 @JsonClass(generateAdapter = true)
 data class TronChainParameter(
+    @Json(name = "key")
     val key: String,
+    @Json(name = "value")
     val value: Long? = null,
 )
 
@@ -81,23 +91,31 @@ data class BlockHeader(
 
 @JsonClass(generateAdapter = true)
 data class RawData(
+    @Json(name = "number")
     val number: Long,
+    @Json(name = "txTrieRoot")
     val txTrieRoot: String,
     @Json(name = "witness_address")
     val witnessAddress: String,
+    @Json(name = "parentHash")
     val parentHash: String,
+    @Json(name = "version")
     val version: Int,
+    @Json(name = "timestamp")
     val timestamp: Long,
 )
 
 @JsonClass(generateAdapter = true)
 data class TronBroadcastRequest(
+    @Json(name = "transaction")
     val transaction: String,
 )
 
 @JsonClass(generateAdapter = true)
 data class TronBroadcastResponse(
+    @Json(name = "result")
     val result: Boolean,
+    @Json(name = "txid")
     val txid: String,
     @Json(name = "message")
     val errorMessage: String?,
