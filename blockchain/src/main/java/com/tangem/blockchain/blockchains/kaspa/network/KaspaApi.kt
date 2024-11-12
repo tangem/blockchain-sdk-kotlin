825c8000832022ceea98902c12d27b22c7b50e62
Tangem <git@tangem.com>
2024-11-12 14:11:58 +0500

    Updated on 2025-11-28

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/kaspa/network/KaspaApi.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/kaspa/network/KaspaApi.kt
@@ -1,5 +1,6 @@
 package com.tangem.blockchain.blockchains.kaspa.network
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import retrofit2.http.*
 
@@ -24,40 +25,56 @@ interface KaspaApi {
 
 @JsonClass(generateAdapter = true)
 data class KaspaTransactionBody(
+    @Json(name = "transaction")
     val transaction: KaspaTransactionData,
 )
 
 @JsonClass(generateAdapter = true)
 data class KaspaTransactionData(
+    @Json(name = "version")
     val version: Int = 0,
+    @Json(name = "inputs")
     val inputs: List<KaspaInput>,
+    @Json(name = "outputs")
     val outputs: List<KaspaOutput>,
+    @Json(name = "lockTime")
     val lockTime: Int = 0,
+    @Json(name = "subnetworkId")
     val subnetworkId: String = "0000000000000000000000000000000000000000",
 )
 
 @JsonClass(generateAdapter = true)
 data class KaspaInput(
+    @Json(name = "previousOutpoint")
     val previousOutpoint: KaspaPreviousOutpoint,
+    @Json(name = "signatureScript")
     val signatureScript: String,
+    @Json(name = "sequence")
     val sequence: Long = 0,
+    @Json(name = "sigOpCount")
     val sigOpCount: Int = 1,
 )
 
 @JsonClass(generateAdapter = true)
 data class KaspaPreviousOutpoint(
+    @Json(name = "transactionId")
     val transactionId: String,
+    @Json(name = "index")
     val index: Long,
 )
 
 @JsonClass(generateAdapter = true)
 data class KaspaOutput(
+    @Json(name = "amount")
     val amount: Long,
+    @Json(name = "scriptPublicKey")
     val scriptPublicKey: KaspaScriptPublicKey,
 )
 
 @JsonClass(generateAdapter = true)
 data class KaspaScriptPublicKey(
+    @Json(name = "scriptPublicKey")
     val scriptPublicKey: String,
+    @Json(name = "version")
     val version: Int = 0,
 )
