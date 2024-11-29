bd4f483793877c1c07f68a6734b0270ca55d0ae6
Tangem <git@tangem.com>
2024-11-29 14:15:13 +0500

    Updated on 2025-10-09

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/kaspa/krc20/model/IncompleteTokenTransactionParams.kt
@@ -0,0 +1,10 @@
+package com.tangem.blockchain.blockchains.kaspa.krc20.model
+
+import java.math.BigDecimal
+
+data class IncompleteTokenTransactionParams(
+    val transactionId: String,
+    val amountValue: BigDecimal,
+    val feeAmountValue: BigDecimal,
+    val envelope: Envelope,
+)
