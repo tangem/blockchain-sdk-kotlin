d02df9059645139d13c5244dde6f97ec49a891d7
Tangem <git@tangem.com>
2024-12-02 15:10:10 +0700

    Updated on 2026-03-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/factorn/Fact0rnAddressService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/factorn/Fact0rnAddressService.kt
@@ -1,15 +1,36 @@
 package com.tangem.blockchain.blockchains.factorn
 
+import com.tangem.blockchain.blockchains.bitcoin.BitcoinAddressService
+import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.address.AddressService
 import com.tangem.common.card.EllipticCurve
+import com.tangem.common.extensions.toHexString
+import org.bitcoinj.core.SegwitAddress
+import org.bitcoinj.core.Sha256Hash
+import org.bitcoinj.script.Script
+import org.bitcoinj.script.ScriptBuilder
 
 internal class Fact0rnAddressService : AddressService() {
 
+    private val bitcoinAddressService = BitcoinAddressService(Blockchain.Fact0rn)
+
     override fun makeAddress(walletPublicKey: ByteArray, curve: EllipticCurve?): String {
-        TODO("Not yet implemented")
+        return bitcoinAddressService.makeSegwitAddress(walletPublicKey).value
     }
 
     override fun validate(address: String): Boolean {
-        TODO("Not yet implemented")
+        return bitcoinAddressService.validateSegwitAddress(address)
+    }
+
+    companion object {
+
+        internal fun addressToScript(address: String): Script =
+            ScriptBuilder.createOutputScript(SegwitAddress.fromBech32(Fact0rnMainNetParams(), address))
+
+        internal fun addressToScriptHash(address: String): String {
+            val p2pkhScript = addressToScript(address)
+            val sha256Hash = Sha256Hash.hash(p2pkhScript.program)
+            return sha256Hash.reversedArray().toHexString()
+        }
     }
 }
