0b497faf2046b4ae4abcd690e1520ee8a85c2292
Tangem <git@tangem.com>
2024-12-04 19:23:53 +0700

    Updated on 2025-11-03

---
+++ blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/CloreExternalLinkProvider.kt
@@ -0,0 +1,17 @@
+package com.tangem.blockchain.externallinkprovider.providers
+
+import com.tangem.blockchain.externallinkprovider.ExternalLinkProvider
+import com.tangem.blockchain.externallinkprovider.TxExploreState
+
+internal class CloreExternalLinkProvider : ExternalLinkProvider {
+
+    override val explorerBaseUrl: String = "https://clore.cryptoscope.io/"
+
+    override fun explorerUrl(walletAddress: String, contractAddress: String?): String {
+        return explorerBaseUrl + "address/?address=$walletAddress"
+    }
+
+    override fun getExplorerTxUrl(transactionHash: String): TxExploreState {
+        return TxExploreState.Url(explorerBaseUrl + "tx/?txid=$transactionHash")
+    }
+}
