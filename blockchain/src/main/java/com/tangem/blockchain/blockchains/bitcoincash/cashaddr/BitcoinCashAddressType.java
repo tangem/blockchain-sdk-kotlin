96ae43999e8be556cb5ebdb64ce43d0a456307b2
Tangem <git@tangem.com>
2025-01-10 18:32:49 +0500

    Updated on 2026-03-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoincash/cashaddr/BitcoinCashAddressType.java
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/bitcoincash/cashaddr/BitcoinCashAddressType.java
@@ -9,15 +9,21 @@ package com.tangem.blockchain.blockchains.bitcoincash.cashaddr;
  */
 public enum BitcoinCashAddressType {
 
-	P2PKH((byte) 0), P2SH((byte) 8);
+	P2PKH((byte) 0, "q"), P2SH((byte) 8, "p");
 
 	private final byte versionByte;
+	private final String addressPrefix;
 
-	BitcoinCashAddressType(byte versionByte) {
+	BitcoinCashAddressType(byte versionByte, String addressPrefix) {
 		this.versionByte = versionByte;
+		this.addressPrefix = addressPrefix;
 	}
 
 	public byte getVersionByte() {
 		return versionByte;
+	}
+
+	public String getAddressPrefix() {
+		return addressPrefix;
 	}
 }
