7fda6801d4e01b570721912ccfb42899b38ffc52
Tangem <git@tangem.com>
2025-01-14 19:38:06 +0500

    Updated on 2025-09-29

--- blockchain/src/main/java/com/tangem/blockchain/common/transaction/UTXOCollectionUtils.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/transaction/UTXOCollectionUtils.kt
@@ -1,5 +1,6 @@
 package com.tangem.blockchain.common.transaction
 
+import com.tangem.common.extensions.isZero
 import java.math.BigDecimal
 
 /**
@@ -12,6 +13,7 @@ import java.math.BigDecimal
  *     and it ready to search for the next UTXO closest to this amount
  *  4. When all the UTXOs necessary to complete the requested transaction amount have been collected,
  *     the function returns a list of UTXOs sorted in descending order of the amount
+ *  5. If necessary UTXOs result in change with output UTXO less than dust add smallest UTXO
  *
  *  @throws IllegalArgumentException if transactionAmount < 0 or transactionFeeAmount < 0
  *  @param unspentOutputs the list of UTXOs that will be used for selection
@@ -29,6 +31,7 @@ internal inline fun <T> getMinimumRequiredUTXOsToSend(
     unspentOutputs: List<T>,
     transactionAmount: BigDecimal,
     transactionFeeAmount: BigDecimal,
+    dustValue: BigDecimal?,
     crossinline unspentToAmount: (T) -> BigDecimal,
 ): List<T> {
     require(transactionAmount >= BigDecimal.ZERO)
@@ -47,22 +50,23 @@ internal inline fun <T> getMinimumRequiredUTXOsToSend(
     val outputsRes = mutableListOf<T>()
     var currentTotal = BigDecimal.ZERO
 
+    // Searching biggest utxo to full spending amount (transaction amount + fee)
     while (currentTotal < amount && unusedSortedUnspent.size > 0) {
         val binRes = unusedSortedUnspent.binarySearchBy(amount - currentTotal) { unspentToAmount(it) }
-
-        val utxoIndex = when {
-            binRes < 0 -> {
-                val possibleIndex = -binRes - 1
-                if (possibleIndex == unusedSortedUnspent.size) {
-                    possibleIndex - 1
-                } else {
-                    possibleIndex
-                }
-            }
-            else -> binRes
-        }
+        val utxoIndex = getUtxoIndex(binRes, unusedSortedUnspent.size)
 
         val utxo = unusedSortedUnspent[utxoIndex]
+        currentTotal += unspentToAmount(utxo)
+        outputsRes.add(utxo)
+        unusedSortedUnspent.remove(utxo)
+    }
+
+    // Check if resulting spending UTXOs leads to output UTXO having amount less than dust.
+    // In that case add smallest UTXO so output UTXO was greater than dust.
+    val change = currentTotal - amount
+    if (!change.isZero() && dustValue != null && change < dustValue) {
+        val utxo = unusedSortedUnspent.first()
+
         currentTotal += unspentToAmount(utxo)
         outputsRes.add(utxo)
         unusedSortedUnspent.remove(utxo)
@@ -86,6 +90,7 @@ internal inline fun <T> getMinimumRequiredUTXOsToSend(
  *     and it ready to search for the next UTXO closest to this amount
  *  4. When all the UTXOs necessary to complete the requested transaction amount have been collected,
  *     the function returns a list of UTXOs sorted in descending order of the amount
+ *  5. If necessary UTXOs result in change with output UTXO less than dust add smallest UTXO
  *
  *  @throws IllegalArgumentException if transactionAmount < 0 or transactionFeeAmount < 0
  *  @param unspentOutputs the list of UTXOs that will be used for selection
@@ -103,6 +108,7 @@ internal inline fun <T> getMinimumRequiredUTXOsToSendSatoshi(
     unspentOutputs: List<T>,
     transactionSatoshiAmount: Long,
     transactionSatoshiFeeAmount: Long,
+    dustSatoshiValue: Long,
     crossinline unspentToSatoshiAmount: (T) -> Long,
 ): List<T> {
     require(transactionSatoshiAmount >= 0)
@@ -121,20 +127,10 @@ internal inline fun <T> getMinimumRequiredUTXOsToSendSatoshi(
     val outputsRes = mutableListOf<T>()
     var currentTotal = 0L
 
+    // Searching biggest utxo to full spending amount (transaction amount + fee)
     while (currentTotal < amount && unusedSortedUnspent.size > 0) {
         val binRes = unusedSortedUnspent.binarySearchBy(amount - currentTotal) { unspentToSatoshiAmount(it) }
-
-        val utxoIndex = when {
-            binRes < 0 -> {
-                val possibleIndex = -binRes - 1
-                if (possibleIndex == unusedSortedUnspent.size) {
-                    possibleIndex - 1
-                } else {
-                    possibleIndex
-                }
-            }
-            else -> binRes
-        }
+        val utxoIndex = getUtxoIndex(binRes, unusedSortedUnspent.size)
 
         val utxo = unusedSortedUnspent[utxoIndex]
         currentTotal += unspentToSatoshiAmount(utxo)
@@ -142,5 +138,28 @@ internal inline fun <T> getMinimumRequiredUTXOsToSendSatoshi(
         unusedSortedUnspent.remove(utxo)
     }
 
+    // Check if resulting spending UTXOs leads to output UTXO having amount less than dust.
+    // In that case add smallest UTXO so output UTXO was greater than dust.
+    val change = currentTotal - amount
+    if (change != 0L && change < dustSatoshiValue) {
+        val utxo = unusedSortedUnspent.first()
+
+        currentTotal += unspentToSatoshiAmount(utxo)
+        outputsRes.add(utxo)
+        unusedSortedUnspent.remove(utxo)
+    }
+
     return outputsRes
+}
+
+private fun getUtxoIndex(binRes: Int, size: Int) = when {
+    binRes < 0 -> {
+        val possibleIndex = -binRes - 1
+        if (possibleIndex == size) {
+            possibleIndex - 1
+        } else {
+            possibleIndex
+        }
+    }
+    else -> binRes
 }
