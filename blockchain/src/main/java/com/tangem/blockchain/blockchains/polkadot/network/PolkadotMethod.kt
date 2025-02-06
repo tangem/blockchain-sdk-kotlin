22a230973a37c1436b128eed7996e9cf68e2341c
Tangem <git@tangem.com>
2025-02-06 14:30:48 +0500

    Updated on 2026-06-27

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/polkadot/network/PolkadotMethod.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/polkadot/network/PolkadotMethod.kt
@@ -1,5 +1,5 @@
 package com.tangem.blockchain.blockchains.polkadot.network
 
 internal enum class PolkadotMethod(val method: String) {
-    GET_FEE("payment_queryInfo"),
+    GET_FEE("TransactionPaymentApi_query_info"),
 }
