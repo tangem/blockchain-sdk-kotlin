a236c081faf7834cde89f2e731e5ec69aab3c744
Tangem <git@tangem.com>
2025-02-06 14:30:48 +0500

    Updated on 2026-02-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/polkadot/network/PolkadotJsonRpcProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/polkadot/network/PolkadotJsonRpcProvider.kt
@@ -1,19 +1,22 @@
 package com.tangem.blockchain.blockchains.polkadot.network
 
-import com.tangem.blockchain.common.BlockchainSdkError
+import com.tangem.blockchain.blockchains.polkadot.models.PolkadotRuntimeDispatchInfo
+import com.tangem.blockchain.common.*
 import com.tangem.blockchain.common.JsonRPCRequest
 import com.tangem.blockchain.common.JsonRPCResponse
 import com.tangem.blockchain.common.logging.AddHeaderInterceptor
-import com.tangem.blockchain.common.toBlockchainSdkError
 import com.tangem.blockchain.extensions.Result
+import com.tangem.blockchain.extensions.bytes4LittleEndian
 import com.tangem.blockchain.extensions.retryIO
 import com.tangem.blockchain.extensions.successOr
 import com.tangem.blockchain.network.createRetrofitInstance
+import com.tangem.common.extensions.remove
 import com.tangem.common.extensions.toHexString
-import io.emeraldpay.polkaj.api.PolkadotMethod.CHAIN_GET_BLOCK_HASH
-import io.emeraldpay.polkaj.api.PolkadotMethod.CHAIN_GET_HEADER
+import io.emeraldpay.polkaj.api.PolkadotMethod.*
+import io.emeraldpay.polkaj.scale.ScaleCodecReader
 import org.kethereum.extensions.hexToBigInteger
 import org.komputing.khex.model.HexString
+import org.ton.tl.ByteString.Companion.decodeFromHex
 import java.math.BigDecimal
 import java.math.BigInteger
 
@@ -32,11 +35,27 @@ internal class PolkadotJsonRpcProvider(
     @Throws
     suspend fun getFee(transaction: ByteArray, decimals: Int): BigDecimal {
         val response = createRpcBody(
-            method = PolkadotMethod.GET_FEE.method,
-            params = listOf("0x" + transaction.toHexString()),
+            method = STATE_CALL,
+            params = listOf(
+                PolkadotMethod.GET_FEE.method,
+                HEX_PREFIX + transaction.toHexString() + transaction.size.bytes4LittleEndian().toHexString(),
+            ),
         ).post()
 
-        return response.extractResult().getFee(decimals)
+        return when (response) {
+            is Result.Success -> {
+                val result = response.data.result.toString().remove(HEX_PREFIX)
+                val reader = ScaleCodecReader(result.decodeFromHex().toByteArray())
+                val parsedData = PolkadotRuntimeDispatchInfo(
+                    refTime = reader.readCompactInt(),
+                    proofSize = reader.readCompactInt(),
+                    classType = reader.readByte(),
+                    partialFee = reader.readUint128(),
+                )
+                parsedData.partialFee.toBigDecimal().movePointLeft(decimals) ?: BigDecimal.ZERO
+            }
+            is Result.Failure -> throw response.error
+        }
     }
 
     @Throws
@@ -83,14 +102,5 @@ internal class PolkadotJsonRpcProvider(
         is Result.Failure -> {
             throw this.error as? BlockchainSdkError ?: BlockchainSdkError.CustomError("Unknown error format")
         }
-    }
-
-    private fun Map<String, Any>.getFee(decimals: Int): BigDecimal {
-        val feeString = this[FEE] as? String
-        return feeString?.toBigDecimal()?.movePointLeft(decimals) ?: BigDecimal.ZERO
-    }
-
-    companion object {
-        const val FEE = "partialFee"
     }
 }
