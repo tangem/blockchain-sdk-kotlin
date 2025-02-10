181d8bf74c9ab6da197f7131ad005ad3153f8d25
Tangem <git@tangem.com>
2025-02-10 14:05:51 +0500

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/nft/providers/moralis/network/MoralisEvmNFTPricesResponse.kt
+++ blockchain/src/main/java/com/tangem/blockchain/nft/providers/moralis/network/MoralisEvmNFTPricesResponse.kt
@@ -9,35 +9,35 @@ internal data class MoralisEvmNFTPricesResponse(
     @Json(name = "lowest_sale") val lowestSale: MoralisEvmNFTSaleResponse?,
     @Json(name = "highest_sale") val highestSale: MoralisEvmNFTSaleResponse?,
     @Json(name = "average_sale") val averageSale: MoralisEvmNFTAverageSaleResponse?,
-    @Json(name = "total_trades") val totalTrades: Int,
+    @Json(name = "total_trades") val totalTrades: Int?,
 )
 
 @JsonClass(generateAdapter = true)
 internal data class MoralisEvmNFTSaleResponse(
-    @Json(name = "transaction_hash") val transactionHash: String,
-    @Json(name = "block_timestamp") val blockTimestamp: String,
-    @Json(name = "buyer_address") val buyerAddress: String,
-    @Json(name = "seller_address") val sellerAddress: String,
-    @Json(name = "price") val price: String,
-    @Json(name = "price_formatted") val priceFormatted: String,
-    @Json(name = "usd_price_at_sale") val usdPriceAtSale: String,
-    @Json(name = "current_usd_value") val currentUsdValue: String,
-    @Json(name = "token_id") val tokenId: String,
-    @Json(name = "payment_token") val paymentToken: MoralisEvmNFTPaymentTokenResponse,
+    @Json(name = "transaction_hash") val transactionHash: String?,
+    @Json(name = "block_timestamp") val blockTimestamp: String?,
+    @Json(name = "buyer_address") val buyerAddress: String?,
+    @Json(name = "seller_address") val sellerAddress: String?,
+    @Json(name = "price") val price: String?,
+    @Json(name = "price_formatted") val priceFormatted: String?,
+    @Json(name = "usd_price_at_sale") val usdPriceAtSale: String?,
+    @Json(name = "current_usd_value") val currentUsdValue: String?,
+    @Json(name = "token_id") val tokenId: String?,
+    @Json(name = "payment_token") val paymentToken: MoralisEvmNFTPaymentTokenResponse?,
 )
 
 @JsonClass(generateAdapter = true)
 internal data class MoralisEvmNFTPaymentTokenResponse(
-    @Json(name = "token_name") val tokenName: String,
-    @Json(name = "token_symbol") val tokenSymbol: String,
-    @Json(name = "token_logo") val tokenLogo: String,
-    @Json(name = "token_decimals") val tokenDecimals: String,
-    @Json(name = "token_address") val tokenAddress: String,
+    @Json(name = "token_name") val tokenName: String?,
+    @Json(name = "token_symbol") val tokenSymbol: String?,
+    @Json(name = "token_logo") val tokenLogo: String?,
+    @Json(name = "token_decimals") val tokenDecimals: String?,
+    @Json(name = "token_address") val tokenAddress: String?,
 )
 
 @JsonClass(generateAdapter = true)
 internal data class MoralisEvmNFTAverageSaleResponse(
-    @Json(name = "price") val price: String,
-    @Json(name = "price_formatted") val priceFormatted: String,
-    @Json(name = "current_usd_value") val currentUsdValue: String,
+    @Json(name = "price") val price: String?,
+    @Json(name = "price_formatted") val priceFormatted: String?,
+    @Json(name = "current_usd_value") val currentUsdValue: String?,
 )
