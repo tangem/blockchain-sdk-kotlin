266130d083440c6647d2dd6394362849c2d166a7
Tangem <git@tangem.com>
2025-02-13 13:49:53 +0700

    Updated on 2025-09-29

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/alephium/source/TokenId.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/alephium/source/TokenId.kt
@@ -5,7 +5,7 @@ import com.tangem.blockchain.blockchains.alephium.source.serde.Staging
 import kotlinx.io.bytestring.ByteString
 
 @JvmInline
-internal value class TokenId constructor(val value: Blake2b) {
+internal value class TokenId constructor(val value: Blake2b256) {
     fun length(): Int {
         return value.length()
     }
@@ -16,9 +16,9 @@ internal value class TokenId constructor(val value: Blake2b) {
 
     companion object {
         val serde = object : Serde<TokenId> {
-            override fun serialize(input: TokenId): ByteString = Blake2b.serde.serialize(input.value)
+            override fun serialize(input: TokenId): ByteString = Blake2b256.serde.serialize(input.value)
             override fun _deserialize(input: ByteString): Result<Staging<TokenId>> =
-                Blake2b.serde._deserialize(input).map { it.mapValue { value -> TokenId(value) } }
+                Blake2b256.serde._deserialize(input).map { it.mapValue { value -> TokenId(value) } }
         }
     }
 }
