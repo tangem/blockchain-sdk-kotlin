3f70447b3bc00f9c65742d4d8e9b52b53e82de99
Tangem <git@tangem.com>
2025-02-14 14:51:27 +0500

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/nft/NFTProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/nft/NFTProvider.kt
@@ -1,19 +1,13 @@
 package com.tangem.blockchain.nft
 
-import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.nft.models.NFTAsset
 import com.tangem.blockchain.nft.models.NFTAssetSalePrice
 import com.tangem.blockchain.nft.models.NFTCollection
 
 interface NFTProvider {
-    suspend fun getCollections(walletAddress: String, blockchain: Blockchain): List<NFTCollection>
+    suspend fun getCollections(walletAddress: String): List<NFTCollection>
 
-    suspend fun getAssets(walletAddress: String, blockchain: Blockchain, contractAddress: String): List<NFTAsset>
+    suspend fun getAssets(walletAddress: String, contractAddress: String): List<NFTAsset>
 
-    suspend fun getSalePrice(
-        walletAddress: String,
-        blockchain: Blockchain,
-        contractAddress: String,
-        tokenId: String,
-    ): NFTAssetSalePrice
+    suspend fun getSalePrice(walletAddress: String, contractAddress: String, tokenId: String): NFTAssetSalePrice?
 }
