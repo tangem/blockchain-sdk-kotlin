a5abe9d13512115142602d9364346ab01ba91181
Tangem <git@tangem.com>
2025-02-16 04:17:48 +0500

    Updated on 2025-11-28

--- blockchain/src/main/java/com/tangem/blockchain/nft/models/NFTCollection.kt
+++ blockchain/src/main/java/com/tangem/blockchain/nft/models/NFTCollection.kt
@@ -3,11 +3,21 @@ package com.tangem.blockchain.nft.models
 import com.tangem.blockchain.common.Blockchain
 
 data class NFTCollection(
-    val contractName: String,
-    val contractAddress: String,
+    val identifier: Identifier,
     val blockchain: Blockchain,
+    val name: String?,
     val description: String?,
     val logoUrl: String?,
     val count: Int,
     val assets: List<NFTAsset> = emptyList(),
-)
+) {
+    sealed class Identifier {
+        data class EVM(
+            val tokenAddress: String,
+        ) : Identifier()
+
+        data class TON(
+            val contractAddress: String?,
+        ) : Identifier()
+    }
+}
