b12de05402bfbee9b5b9d51bcab1190edd68d6af
Tangem <git@tangem.com>
2025-02-19 19:20:38 +0700

    Updated on 2025-10-09

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/sui/network/SuiConstants.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/sui/network/SuiConstants.kt
@@ -1,7 +1,10 @@
 package com.tangem.blockchain.blockchains.sui.network
 
+import java.math.BigDecimal
+
 internal object SuiConstants {
 
     const val COIN_TYPE = "0x2::sui::SUI"
     const val MIST_SCALE = 9
+    val suiGasBudgetMaxValue = BigDecimal(50_000_000_000)
 }
