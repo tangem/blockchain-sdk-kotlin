abd6bcfbbe4743807ddebc04f3b27fb35655789c
Tangem <git@tangem.com>
2025-02-24 11:44:09 +0500

    Updated on 2026-06-27

--- blockchain/src/main/java/com/tangem/blockchain/nft/models/NFTAsset.kt
+++ blockchain/src/main/java/com/tangem/blockchain/nft/models/NFTAsset.kt
@@ -1,47 +1,62 @@
 package com.tangem.blockchain.nft.models
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import com.tangem.blockchain.common.Blockchain
+import kotlinx.serialization.Serializable
 import java.math.BigDecimal
 
+@JsonClass(generateAdapter = true)
 data class NFTAsset(
-    val identifier: Identifier,
-    val collectionIdentifier: NFTCollection.Identifier,
-    val blockchain: Blockchain,
-    val contractType: String,
-    val owner: String?,
-    val name: String?,
-    val description: String?,
-    val salePrice: SalePrice?,
-    val rarity: Rarity?,
-    val media: Media?,
-    val traits: List<Trait>,
+    @Json(name = "identifier") val identifier: Identifier,
+    @Json(name = "collectionIdentifier") val collectionIdentifier: NFTCollection.Identifier,
+    @Json(name = "blockchain") val blockchain: Blockchain,
+    @Json(name = "contractType") val contractType: String,
+    @Json(name = "owner") val owner: String?,
+    @Json(name = "name") val name: String?,
+    @Json(name = "description") val description: String?,
+    @Json(name = "salePrice") val salePrice: SalePrice?,
+    @Json(name = "rarity") val rarity: Rarity?,
+    @Json(name = "media") val media: Media?,
+    @Json(name = "traits") val traits: List<Trait>,
 ) {
+    @Serializable
     sealed class Identifier {
+        @Serializable
+        @JsonClass(generateAdapter = true)
         data class EVM(
-            val tokenId: String,
-            val tokenAddress: String,
+            @Json(name = "tokenId") val tokenId: String,
+            @Json(name = "tokenAddress") val tokenAddress: String,
         ) : Identifier()
 
-        data class TON(val tokenAddress: String) : Identifier()
+        @Serializable
+        @JsonClass(generateAdapter = true)
+        data class TON(
+            @Json(name = "tokenAddress") val tokenAddress: String,
+        ) : Identifier()
     }
 
+    @JsonClass(generateAdapter = true)
     data class Media(
-        val mimetype: String,
-        val url: String,
+        @Json(name = "mimetype") val mimetype: String,
+        @Json(name = "url") val url: String,
     )
 
+    @JsonClass(generateAdapter = true)
     data class Rarity(
-        val rank: String,
-        val label: String,
+        @Json(name = "rank") val rank: String,
+        @Json(name = "label") val label: String,
     )
 
+    @JsonClass(generateAdapter = true)
     data class Trait(
-        val name: String,
-        val value: String,
+        @Json(name = "name") val name: String,
+        @Json(name = "value") val value: String,
     )
 
+    @JsonClass(generateAdapter = true)
     data class SalePrice(
-        val value: BigDecimal,
-        val symbol: String,
+        @Json(name = "value") val value: BigDecimal,
+        @Json(name = "symbol") val symbol: String,
     )
 }
