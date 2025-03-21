e28cc1cff28b6b4da001ec5be0adab51b23d7950
Tangem <git@tangem.com>
2025-03-21 21:33:51 +0200

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/koinos/KoinosProviderBuilder.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/koinos/KoinosProviderBuilder.kt
@@ -1,6 +1,6 @@
 package com.tangem.blockchain.blockchains.koinos
 
-import com.tangem.blockchain.blockchains.koinos.network.KoinosNetworkProvier
+import com.tangem.blockchain.blockchains.koinos.network.KoinosNetworkProvider
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.BlockchainSdkConfig
 import com.tangem.blockchain.common.network.providers.NetworkProvidersBuilder
@@ -11,9 +11,9 @@ private const val KOINOS_PRO_URL = "https://api.koinos.pro/jsonrpc/"
 internal class KoinosProviderBuilder(
     override val providerTypes: List<ProviderType>,
     private val config: BlockchainSdkConfig,
-) : NetworkProvidersBuilder<KoinosNetworkProvier>() {
+) : NetworkProvidersBuilder<KoinosNetworkProvider>() {
 
-    override fun createProviders(blockchain: Blockchain): List<KoinosNetworkProvier> {
+    override fun createProviders(blockchain: Blockchain): List<KoinosNetworkProvider> {
         return providerTypes
             .mapNotNull {
                 when (it) {
@@ -28,14 +28,14 @@ internal class KoinosProviderBuilder(
             }
     }
 
-    override fun createTestnetProviders(blockchain: Blockchain): List<KoinosNetworkProvier> {
+    override fun createTestnetProviders(blockchain: Blockchain): List<KoinosNetworkProvider> {
         return listOf(
             createProvider(baseUrl = "https://harbinger-api.koinos.io/", isTestnet = true),
             createProvider(baseUrl = "https://testnet.koinosblocks.com/", isTestnet = true),
         )
     }
 
-    private fun createProvider(baseUrl: String, isTestnet: Boolean, apiKey: String? = null): KoinosNetworkProvier {
-        return KoinosNetworkProvier(baseUrl = baseUrl, isTestnet = isTestnet, apiKey = apiKey)
+    private fun createProvider(baseUrl: String, isTestnet: Boolean, apiKey: String? = null): KoinosNetworkProvider {
+        return KoinosNetworkProvider(baseUrl = baseUrl, apiKey = apiKey, isTestnet = isTestnet)
     }
 }
