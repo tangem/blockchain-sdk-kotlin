cffaef103f74b07ebcf3af4fc23cb5a045b37596
Tangem <git@tangem.com>
2025-03-26 17:47:16 +0200

    Updated on 2026-05-04

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/hedera/network/HederaResponse.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/hedera/network/HederaResponse.kt
@@ -80,98 +80,32 @@ internal data class HederaTransactionResponse(
 
 @JsonClass(generateAdapter = true)
 data class HederaTokenDetailsResponse(
-    // / there are more fields available
-    // / on the /tokens/{token_id} response, ignore for now
+    // there are more fields available here and in the inner objects
+    // ignore them for now
     @Json(name = "custom_fees")
     val customFees: CustomFees?,
-)
-
-@JsonClass(generateAdapter = true)
-data class CustomFees(
-    @Json(name = "created_timestamp")
-    val createdTimestamp: String?,
-
-    @Json(name = "fixed_fees")
-    val fixedFees: List<CustomFixedFee>,
-
-    @Json(name = "fractional_fees")
-    val fractionalFees: List<CustomFractionalFee>,
-
-    @Json(name = "royalty_fees")
-    val royaltyFees: List<CustomRoyaltyFee>?,
 ) {
     @JsonClass(generateAdapter = true)
-    data class CustomFixedFee(
-        @Json(name = "collector_account_id")
-        val collectorAccountId: String?,
+    data class CustomFees(
+        @Json(name = "fixed_fees")
+        val fixedFees: List<CustomFixedFee>,
 
-        @Json(name = "all_collectors_are_exempt")
-        val allCollectorsAreExempt: Boolean?,
+        @Json(name = "fractional_fees")
+        val fractionalFees: List<CustomFractionalFee>,
+    ) {
+        @JsonClass(generateAdapter = true)
+        data class CustomFixedFee(
+            @Json(name = "amount")
+            val amount: BigDecimal?,
 
-        @Json(name = "amount")
-        val amount: BigDecimal?,
+            @Json(name = "denominating_token_id")
+            val denominatingTokenId: String?,
+        )
 
-        @Json(name = "denominating_token_id")
-        val denominatingTokenId: String?,
-    )
-
-    @JsonClass(generateAdapter = true)
-    data class CustomFractionalFee(
-        @Json(name = "collector_account_id")
-        val collectorAccountId: String?,
-
-        @Json(name = "all_collectors_are_exempt")
-        val allCollectorsAreExempt: Boolean?,
-
-        @Json(name = "amount")
-        val amount: Amount?,
-
-        @Json(name = "maximum")
-        val maximum: BigDecimal?,
-
-        @Json(name = "minimum")
-        val minimum: BigDecimal?,
-
-        @Json(name = "denominating_token_id")
-        val denominatingTokenId: String?,
-
-        @Json(name = "net_of_transfers")
-        val netOfTransfers: Boolean?,
-    )
-
-    @JsonClass(generateAdapter = true)
-    data class CustomRoyaltyFee(
-        @Json(name = "collector_account_id")
-        val collectorAccountId: String?,
-
-        @Json(name = "all_collectors_are_exempt")
-        val allCollectorsAreExempt: Boolean?,
-
-        @Json(name = "amount")
-        val amount: Amount?,
-
-        @Json(name = "denominating_token_id")
-        val denominatingTokenId: String?,
-
-        @Json(name = "fallback_fee")
-        val fallbackFee: FixedFee?,
-    )
-
-    @JsonClass(generateAdapter = true)
-    data class Amount(
-        @Json(name = "numerator")
-        val numerator: Int?,
-
-        @Json(name = "denominator")
-        val denominator: Int?,
-    )
-
-    @JsonClass(generateAdapter = true)
-    data class FixedFee(
-        @Json(name = "amount")
-        val amount: BigDecimal?,
-
-        @Json(name = "denominating_token_id")
-        val denominatingTokenId: String?,
-    )
+        @JsonClass(generateAdapter = true)
+        data class CustomFractionalFee(
+            @Json(name = "denominating_token_id")
+            val denominatingTokenId: String?,
+        )
+    }
 }
