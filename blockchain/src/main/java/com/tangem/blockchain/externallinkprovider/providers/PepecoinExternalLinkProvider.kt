7770bb4750c40805dd2b4ae344d0af1e9671f4ff
Tangem <git@tangem.com>
2025-03-27 21:39:21 +0500

    Updated on 2026-02-02

---
+++ blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/PepecoinExternalLinkProvider.kt
@@ -0,0 +1,22 @@
+package com.tangem.blockchain.externallinkprovider.providers
+
+import com.tangem.blockchain.externallinkprovider.ExternalLinkProvider
+import com.tangem.blockchain.externallinkprovider.TxExploreState
+
+internal class PepecoinExternalLinkProvider(private val isTestnet: Boolean) : ExternalLinkProvider {
+    override val explorerBaseUrl: String = if (isTestnet) {
+        "https://testnet.pepeblocks.com/"
+    } else {
+        "https://pepeblocks.com/"
+    }
+
+    override val testNetTopUpUrl: String = "https://pepeblocks.com/faucet"
+
+    override fun explorerUrl(walletAddress: String, contractAddress: String?): String {
+        return "${explorerBaseUrl}address/$walletAddress"
+    }
+
+    override fun getExplorerTxUrl(transactionHash: String): TxExploreState {
+        return TxExploreState.Url("${explorerBaseUrl}tx/$transactionHash")
+    }
+}
