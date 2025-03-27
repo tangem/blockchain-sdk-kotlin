42665029aec76dc1399ae2d1b303e1b853693036
Tangem <git@tangem.com>
2025-03-27 21:39:21 +0500

    Updated on 2026-02-11

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/pepecoin/PepecoinMainNetParams.kt
@@ -0,0 +1,14 @@
+package com.tangem.blockchain.blockchains.pepecoin
+
+import org.bitcoinj.params.MainNetParams
+
+class PepecoinMainNetParams : MainNetParams() {
+    init {
+        segwitAddressHrp = "P"
+        addressHeader = 0x38
+        p2shHeader = 0x16
+        dumpedPrivateKeyHeader = 0x9E
+        bip32HeaderP2PKHpub = 0x02FACAFD
+        bip32HeaderP2PKHpriv = 0x02FAC398
+    }
+}
