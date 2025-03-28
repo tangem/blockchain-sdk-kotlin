7f50de3a8a37d4000d35bb0205fe19ca636ee81b
Tangem <git@tangem.com>
2025-03-28 18:04:23 +0500

    Updated on 2026-02-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/pepecoin/PepecoinFeeCalculator.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/pepecoin/PepecoinFeeCalculator.kt
@@ -8,6 +8,7 @@ internal open class PepecoinFeeCalculator(
     private val blockchain: Blockchain,
 ) : BitcoinFeesCalculator(blockchain) {
 
+    // https://github.com/pepecoinppc/pepecoin/blob/4fb5a0cd930c0df82c88292e973a7b7cfa06c4e8/doc/fee-recommendation.md
     override val minimalFeePerKb: BigDecimal = 0.01.toBigDecimal()
     override val minimalFee: BigDecimal = 0.01.toBigDecimal()
 }
