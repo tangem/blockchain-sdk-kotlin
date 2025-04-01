199de5185500ff45ea1926b9a71ab9bfa4a69273
Tangem <git@tangem.com>
2025-04-01 20:02:28 +0500

    releases/5.27.0-1144

--- blockchain/src/main/java/com/tangem/blockchain/common/smartcontract/CompiledSmartContractCallData.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/smartcontract/CompiledSmartContractCallData.kt
@@ -1,5 +1,7 @@
 package com.tangem.blockchain.common.smartcontract
 
+import org.komputing.khex.extensions.toHexString
+
 /**
  * Model for already compiled smart contract call data.
  * If some service or provider sent ready to send transaction data
@@ -7,5 +9,5 @@ package com.tangem.blockchain.common.smartcontract
 data class CompiledSmartContractCallData(
     override val data: ByteArray,
 ) : SmartContractCallData {
-    override val methodId: String = ""
+    override val methodId: String = data.take(n = 4).toHexString()
 }
