959c97d383ceded20b3be0958387f584bd913079
Tangem <git@tangem.com>
2025-04-21 20:39:23 +0500

    Updated on 2026-02-02

--- blockchain/src/main/java/com/tangem/blockchain/nft/NFTProviderFactory.kt
+++ blockchain/src/main/java/com/tangem/blockchain/nft/NFTProviderFactory.kt
@@ -3,12 +3,17 @@ package com.tangem.blockchain.nft
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.BlockchainSdkConfig
 import com.tangem.blockchain.nft.providers.moralis.MoralisEvmNFTProvider
+import com.tangem.blockchain.nft.providers.moralis.MoralisSolanaNFTProvider
 
 internal object NFTProviderFactory {
     fun createNFTProvider(blockchain: Blockchain, config: BlockchainSdkConfig): NFTProvider = when {
         blockchain.canHandleNFTs() -> {
             when {
                 blockchain.isEvm() -> MoralisEvmNFTProvider(
+                    blockchain = blockchain,
+                    apiKey = config.moralisApiKey,
+                )
+                blockchain == Blockchain.Solana -> MoralisSolanaNFTProvider(
                     blockchain = blockchain,
                     apiKey = config.moralisApiKey,
                 )
