5c8815f085f96899b9c76c815bb5bac7375b85c0
Tangem <git@tangem.com>
2025-04-25 14:05:02 +0300

    Updated on 2026-05-04

--- blockchain/src/test/java/com/tangem/blockchain/blockchains/sui/SuiAddressServiceReformatTest.kt
+++ blockchain/src/test/java/com/tangem/blockchain/blockchains/sui/SuiAddressServiceReformatTest.kt
@@ -5,7 +5,7 @@ import com.tangem.blockchain.blockchains.sui.network.SuiConstants
 import com.tangem.blockchain.common.Blockchain
 import org.junit.Test
 
-class SuiAddressServiceTest {
+class SuiAddressServiceReformatTest {
 
     private val addressService = SuiAddressService(Blockchain.Sui)
 
@@ -23,7 +23,6 @@ class SuiAddressServiceTest {
     fun trimmedContractAddressTest() {
         val fullAddress = "0x6864a6f921804860930db6ddbe2e16acdf8504495ea7481637a1c8b9a8fe54b::cetus::CETUS"
         val actualAddress = addressService.reformatContractAddress(fullAddress)
-
         Truth.assertThat(actualAddress).isEqualTo(expectedTokenAddress)
     }
 
@@ -32,5 +31,12 @@ class SuiAddressServiceTest {
         val coinAddress = SuiConstants.COIN_TYPE
         val actualAddress = addressService.reformatContractAddress(coinAddress)
         Truth.assertThat(actualAddress).isEqualTo(coinAddress)
+    }
+
+    @Test
+    fun nullContractAddressTest() {
+        val coinAddress = null
+        val actualAddress = addressService.reformatContractAddress(coinAddress)
+        Truth.assertThat(actualAddress).isEqualTo(null)
     }
 }
