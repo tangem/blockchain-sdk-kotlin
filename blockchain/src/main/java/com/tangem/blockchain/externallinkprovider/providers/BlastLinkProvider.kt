987ca37c5afb975f919498d49250a2781608ee08
Tangem <git@tangem.com>
2025-05-15 23:48:23 +0500

    Updated on 2025-11-03

--- blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/BlastLinkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/BlastLinkProvider.kt
@@ -2,6 +2,7 @@ package com.tangem.blockchain.externallinkprovider.providers
 
 import com.tangem.blockchain.externallinkprovider.ExternalLinkProvider
 import com.tangem.blockchain.externallinkprovider.TxExploreState
+import com.tangem.blockchain.nft.models.NFTAsset
 
 internal class BlastLinkProvider(isTestnet: Boolean) : ExternalLinkProvider {
 
@@ -16,5 +17,10 @@ internal class BlastLinkProvider(isTestnet: Boolean) : ExternalLinkProvider {
 
     override fun getExplorerTxUrl(transactionHash: String): TxExploreState {
         return TxExploreState.Url(url = "${explorerBaseUrl}tx/$transactionHash")
+    }
+
+    override fun getNFTExplorerUrl(assetIdentifier: NFTAsset.Identifier): String {
+        require(assetIdentifier is NFTAsset.Identifier.EVM)
+        return explorerBaseUrl + "nft/${assetIdentifier.tokenAddress}/${assetIdentifier.tokenId}"
     }
 }
--- blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/FantomExternalLinkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/FantomExternalLinkProvider.kt
@@ -2,6 +2,7 @@ package com.tangem.blockchain.externallinkprovider.providers
 
 import com.tangem.blockchain.externallinkprovider.ExternalLinkProvider
 import com.tangem.blockchain.externallinkprovider.TxExploreState
+import com.tangem.blockchain.nft.models.NFTAsset
 
 internal class FantomExternalLinkProvider(isTestnet: Boolean) : ExternalLinkProvider {
 
@@ -23,5 +24,10 @@ internal class FantomExternalLinkProvider(isTestnet: Boolean) : ExternalLinkProv
 
     override fun getExplorerTxUrl(transactionHash: String): TxExploreState {
         return TxExploreState.Url(explorerBaseUrl + "tx/$transactionHash")
+    }
+
+    override fun getNFTExplorerUrl(assetIdentifier: NFTAsset.Identifier): String {
+        require(assetIdentifier is NFTAsset.Identifier.EVM)
+        return explorerBaseUrl + "assets/${assetIdentifier.tokenAddress}/${assetIdentifier.tokenId}"
     }
 }
