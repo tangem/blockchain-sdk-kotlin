ea25dba230a2f72ef107d42146b602f3d436f7dc
Tangem <git@tangem.com>
2025-05-15 23:48:23 +0500

    Updated on 2026-05-04

--- blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/ZkSyncEraExternalLinkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/ZkSyncEraExternalLinkProvider.kt
@@ -2,6 +2,7 @@ package com.tangem.blockchain.externallinkprovider.providers
 
 import com.tangem.blockchain.externallinkprovider.ExternalLinkProvider
 import com.tangem.blockchain.externallinkprovider.TxExploreState
+import com.tangem.blockchain.nft.models.NFTAsset
 
 internal class ZkSyncEraExternalLinkProvider(isTestnet: Boolean) : ExternalLinkProvider {
 
@@ -19,5 +20,11 @@ internal class ZkSyncEraExternalLinkProvider(isTestnet: Boolean) : ExternalLinkP
 
     override fun getExplorerTxUrl(transactionHash: String): TxExploreState {
         return TxExploreState.Url(url = "${explorerBaseUrl}tx/$transactionHash")
+    }
+
+    override fun getNFTExplorerUrl(assetIdentifier: NFTAsset.Identifier): String {
+        require(assetIdentifier is NFTAsset.Identifier.EVM)
+        // https://explorer.zksync.io/ doesn't provide a way to see specific nft asset
+        return "https://zksync.blockscout.com/token/${assetIdentifier.tokenAddress}/instance/${assetIdentifier.tokenId}"
     }
 }
