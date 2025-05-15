a4bca3232d2696de3e01d4637e9e4ca508246631
Tangem <git@tangem.com>
2025-05-15 23:48:23 +0500

    Updated on 2026-02-11

--- blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/MantleExternalLinkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/MantleExternalLinkProvider.kt
@@ -2,6 +2,7 @@ package com.tangem.blockchain.externallinkprovider.providers
 
 import com.tangem.blockchain.externallinkprovider.ExternalLinkProvider
 import com.tangem.blockchain.externallinkprovider.TxExploreState
+import com.tangem.blockchain.nft.models.NFTAsset
 
 internal class MantleExternalLinkProvider(isTestnet: Boolean) : ExternalLinkProvider {
 
@@ -19,5 +20,10 @@ internal class MantleExternalLinkProvider(isTestnet: Boolean) : ExternalLinkProv
 
     override fun getExplorerTxUrl(transactionHash: String): TxExploreState {
         return TxExploreState.Url(url = "${explorerBaseUrl}tx/$transactionHash")
+    }
+
+    override fun getNFTExplorerUrl(assetIdentifier: NFTAsset.Identifier): String {
+        require(assetIdentifier is NFTAsset.Identifier.EVM)
+        return explorerBaseUrl + "token/${assetIdentifier.tokenAddress}/instance/${assetIdentifier.tokenId}"
     }
 }
