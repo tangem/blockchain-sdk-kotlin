19cb604b54d5171dd960caf3027f5cb9466dc000
Tangem <git@tangem.com>
2025-05-21 09:49:00 +0500

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/BlastLinkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/BlastLinkProvider.kt
@@ -2,7 +2,6 @@ package com.tangem.blockchain.externallinkprovider.providers
 
 import com.tangem.blockchain.externallinkprovider.ExternalLinkProvider
 import com.tangem.blockchain.externallinkprovider.TxExploreState
-import com.tangem.blockchain.nft.models.NFTAsset
 
 internal class BlastLinkProvider(isTestnet: Boolean) : ExternalLinkProvider {
 
@@ -17,10 +16,5 @@ internal class BlastLinkProvider(isTestnet: Boolean) : ExternalLinkProvider {
 
     override fun getExplorerTxUrl(transactionHash: String): TxExploreState {
         return TxExploreState.Url(url = "${explorerBaseUrl}tx/$transactionHash")
-    }
-
-    override fun getNFTExplorerUrl(assetIdentifier: NFTAsset.Identifier): String {
-        require(assetIdentifier is NFTAsset.Identifier.EVM)
-        return explorerBaseUrl + "nft/${assetIdentifier.tokenAddress}/${assetIdentifier.tokenId}"
     }
 }
