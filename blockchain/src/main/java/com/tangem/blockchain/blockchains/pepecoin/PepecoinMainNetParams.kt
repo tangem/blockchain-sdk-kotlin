dbbaf5f6faaa9ee5bfdcdc19cd4f74925e53f2e3
Tangem <git@tangem.com>
2025-05-29 11:39:08 +0400

    Updated on 2025-11-28

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/pepecoin/PepecoinMainNetParams.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/pepecoin/PepecoinMainNetParams.kt
@@ -1,9 +1,11 @@
 package com.tangem.blockchain.blockchains.pepecoin
 
+import org.bitcoinj.core.Coin
 import org.bitcoinj.params.MainNetParams
 
 @Suppress("MagicNumber")
-class PepecoinMainNetParams : MainNetParams() {
+internal class PepecoinMainNetParams : MainNetParams() {
+
     init {
         segwitAddressHrp = "P"
         addressHeader = 0x38
@@ -12,4 +14,8 @@ class PepecoinMainNetParams : MainNetParams() {
         bip32HeaderP2PKHpub = 0x02FACAFD
         bip32HeaderP2PKHpriv = 0x02FAC398
     }
+
+    override fun hasMaxMoney(): Boolean = true
+
+    override fun getMaxMoney(): Coin = Coin.valueOf(Long.MAX_VALUE)
 }
