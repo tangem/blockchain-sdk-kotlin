4fc97dcb418f7cd35170cd5a3e77f68a664317b6
Tangem <git@tangem.com>
2025-06-19 22:08:25 +0800

    Updated on 2026-02-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/stellar/StellarAddressService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/stellar/StellarAddressService.kt
@@ -1,10 +1,11 @@
 package com.tangem.blockchain.blockchains.stellar
 
 import com.tangem.blockchain.common.address.AddressService
+import com.tangem.blockchain.common.address.ContractAddressValidator
 import com.tangem.common.card.EllipticCurve
 import org.stellar.sdk.KeyPair
 
-class StellarAddressService : AddressService() {
+class StellarAddressService : AddressService(), ContractAddressValidator {
     override fun makeAddress(walletPublicKey: ByteArray, curve: EllipticCurve?): String {
         val kp = KeyPair.fromPublicKey(walletPublicKey)
         return kp.accountId
@@ -16,5 +17,17 @@ class StellarAddressService : AddressService() {
         } catch (exception: Exception) {
             false
         }
+    }
+
+    override fun validateContractAddress(address: String): Boolean {
+        val split = address.split("-")
+        val currencyCode = split.getOrNull(0) ?: return false
+        val issuer = split.getOrNull(1) ?: return false
+        return isValidCurrencyCode(currencyCode) && validate(issuer)
+    }
+
+    private fun isValidCurrencyCode(code: String): Boolean {
+        val regex = Regex("^[A-Z0-9]{1,12}$")
+        return regex.matches(code)
     }
 }
