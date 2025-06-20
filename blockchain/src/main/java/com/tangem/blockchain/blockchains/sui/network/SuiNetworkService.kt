cc03d27cb606a3349566e22029572df49d968519
Tangem <git@tangem.com>
2025-06-20 12:54:41 +0500

    develop-1150

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/sui/network/SuiNetworkService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/sui/network/SuiNetworkService.kt
@@ -10,6 +10,9 @@ import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.extensions.successOr
 import com.tangem.blockchain.network.MultiNetworkProvider
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import java.math.BigDecimal
 
 internal class SuiNetworkService(
@@ -21,14 +24,26 @@ internal class SuiNetworkService(
     val host: String get() = multiJsonRpcProvider.currentProvider.baseUrl
 
     suspend fun getInfo(address: String): Result<SuiWalletInfo> {
-        val response = multiJsonRpcProvider
-            .performRequest(SuiJsonRpcProvider::getCoins, address)
-            .successOr { return it }
+        val response = coroutineScope {
+            val firstRequest =
+                async { multiJsonRpcProvider.performRequest { getCoins(address = address, cursor = null) } }
+            val secondRequest =
+                async {
+                    multiJsonRpcProvider.performRequest {
+                        getCoins(
+                            address = address,
+                            cursor = "eyJjb2luX3R5cGUiOiIweGYyMmRhOWEyNGFkMDI3Y2NjYjVmMmQ0OTZjYmU5MWRlOTUzZDM2MzUxM2RiMDhhM2E3MzRkMzYxYzdjMTc1MDM6OkxPRkk6OkxPRkkiLCJpbnZlcnRlZF9iYWxhbmNlIjoxODQ0Njc0NDA3MzcwOTU1MTYxNSwib2JqZWN0X2lkIjoiMHhlMTA2ODU5OWIyN2YxMGM1NmM4NjEyMTkzNjJjOGM1MWZhZDgwNDg2YjA3MjUwZmQ5NmM1OGI5MzZjZDhmZGNjIn0="
+                        )
+                    }
+                }
+            listOf(firstRequest, secondRequest).awaitAll().map { it.successOr { throw it.error } }
+        }
+        val dataList = response.flatMap { it.data }
 
         var totalSuiBalance = BigDecimal.ZERO
         val coins = mutableListOf<SuiCoin>()
 
-        for (coin in response.data) {
+        for (coin in dataList) {
             if (coin.coinType == COIN_TYPE) totalSuiBalance += coin.balance.movePointLeft(Blockchain.Sui.decimals())
 
             val suiCoin = SuiCoin(
