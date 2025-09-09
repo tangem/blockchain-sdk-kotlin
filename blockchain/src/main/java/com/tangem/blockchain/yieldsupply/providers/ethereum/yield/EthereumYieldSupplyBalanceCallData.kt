62f7005b0df7a1727d7f44daa52643cfbb3d1b0b
Tangem <git@tangem.com>
2025-09-09 18:14:43 +0500

    Updated on 2025-10-09

---
+++ blockchain/src/main/java/com/tangem/blockchain/yieldsupply/providers/ethereum/yield/EthereumYieldSupplyBalanceCallData.kt
@@ -0,0 +1,22 @@
+package com.tangem.blockchain.yieldsupply.providers.ethereum.yield
+
+import com.tangem.blockchain.common.smartcontract.SmartContractCallData
+import com.tangem.blockchain.extensions.hexToFixedSizeBytes
+import com.tangem.common.extensions.hexToBytes
+
+/**
+ *  Call data for the method that retrieves the effective balance of a specific yield token
+ *
+ *  Signature: effectiveBalanceOf(address)
+ */
+internal class EthereumYieldSupplyBalanceCallData(
+    private val tokenContractAddress: String,
+) : SmartContractCallData {
+    override val methodId: String = "0x16a398f7"
+    override val data: ByteArray
+        get() {
+            val prefixData = methodId.hexToBytes()
+            val addressData = tokenContractAddress.hexToFixedSizeBytes()
+            return prefixData + addressData
+        }
+}
