fb5f071b0fe8a3d2546c6c63b48aae6d59f7cffb
Tangem <git@tangem.com>
2025-09-09 18:14:43 +0500

    Updated on 2026-07-23

---
+++ blockchain/src/main/java/com/tangem/blockchain/yieldsupply/providers/ethereum/factory/EthereumYieldSupplyContractAddressCallData.kt
@@ -0,0 +1,24 @@
+package com.tangem.blockchain.yieldsupply.providers.ethereum.factory
+
+import com.tangem.blockchain.common.smartcontract.SmartContractCallData
+import com.tangem.blockchain.extensions.hexToFixedSizeBytes
+import com.tangem.common.extensions.hexToBytes
+
+/**
+ *  Call data for the method that calculates the address of the Yield Module contract
+ *  associated with the specified owner address.
+ *
+ *  Signature: `calculateYieldModuleAddress(address)`
+ */
+internal class EthereumYieldSupplyContractAddressCallData(
+    private val address: String,
+) : SmartContractCallData {
+    override val methodId: String = "0xebd6d0a4"
+    override val data: ByteArray
+        get() {
+            val prefixData = methodId.hexToBytes()
+            val addressData = address.hexToFixedSizeBytes()
+
+            return prefixData + addressData
+        }
+}
