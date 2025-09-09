13d1e197b7bb338885ec5622025cf40c403225c3
Tangem <git@tangem.com>
2025-09-09 18:14:43 +0500

    Updated on 2026-02-11

---
+++ blockchain/src/main/java/com/tangem/blockchain/yieldsupply/providers/ethereum/yield/EthereumYieldSupplyReactivateTokenCallData.kt
@@ -0,0 +1,29 @@
+package com.tangem.blockchain.yieldsupply.providers.ethereum.yield
+
+import com.tangem.blockchain.common.Amount
+import com.tangem.blockchain.common.smartcontract.SmartContractCallData
+import com.tangem.blockchain.extensions.bigIntegerValue
+import com.tangem.blockchain.extensions.hexToFixedSizeBytes
+import com.tangem.common.extensions.hexToBytes
+import org.kethereum.extensions.toBytesPadded
+
+/**
+ *  Call data for the method that reactivates a previously deactivated yield token,
+ *  allowing users to resume their participation in the associated yield protocol.
+ *
+ *  Signature: `reactivateToken(address,uint240)`
+ */
+internal class EthereumYieldSupplyReactivateTokenCallData(
+    private val tokenContractAddress: String,
+    private val maxNetworkFee: Amount,
+) : SmartContractCallData {
+    override val methodId: String = "0xc478e956"
+    override val data: ByteArray
+        get() {
+            val prefixData = methodId.hexToBytes()
+            val tokenContractAddressData = tokenContractAddress.hexToFixedSizeBytes()
+            val maxFeeData = maxNetworkFee.bigIntegerValue()?.toBytesPadded(length = 32) ?: error("Invalid fee amount")
+
+            return prefixData + tokenContractAddressData + maxFeeData
+        }
+}
