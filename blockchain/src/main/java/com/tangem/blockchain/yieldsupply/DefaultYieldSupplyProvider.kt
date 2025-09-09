30c56b1c3f2c3e200772066107b94e0efa48d01a
Tangem <git@tangem.com>
2025-09-09 19:29:46 +0500

    Updated on 2025-11-28

---
+++ blockchain/src/main/java/com/tangem/blockchain/yieldsupply/DefaultYieldSupplyProvider.kt
@@ -0,0 +1,22 @@
+package com.tangem.blockchain.yieldsupply
+
+import com.tangem.blockchain.common.Token
+import com.tangem.blockchain.yieldsupply.providers.YieldSupplyStatus
+import java.math.BigDecimal
+
+internal object DefaultYieldSupplyProvider : YieldSupplyProvider {
+    override val factoryContractAddress: String = ""
+    override val processorContractAddress: String = ""
+
+    override suspend fun getYieldContract(): String = ""
+
+    override suspend fun calculateYieldContract(): String = ""
+
+    override suspend fun getServiceFee(): BigDecimal = BigDecimal.ZERO
+
+    override suspend fun getYieldSupplyStatus(tokenContractAddress: String): YieldSupplyStatus? = null
+
+    override suspend fun getProtocolBalance(token: Token): BigDecimal = BigDecimal.ZERO
+
+    override suspend fun isAllowedToSpend(tokenContractAddress: String): Boolean = false
+}
