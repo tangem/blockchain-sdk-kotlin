78318a99447093fd7e57e9271f3106156edb59ae
Tangem <git@tangem.com>
2025-09-11 20:50:21 +0500

    Updated on 2025-11-03

--- blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/MantleWalletManagerAssembly.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/assembly/impl/MantleWalletManagerAssembly.kt
@@ -6,17 +6,22 @@ import com.tangem.blockchain.blockchains.ethereum.txbuilder.EthereumTransactionB
 import com.tangem.blockchain.blockchains.mantle.MantleWalletManager
 import com.tangem.blockchain.common.assembly.WalletManagerAssembly
 import com.tangem.blockchain.common.assembly.WalletManagerAssemblyInput
+import com.tangem.blockchain.network.MultiNetworkProvider
 import com.tangem.blockchain.transactionhistory.TransactionHistoryProviderFactory
 
 internal object MantleWalletManagerAssembly : WalletManagerAssembly<MantleWalletManager>() {
 
     override fun make(input: WalletManagerAssemblyInput): MantleWalletManager {
         return with(input.wallet) {
+            val multiNetworkProvider = MultiNetworkProvider(
+                MantleProvidersBuilder(input.providerTypes).build(blockchain),
+            )
+
             MantleWalletManager(
                 wallet = this,
                 transactionBuilder = EthereumTransactionBuilder.create(wallet = input.wallet),
                 networkProvider = EthereumNetworkService(
-                    jsonRpcProviders = MantleProvidersBuilder(input.providerTypes).build(blockchain),
+                    multiJsonRpcProvider = multiNetworkProvider,
                 ),
                 transactionHistoryProvider = TransactionHistoryProviderFactory.makeProvider(blockchain, input.config),
             )
