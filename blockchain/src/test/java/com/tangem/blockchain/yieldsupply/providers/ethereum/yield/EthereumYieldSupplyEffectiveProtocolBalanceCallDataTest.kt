ae94fde94610100640dfd823d72faf033311bdb6
Tangem <git@tangem.com>
2025-09-23 15:00:55 +0500

    Updated on 2026-03-11

--- blockchain/src/test/java/com/tangem/blockchain/yieldsupply/providers/ethereum/yield/EthereumYieldSupplyEffectiveProtocolBalanceCallDataTest.kt
+++ blockchain/src/test/java/com/tangem/blockchain/yieldsupply/providers/ethereum/yield/EthereumYieldSupplyEffectiveProtocolBalanceCallDataTest.kt
@@ -5,18 +5,18 @@ import com.tangem.common.extensions.hexToBytes
 import org.junit.Test
 
 /**
- * Test for [EthereumYieldSupplyProtocolBalanceCallData]
+ * Test for [EthereumYieldSupplyEffectiveProtocolBalanceCallData]
  */
-internal class EthereumYieldSupplyProtocolBalanceCallDataTest {
+internal class EthereumYieldSupplyEffectiveProtocolBalanceCallDataTest {
 
-    private val signature = "0x4bd22a1b".hexToBytes()
+    private val signature = "0x5002bb7e".hexToBytes()
     private val tokenContractAddress = "0x1234567890abcdef1234567890abcdef12345678"
 
     private val tokenAddressData = "0000000000000000000000001234567890abcdef1234567890abcdef12345678".hexToBytes()
 
     @Test
     fun `Is call data is correct`() {
-        val callData = EthereumYieldSupplyProtocolBalanceCallData(
+        val callData = EthereumYieldSupplyEffectiveProtocolBalanceCallData(
             tokenContractAddress = tokenContractAddress,
         )
         val expected = signature + tokenAddressData
