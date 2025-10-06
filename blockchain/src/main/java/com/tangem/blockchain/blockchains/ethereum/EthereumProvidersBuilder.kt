dbaba9b83329eb8a21ab20af7064c89fb402fcab
Tangem <git@tangem.com>
2025-10-06 18:07:47 +0500

    Updated on 2026-06-27

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/EthereumProvidersBuilder.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/EthereumProvidersBuilder.kt
@@ -20,6 +20,7 @@ internal class EthereumProvidersBuilder(
                     baseUrl = "https://eth.nownodes.io/",
                 )
                 ProviderType.GetBlock -> ethereumProviderFactory.getGetBlockProvider { eth?.jsonRpc }
+                ProviderType.Blink -> ethereumProviderFactory.getBlinkProvider("https://eth.blinklabs.xyz/v1/")
                 ProviderType.EthereumLike.Infura -> {
                     ethereumProviderFactory.getInfuraProvider(baseUrl = "https://mainnet.infura.io/v3/")
                 }
