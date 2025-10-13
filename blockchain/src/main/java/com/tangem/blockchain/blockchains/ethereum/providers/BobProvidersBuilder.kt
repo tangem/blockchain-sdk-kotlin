203c5878bcbc59bc2e4a69c421e457eb850a48ab
Tangem <git@tangem.com>
2025-10-13 21:27:06 +0300

    Updated on 2025-12-22

---
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/providers/BobProvidersBuilder.kt
@@ -0,0 +1,12 @@
+package com.tangem.blockchain.blockchains.ethereum.providers
+
+import com.tangem.blockchain.blockchains.ethereum.network.EthereumJsonRpcProvider
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchain.common.network.providers.OnlyPublicProvidersBuilder
+import com.tangem.blockchain.common.network.providers.ProviderType
+
+internal class BobProvidersBuilder(
+    override val providerTypes: List<ProviderType>,
+) : OnlyPublicProvidersBuilder<EthereumJsonRpcProvider>(providerTypes = providerTypes) {
+    override fun createProvider(url: String, blockchain: Blockchain) = EthereumJsonRpcProvider(url)
+}
