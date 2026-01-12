27076abbb9423e827106ebd09b858eae1bc3305e
Tangem <git@tangem.com>
2026-01-12 12:13:08 +0300

    Updated on 2026-02-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/models/EIP7702AuthorizationData.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/ethereum/models/EIP7702AuthorizationData.kt
@@ -6,6 +6,7 @@ data class EIP7702AuthorizationData(
     val chainId: Int,
     val nonce: BigInteger,
     val data: ByteArray,
+    val executorAddress: String,
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -16,6 +17,7 @@ data class EIP7702AuthorizationData(
         if (chainId != other.chainId) return false
         if (nonce != other.nonce) return false
         if (!data.contentEquals(other.data)) return false
+        if (executorAddress != other.executorAddress) return false
 
         return true
     }
@@ -24,6 +26,7 @@ data class EIP7702AuthorizationData(
         var result = chainId
         result = 31 * result + nonce.hashCode()
         result = 31 * result + data.contentHashCode()
+        result = 31 * result + executorAddress.hashCode()
         return result
     }
 }
