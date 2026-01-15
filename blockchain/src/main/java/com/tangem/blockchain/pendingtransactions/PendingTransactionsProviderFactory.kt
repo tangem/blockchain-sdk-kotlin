ef628ab58e45411ae54ead1e258c95a186c1fa0f
Tangem <git@tangem.com>
2026-01-15 13:42:44 +0400

    Updated on 2026-03-11

---
+++ blockchain/src/main/java/com/tangem/blockchain/pendingtransactions/PendingTransactionsProviderFactory.kt
@@ -0,0 +1,45 @@
+package com.tangem.blockchain.pendingtransactions
+
+import com.tangem.blockchain.blockchains.ethereum.network.EthereumJsonRpcProvider
+import com.tangem.blockchain.common.NetworkProvider
+import com.tangem.blockchain.common.Wallet
+import com.tangem.blockchain.common.datastorage.implementations.AdvancedDataStorage
+import com.tangem.blockchain.common.di.DepsContainer
+import com.tangem.blockchain.common.network.providers.ProviderType
+import com.tangem.blockchain.network.MultiNetworkProvider
+import com.tangem.blockchain.pendingtransactions.providers.EthereumPendingTransactionsProvider
+import kotlinx.coroutines.CoroutineScope
+
+/**
+ * Factory for creating instances of [PendingTransactionsProvider] based on the wallet's blockchain.
+ */
+internal class PendingTransactionsProviderFactory(
+    private val dataStorage: AdvancedDataStorage,
+    private val coroutineScope: CoroutineScope,
+) {
+
+    @Suppress("UNCHECKED_CAST")
+    fun makeProvider(
+        wallet: Wallet,
+        networkProvider: MultiNetworkProvider<out NetworkProvider>,
+        networkProviderMap: Map<ProviderType, NetworkProvider>,
+    ): PendingTransactionsProvider {
+        if (!DepsContainer.blockchainFeatureToggles.isPendingTransactionsEnabled) {
+            return DefaultPendingTransactionsProvider
+        }
+
+        return when {
+            wallet.blockchain.isEvm() -> {
+                val storage = PendingTransactionStorage(wallet, dataStorage)
+                EthereumPendingTransactionsProvider(
+                    wallet = wallet,
+                    multiJsonRpcProvider = networkProvider as MultiNetworkProvider<EthereumJsonRpcProvider>,
+                    storage = storage,
+                    coroutineScope = coroutineScope,
+                    networkProviderMap = networkProviderMap,
+                )
+            }
+            else -> DefaultPendingTransactionsProvider
+        }
+    }
+}
