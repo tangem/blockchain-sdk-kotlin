41fb3911b67fcda6ea61c12ff30a67fa3038b7a3
Tangem <git@tangem.com>
2026-01-16 14:03:00 +0400

    Updated on 2026-02-11

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/chia/ChiaAddressService.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/chia/ChiaAddressService.kt
@@ -1,41 +1,46 @@
 package com.tangem.blockchain.blockchains.chia
 
-import Bech32
 import com.tangem.blockchain.blockchains.binance.client.encoding.Crypto
 import com.tangem.blockchain.blockchains.chia.clvm.Program
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.address.AddressService
 import com.tangem.common.card.EllipticCurve
 import com.tangem.common.extensions.hexToBytes
+import org.bitcoinj.core.AddressFormatException
+import org.bitcoinj.core.Bech32
 
 class ChiaAddressService(blockchain: Blockchain) : AddressService() {
 
     private val humanReadablePart = when (blockchain) {
-        Blockchain.Chia -> "xch"
-        Blockchain.ChiaTestnet -> "txch"
+        Blockchain.Chia -> HRP_MAINNET
+        Blockchain.ChiaTestnet -> HRP_TESTNET
         else -> error("$blockchain isn't supported")
     }
 
     override fun makeAddress(walletPublicKey: ByteArray, curve: EllipticCurve?): String {
         val puzzle = getPuzzle(walletPublicKey)
         val puzzleHash = Program.deserialize(puzzle).hash()
-
-        return Bech32.encode(
-            humanReadablePart = humanReadablePart,
-            dataIn = puzzleHash.toUByteArray(),
-        )
+        val fiveBitData = Crypto.convertBits(puzzleHash, 0, puzzleHash.size, FROM_BITS, TO_BITS, true)
+        return Bech32.encode(Bech32.Encoding.BECH32M, humanReadablePart, fiveBitData)
     }
 
     override fun validate(address: String): Boolean {
         return try {
             val decoded = Bech32.decode(address)
-            decoded.humanReadablePart == humanReadablePart
-        } catch (e: Exception) {
+            decoded.hrp == humanReadablePart && decoded.encoding == Bech32.Encoding.BECH32M
+        } catch (e: AddressFormatException) {
+            false
+        } catch (e: IllegalArgumentException) {
             false
         }
     }
 
     companion object {
+        private const val HRP_MAINNET = "xch"
+        private const val HRP_TESTNET = "txch"
+        private const val FROM_BITS = 8
+        private const val TO_BITS = 5
+
         // curried and serialized signature.clsp (https://github.com/Chia-Network/chialisp-crash-course/blob/af620db2505db507b348d4f036dc4955fa81a004/signature.clsp)
         fun getPuzzle(walletPublicKey: ByteArray): ByteArray {
             return (
@@ -46,10 +51,9 @@ class ChiaAddressService(blockchain: Blockchain) : AddressService() {
                 ).hexToBytes() + walletPublicKey + "ff018080".hexToBytes()
         }
 
-        @Suppress("MagicNumber")
         fun getPuzzleHash(address: String): ByteArray {
-            val dataBytes = Bech32.decode(address).data.toByteArray()
-            return Crypto.convertBits(dataBytes, 0, dataBytes.size, 5, 8, false)
+            val decoded = Bech32.decode(address)
+            return Crypto.convertBits(decoded.data, 0, decoded.data.size, TO_BITS, FROM_BITS, false)
         }
     }
 }
