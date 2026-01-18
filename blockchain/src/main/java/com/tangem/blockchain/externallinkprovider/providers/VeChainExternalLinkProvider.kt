b68c0ed8e6245afebfe323fc2349cb3031d6c294
Tangem <git@tangem.com>
2026-01-19 01:29:32 +0400

    Updated on 2026-01-28

--- blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/VeChainExternalLinkProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/externallinkprovider/providers/VeChainExternalLinkProvider.kt
@@ -14,7 +14,7 @@ internal class VeChainExternalLinkProvider(isTestnet: Boolean) : ExternalLinkPro
         return explorerBaseUrl + "accounts/$walletAddress"
     }
 
-    override fun getExplorerTxUrl(transactionHash: String): TxExploreState {
+    override fun getExplorerTxUrl(transactionHash: String, contractAddress: String?): TxExploreState {
         return TxExploreState.Url(explorerBaseUrl + "transactions/$transactionHash")
     }
 }
