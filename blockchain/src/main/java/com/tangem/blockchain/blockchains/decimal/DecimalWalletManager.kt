017b95eeb34806f68bcd60e57dbf271af2bf60cc
Tangem <git@tangem.com>
2026-01-22 16:13:50 +0400

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/decimal/DecimalWalletManager.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/decimal/DecimalWalletManager.kt
@@ -57,16 +57,16 @@ internal class DecimalWalletManager(
         transactionData: TransactionData,
         signer: TransactionSigner,
     ): Result<TransactionSendResult> {
-        transactionData.requireUncompiled()
-        return super.send(convertTransactionDataAddress(transactionData), signer)
+        val uncompiledTransaction = transactionData.requireUncompiled()
+        return super.send(convertTransactionDataAddress(uncompiledTransaction), signer)
     }
 
     override suspend fun sign(
         transactionData: TransactionData,
         signer: TransactionSigner,
     ): Result<Pair<ByteArray, EthereumCompiledTxInfo>> {
-        transactionData.requireUncompiled()
-        return super.sign(convertTransactionDataAddress(transactionData), signer)
+        val uncompiledTransaction = transactionData.requireUncompiled()
+        return super.sign(convertTransactionDataAddress(uncompiledTransaction), signer)
     }
 
     private fun convertTransactionDataAddress(transactionData: TransactionData.Uncompiled) = transactionData.copy(
