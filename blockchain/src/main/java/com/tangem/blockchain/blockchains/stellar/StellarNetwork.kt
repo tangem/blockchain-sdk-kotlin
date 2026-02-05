cba48b932a5ed8989be5bbf9f90c58aa40bc5323
Tangem <git@tangem.com>
2026-02-05 17:43:59 +0400

    Updated on 2026-06-27

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/stellar/StellarNetwork.kt
+++ blockchain/src/main/java/com/tangem/blockchain/blockchains/stellar/StellarNetwork.kt
@@ -4,6 +4,12 @@ sealed class StellarNetwork(val url: String) {
 
     data class Nownodes(val apiKey: String) : StellarNetwork(url = "https://xlm.nownodes.io/$apiKey/")
 
+    data class QuickNode(val subdomain: String, val apiKey: String) :
+        StellarNetwork(url = "https://$subdomain/$apiKey/")
+
+    data class GetBlock(val accessToken: String) :
+        StellarNetwork(url = "https://xlm.getblock.io/$accessToken/mainnet/")
+
     data class Public(val baseUrl: String) : StellarNetwork(url = baseUrl)
 
     object HorizonTestnet : StellarNetwork(url = "https://horizon-testnet.stellar.org/")
