16eb87f339839dbbf0e5603a05db442cc370162c
Tangem <git@tangem.com>
2026-04-09 18:02:06 +0400

    Updated on 2026-05-04

--- blockchain/src/main/java/com/tangem/blockchain/common/BlockchainFeatureToggles.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/BlockchainFeatureToggles.kt
@@ -9,4 +9,5 @@ class BlockchainFeatureToggles(
     val isYieldSupplyEnabled: Boolean,
     val isPendingTransactionsEnabled: Boolean = false,
     val isSolanaTxHistoryEnabled: Boolean = false,
+    val isSolanaScaledUiAmountEnabled: Boolean = false,
 )
