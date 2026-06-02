ecdc4c12aeca8c890e8680989e853f73a34097af
Tangem <git@tangem.com>
2026-06-02 20:23:20 +0500

    Updated on 2026-07-23

--- blockchain/src/main/java/com/tangem/blockchain/transactionhistory/blockchains/tron/TronTransactionHistoryProvider.kt
+++ blockchain/src/main/java/com/tangem/blockchain/transactionhistory/blockchains/tron/TronTransactionHistoryProvider.kt
@@ -1,9 +1,13 @@
 package com.tangem.blockchain.transactionhistory.blockchains.tron
 
 import com.tangem.Log
-import com.tangem.blockchain.common.*
+import com.tangem.blockchain.common.Amount
+import com.tangem.blockchain.common.AmountType
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchain.common.Token
 import com.tangem.blockchain.common.pagination.Page
 import com.tangem.blockchain.common.pagination.PaginationWrapper
+import com.tangem.blockchain.common.toBlockchainSdkError
 import com.tangem.blockchain.extensions.Result
 import com.tangem.blockchain.network.blockbook.network.BlockBookApi
 import com.tangem.blockchain.network.blockbook.network.responses.GetAddressResponse
@@ -20,6 +24,7 @@ import java.util.concurrent.TimeUnit
 
 private const val PREFIX = "0x"
 
+@Suppress("LargeClass")
 internal class TronTransactionHistoryProvider(
     private val blockchain: Blockchain,
     private val blockBookApi: BlockBookApi,
@@ -108,6 +113,7 @@ internal class TronTransactionHistoryProvider(
                     TransactionHistoryState.Success.Empty
                 }
             }
+
             is TransactionHistoryRequest.FilterType.Contract -> {
                 val token = response.trxTokens
                     ?.find { it.matching(filterType.tokenInfo.contractAddress) }
@@ -135,7 +141,7 @@ internal class TronTransactionHistoryProvider(
             walletAddress = walletAddress,
         ).guard {
             Log.info { "Transaction $this doesn't contain a required value" }
-            return null
+            error("Transaction $this doesn't contain a required value")
         }
         val amount = extractAmount(
             tx = this,
@@ -144,7 +150,7 @@ internal class TronTransactionHistoryProvider(
             walletAddress = walletAddress,
         ).guard {
             Log.info { "Transaction $this doesn't contain a required value" }
-            return null
+            error("Transaction $this doesn't contain a required value")
         }
         if (shouldExcludeFromHistory(filterType, amount)) {
             Log.info { "Transaction with zero amount is excluded from history. $this" }
@@ -156,7 +162,7 @@ internal class TronTransactionHistoryProvider(
             walletAddress = walletAddress,
         ).guard {
             Log.info { "Transaction $this doesn't contain a required value" }
-            return null
+            error("Transaction $this doesn't contain a required value")
         }
         val type = extractType(filterType = filterType, tx = this)
         return TransactionHistoryItem(
@@ -182,6 +188,7 @@ internal class TronTransactionHistoryProvider(
             is TransactionHistoryItem.SourceType.Multiple -> {
                 sourceType.addresses.any { it.equals(walletAddress, ignoreCase = true) }
             }
+
             is TransactionHistoryItem.SourceType.Single -> {
                 sourceType.address.equals(walletAddress, ignoreCase = true)
             }
@@ -192,18 +199,14 @@ internal class TronTransactionHistoryProvider(
         return when (type) {
             is TronStakingTransactionType.VoteWitnessContract,
             TronStakingTransactionType.FreezeBalanceV2Contract,
-            -> {
-                true
-            }
+            -> true
+
             TronStakingTransactionType.WithdrawBalanceContract,
             TronStakingTransactionType.WithdrawExpireUnfreezeContract,
             TronStakingTransactionType.UnfreezeBalanceV2Contract,
-            -> {
-                false
-            }
-            else -> {
-                null
-            }
+            -> false
+
+            else -> null
         }
     }
 
@@ -212,21 +215,24 @@ internal class TronTransactionHistoryProvider(
         filterType: TransactionHistoryRequest.FilterType,
         walletAddress: String,
     ): TransactionHistoryItem.DestinationType? {
-        tx.toAddress ?: return null
-        tx.fromAddress ?: return null
         return when (filterType) {
             TransactionHistoryRequest.FilterType.Coin -> {
+                val toAddress = tx.toAddress ?: tx.vout.firstOrNull()?.addresses?.firstOrNull()
+                    ?: return null
                 TransactionHistoryItem.DestinationType.Single(
                     addressType = if (tx.tokenTransfers.isEmpty()) {
-                        TransactionHistoryItem.AddressType.User(tx.toAddress)
+                        TransactionHistoryItem.AddressType.User(toAddress)
                     } else {
-                        TransactionHistoryItem.AddressType.Contract(tx.toAddress)
+                        TransactionHistoryItem.AddressType.Contract(toAddress)
                     },
                 )
             }
 
             is TransactionHistoryRequest.FilterType.Contract -> {
-                val transfer = tx.getTokenTransfer(walletAddress, filterType.tokenInfo.contractAddress) ?: return null
+                val transfer = tx.getTokenTransfer(
+                    walletAddress = walletAddress,
+                    contractAddress = filterType.tokenInfo.contractAddress,
+                ) ?: return null
                 TransactionHistoryItem.DestinationType.Single(
                     addressType = TransactionHistoryItem.AddressType.User(transfer.to),
                 )
@@ -240,7 +246,10 @@ internal class TronTransactionHistoryProvider(
         walletAddress: String,
     ): TransactionHistoryItem.SourceType? {
         val address = when (filterType) {
-            TransactionHistoryRequest.FilterType.Coin -> tx.fromAddress
+            TransactionHistoryRequest.FilterType.Coin -> {
+                tx.fromAddress ?: tx.vin.firstOrNull()?.addresses?.firstOrNull()
+            }
+
             is TransactionHistoryRequest.FilterType.Contract -> {
                 tx.getTokenTransfer(walletAddress, filterType.tokenInfo.contractAddress)?.from
             }
@@ -259,24 +268,33 @@ internal class TronTransactionHistoryProvider(
                     TRANSFER_CONTRACT_TYPE, TRANSFER_ASSET_CONTRACT_TYPE -> {
                         TransactionHistoryItem.TransactionType.Transfer
                     }
+
                     VOTE_WITNESS_CONTRACT_TYPE -> { // vote
-                        TronStakingTransactionType.VoteWitnessContract(tx.voteList?.keys?.first().orEmpty())
+                        TronStakingTransactionType.VoteWitnessContract(
+                            validatorAddress = tx.voteList?.keys?.firstOrNull().orEmpty(),
+                        )
                     }
+
                     WITHDRAW_BALANCE_CONTRACT_TYPE -> { // claim rewards
                         TronStakingTransactionType.WithdrawBalanceContract
                     }
+
                     FREEZE_BALANCE_V2_CONTRACT_TYPE -> { // freeze/stake
                         TronStakingTransactionType.FreezeBalanceV2Contract
                     }
+
                     UNFREEZE_BALANCE_V2_CONTRACT_TYPE -> { // unfreeze/unstake
                         TronStakingTransactionType.UnfreezeBalanceV2Contract
                     }
+
                     WITHDRAW_EXPIRE_UNFREEZE_CONTRACT_TYPE -> { // withdraw
                         TronStakingTransactionType.WithdrawExpireUnfreezeContract
                     }
+
                     else -> TransactionHistoryItem.TransactionType.ContractMethod(id = tx.contractName.orEmpty())
                 }
             }
+
             is TransactionHistoryRequest.FilterType.Contract -> {
                 // All TRC10 and TRC20 token transactions are considered simple & plain transfers
                 TransactionHistoryItem.TransactionType.Transfer
@@ -298,7 +316,9 @@ internal class TronTransactionHistoryProvider(
             )
 
             is TransactionHistoryRequest.FilterType.Contract -> {
-                val transfer = tx.getTokenTransfer(walletAddress, filterType.tokenInfo.contractAddress) ?: return null
+                val transfer =
+                    tx.getTokenTransfer(walletAddress, filterType.tokenInfo.contractAddress)
+                        ?: return null
                 val transferValue = transfer.value ?: "0"
                 val token = Token(
                     name = transfer.name.orEmpty(),
@@ -333,7 +353,8 @@ internal class TronTransactionHistoryProvider(
     ): GetAddressResponse.Transaction.TokenTransfer? {
         return tokenTransfers.firstOrNull {
             contractAddress.equals(it.token, ignoreCase = true) &&
-                walletAddress.equals(it.to, ignoreCase = true) || walletAddress.equals(it.from, ignoreCase = true)
+                walletAddress.equals(it.to, ignoreCase = true) ||
+                walletAddress.equals(it.from, ignoreCase = true)
         }
     }
 
